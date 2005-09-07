e4e8ca92341518ad9eb72b2655e814fb6f272d80
Rainer Gerhards <rgerhards@adiscon.com>
2005-09-07 10:04:09 +0000

    removed DOS CRLFs from stringbuf.c

--- stringbuf.c
+++ stringbuf.c
@@ -37,7 +37,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include <stdlib.h>
+#include <stdlib.h
 #include <assert.h>
 #include <string.h>
 #include "liblogging-stub.h"
