3c79979b830ad0204ae47592fa858a996ce90b5e
Rainer Gerhards <rgerhards@adiscon.com>
2007-12-11 16:18:43 +0000

    added synchronization class to handle mutex-operations in the most portable     way.

---
+++ sync.c
@@ -0,0 +1,78 @@
+/* synrchonization-related stuff. In theory, that should
+ * help porting to something different from pthreads.
+ *
+ * Copyright 2007 Rainer Gerhards and Adiscon GmbH.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * A copy of the GPL can be found in the file "COPYING" in this distribution.
+ */
+
+#include "config.h"
+
+#ifdef USE_PTHREADS
+/* all of this code is compiled only if PTHREADS is supported - otherwise
+ * we do not need syncrhonization objects (and do not have them!).
+ */
+#include <stdlib.h>
+
+#include "rsyslog.h"
+#include "sync.h"
+
+
+void
+SyncObjInit(pthread_mutex_t **mut)
+{
+	*mut = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+	pthread_mutex_init(*mut, NULL);
+}
+
+
+/* This function destroys the mutex and also sets the mutex object
+ * to NULL. While the later is not strictly necessary, it is a good
+ * aid when debugging problems. As this function is not exepected to 
+ * be called quite frequently, the additional overhead can well be
+ * accepted. If this changes over time, setting to NULL may be
+ * reconsidered. - rgerhards, 2007-11-12
+ */
+void
+SyncObjExit(pthread_mutex_t **mut)
+{
+	if(*mut != NULL) {
+		pthread_mutex_destroy(*mut);
+		free(*mut);
+		*mut = NULL;
+	}
+}
+
+/* lock an object. The synchronization tool (mutex) must be passed in.
+ */
+void
+lockObj(pthread_mutex_t *mut)
+{
+dbgprintf("trying to lock %lx\n", (unsigned long) mut);
+	pthread_mutex_lock(mut);
+}
+
+/* unlock an object. The synchronization tool (mutex) must be passed in.
+ */
+void
+unlockObj(pthread_mutex_t *mut)
+{
+dbgprintf("trying to UNlock %lx\n", (unsigned long) mut);
+	pthread_mutex_unlock(mut);
+}
+
+#endif /* #ifdef USE_PTHREADS */
---
+++ sync.h
@@ -0,0 +1,52 @@
+/* Definitions syncrhonization-related stuff. In theory, that should
+ * help porting to something different from pthreads.
+ *
+ * Copyright 2007 Rainer Gerhards and Adiscon GmbH.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * A copy of the GPL can be found in the file "COPYING" in this distribution.
+ */
+
+#ifndef INCLUDED_SYNC_H
+#define INCLUDED_SYNC_H
+
+#ifdef USE_PTHREADS /* Code to compile for threading support */
+#include <pthread.h>
+
+/* SYNC_OBJ_TOOL definition must be placed in object to be synced!
+ * SYNC_OBJ_TOOL_INIT must be called upon of object construction and
+ * SUNC_OBJ_TOOL_EXIT must be called upon object destruction
+ */
+#define SYNC_OBJ_TOOL pthread_mutex_t *Sync_mut;
+#define SYNC_OBJ_TOOL_INIT(x) SyncObjInit(&((x)->Sync_mut))
+#define SYNC_OBJ_TOOL_EXIT(x) SyncObjExit(&((x)->Sync_mut))
+#define LockObj(x) lockObj((x)->Sync_mut)
+#define UnlockObj(x) unlockObj((x)->Sync_mut)
+
+void SyncObjInit(pthread_mutex_t **mut);
+void SyncObjExit(pthread_mutex_t **mut);
+extern void lockObj(pthread_mutex_t *mut);
+extern void unlockObj(pthread_mutex_t *mut);
+
+#else /* Code not to compile for threading support */
+#define SYNC_OBJ_TOOL
+#define SYNC_OBJ_TOOL_INIT
+#define SYNC_OBJ_TOOL_EXIT
+#define LockObj(x)
+#define UnlockObj(x)
+#endif
+
+#endif /* #ifndef INCLUDED_SYNC_H */
