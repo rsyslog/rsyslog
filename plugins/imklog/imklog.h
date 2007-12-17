16e92a47ac73dcda92b2e51b854fd9010319f05f
Rainer Gerhards <rgerhards@adiscon.com>
2007-12-17 13:09:18 +0000

    fixed missing file

---
+++ plugins/imklog/imklog.h
@@ -0,0 +1,41 @@
+/* imklog.h
+ * These are the definitions for the klog message generation module.
+ *
+ * File begun on 2007-12-17 by RGerhards
+ *
+ * Copyright 2007 Rainer Gerhards and Adiscon GmbH.
+ *
+ * This file is part of rsyslog.
+ *
+ * Rsyslog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Rsyslog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Rsyslog.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * A copy of the GPL can be found in the file "COPYING" in this distribution.
+ */
+#ifndef	IMKLOG_H_INCLUDED
+#define	IMKLOG_H_INCLUDED 1
+
+#include "rsyslog.h"
+
+/* prototypes */
+extern int InitKsyms(char *);
+extern int InitMsyms(void);
+extern char * ExpandKadds(char *, char *);
+extern void SetParanoiaLevel(int);
+extern void vsyslog(int pri, const char *fmt, va_list ap);
+rsRetVal Syslog(int priority, char *fmt, ...) __attribute__((format(printf,2, 3)));
+
+#endif /* #ifndef IMKLOG_H_INCLUDED */
+/*
+ * vi:set ai:
+ */
