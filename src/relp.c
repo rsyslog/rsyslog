7c4c72da7966b5aa5edb4c973d009c150ccc0033
Rainer Gerhards <rgerhards@adiscon.com>
2008-03-13 14:47:37 +0100

    cleaned up install environment, should now install things to where they belong

--- src/relp.c
+++ src/relp.c
@@ -4,18 +4,18 @@
  *
  * This file is part of librelp.
  *
- * Rsyslog is free software: you can redistribute it and/or modify
+ * Librelp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Rsyslog is distributed in the hope that it will be useful,
+ * Librelp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Rsyslog.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Librelp.  If not, see <http://www.gnu.org/licenses/>.
  *
  * A copy of the GPL can be found in the file "COPYING" in this distribution.
  *
