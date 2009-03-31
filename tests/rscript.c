d27edc7587dba7b850759d151d90cdad1cb34a35
Rainer Gerhards <rgerhards@adiscon.com>
2009-03-31 20:35:15 +0200

    porting parser tests to solaris

--- tests/rscript.c
+++ tests/rscript.c
@@ -40,37 +40,6 @@ DEFobjCurrIf(ctok_token)
 DEFobjCurrIf(vmprg)
 
 
-/* we emulate getline (the dirty way) if we do not have it
- * We do not try very hard, as this is just a test driver.
- * rgerhards, 2009-03-31
- */
-#ifndef HAVE_GETLINE
-ssize_t getline(char **lineptr, size_t *n, FILE *fp)
-{
-	int c;
-	int len = 0;
-
-	if(*lineptr == NULL)
-		*lineptr = malloc(1024); /* quick and dirty! */
-
-	c = fgetc(fp);
-	while(c != EOF && c != '\n') {
-		(*lineptr)[len++] = c;
-		c = fgetc(fp);
-	}
-	if(c != EOF) /* need to add NL? */
-		(*lineptr)[len++] = c;
-	
-	(*lineptr)[len] = '\0';
-
-	*n = len;
-	//printf("getline returns: '%s'\n", *lineptr);
-
-	return (len > 0) ? len : -1;
-}
-#endif /* #ifndef HAVE_GETLINE */
-
-
 BEGINInit
 CODESTARTInit
 	pErrObj = "expr"; CHKiRet(objUse(expr, CORE_COMPONENT));
