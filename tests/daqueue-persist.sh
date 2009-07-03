3250a710abb07bf4cfda1c3030b767286b39eff2
Rainer Gerhards <rgerhards@adiscon.com>
2009-07-03 18:19:11 +0200

    re-enabled a test

--- tests/daqueue-persist.sh
+++ tests/daqueue-persist.sh
@@ -3,9 +3,9 @@
 # added 2009-05-27 by Rgerhards
 # This file is part of the rsyslog project, released  under GPLv3
 source $srcdir/daqueue-persist-drvr.sh LinkedList
-#source $srcdir/daqueue-persist-drvr.sh FixedArray
+source $srcdir/daqueue-persist-drvr.sh FixedArray
 # the disk test should not fail, however, the config is extreme and using
 # it more or less is a config error
-#source $srcdir/daqueue-persist-drvr.sh Disk
+source $srcdir/daqueue-persist-drvr.sh Disk
 # we do not test Direct mode because this absolute can not work in direct mode
 # (maybe we should do a fail-type of test?)
