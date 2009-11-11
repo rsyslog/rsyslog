22bf4fc83274df41f9d490f522718f248e35c1f5
Rainer Gerhards <rgerhards@adiscon.com>
2009-11-11 14:12:09 +0100

    somewhat enhanced test suite
    also prepared test case for TLS testing, but not activated it
    inside the build itself.
    A note on TLS testing: the current testsuite (in git!) already contains
    TLS test cases. However, getting these test cases correct is not simple.
    That's not a problem with the code itself, but rater a problem with
    synchronization in the test environment. So I have deciced to keep the
    TLS tests in, but not yet actually utilize them. This is most probably
    left as an excercise for future (devel) releases.

--- tests/sndrcv_drvr.sh
+++ tests/sndrcv_drvr.sh
@@ -9,26 +9,41 @@
 # config file name ($2). From that name, the sender and receiver config file
 # names are automatically generated. 
 # So: $1 config file name, $2 number of messages
+#
+# A note on TLS testing: the current testsuite (in git!) already contains
+# TLS test cases. However, getting these test cases correct is not simple.
+# That's not a problem with the code itself, but rater a problem with
+# synchronization in the test environment. So I have deciced to keep the
+# TLS tests in, but not yet actually utilize them. This is most probably
+# left as an excercise for future (devel) releases. -- rgerhards, 2009-11-11
+#
 # added 2009-11-11 by Rgerhards
 # This file is part of the rsyslog project, released  under GPLv3
 # uncomment for debugging support:
 source $srcdir/diag.sh init
 # start up the instances
-source $srcdir/diag.sh startup $1_sender.conf 2
+#export RSYSLOG_DEBUG="debug nostdout noprintmutexaction"
+#export RSYSLOG_DEBUGLOG="log"
 source $srcdir/diag.sh startup $1_rcvr.conf 
-source $srcdir/diag.sh wait-startup2
 source $srcdir/diag.sh wait-startup
+#export RSYSLOG_DEBUGLOG="log2"
+#valgrind="valgrind"
+source $srcdir/diag.sh startup $1_sender.conf 2
+source $srcdir/diag.sh wait-startup 2
+# may be needed by TLS (once we do it): sleep 30
 
 # now inject the messages into instance 2. It will connect to instance 1,
 # and that instance will record the data.
-source $srcdir/diag.sh tcpflood 127.0.0.1 13515 1 $2 1
+source $srcdir/diag.sh tcpflood 127.0.0.1 13514 1 $2 1
 # shut down sender when everything is sent, receiver continues to run concurrently
+# may be needed by TLS (once we do it): sleep 60
 source $srcdir/diag.sh shutdown-when-empty 2
 source $srcdir/diag.sh wait-shutdown 2
 # now it is time to stop the receiver as well
 source $srcdir/diag.sh shutdown-when-empty
 source $srcdir/diag.sh wait-shutdown
 
+# may be needed by TLS (once we do it): sleep 60
 # do the final check
 source $srcdir/diag.sh seq-check 1 $2
 source $srcdir/diag.sh exit
--- tests/sndrcv_gzip.sh
+++ tests/sndrcv_gzip.sh
@@ -1,7 +1,7 @@
 # This test is similar to tcpsndrcv, but it forwards messages in
 # zlib-compressed format (our own syslog extension).
+# rgerhards, 2009-11-11
 # This file is part of the rsyslog project, released  under GPLv3
-# uncomment for debugging support:
 echo ===============================================================================
-echo \[tcpsndrcv_gzip.sh\]: testing sending and receiving via tcp in zlib mode
-source $srcdir/tcpsndrcv_drvr.sh tcpsndrcv_gzip 50000
+echo \[sndrcv_gzip.sh\]: testing sending and receiving via tcp in zlib mode
+source $srcdir/sndrcv_drvr.sh sndrcv_gzip 50000
