b5e44445f9b0ac229b7802a46df33d0f2c4a2cd8
Rainer Gerhards <rgerhards@adiscon.com>
2010-02-08 18:35:27 +0100

    added one more test to testbench

--- tests/parsertest.sh
+++ tests/parsertest.sh
@@ -4,10 +4,14 @@ source $srcdir/diag.sh nettester parse1 udp
 source $srcdir/diag.sh nettester parse1 tcp
 source $srcdir/diag.sh nettester parse3 udp
 source $srcdir/diag.sh nettester parse3 tcp
+source $srcdir/diag.sh nettester parse_invld_regex udp
+source $srcdir/diag.sh nettester parse_invld_regex tcp
 
 echo \[parsertest.sh]: redoing tests in IPv4-only mode
 source $srcdir/diag.sh nettester parse1 udp -4
 source $srcdir/diag.sh nettester parse1 tcp -4
 source $srcdir/diag.sh nettester parse3 udp -4
 source $srcdir/diag.sh nettester parse3 tcp -4
+source $srcdir/diag.sh nettester parse_invld_regex udp -4
+source $srcdir/diag.sh nettester parse_invld_regex tcp -4
 source $srcdir/diag.sh exit
