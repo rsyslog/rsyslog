eff5630ec0944b9fe10a50be7f236ac714807be7
Rainer Gerhards <rgerhards@adiscon.com>
2010-02-08 17:28:58 +0100

    enhanced test suite
    now CSV and regex extraction are being tested (would have caught the
    bug fixed with the last commit).

--- tests/parsertest.sh
+++ tests/parsertest.sh
@@ -1,5 +1,13 @@
-echo TEST: parsertest.sh - various parser tests
+echo TEST: \[parsertest.sh\]: various parser tests
 source $srcdir/diag.sh init
 source $srcdir/diag.sh nettester parse1 udp
 source $srcdir/diag.sh nettester parse1 tcp
-source $srcdir/diag.sh init
+source $srcdir/diag.sh nettester parse3 udp
+source $srcdir/diag.sh nettester parse3 tcp
+
+echo \[parsertest.sh]: redoing tests in IPv4-only mode
+source $srcdir/diag.sh nettester parse1 udp -4
+source $srcdir/diag.sh nettester parse1 tcp -4
+source $srcdir/diag.sh nettester parse3 udp -4
+source $srcdir/diag.sh nettester parse3 tcp -4
+source $srcdir/diag.sh exit
