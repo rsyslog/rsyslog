d080ffe467f6f58be82ee14dbeed94026002f1e0
Rainer Gerhards <rgerhards@adiscon.com>
2010-03-03 09:24:40 +0100

    reordered some include, because otherwise we get zlib-related errors
    on some platforms (namely Debian sid). This smells like a bug in zlib.
    Thanks to Michael Biebl for reporting the issue.

--- tests/rt-init.c
+++ tests/rt-init.c
@@ -21,10 +21,9 @@
  *
  * A copy of the GPL can be found in the file "COPYING" in this distribution.
  */
-#include <stdio.h>
-
 #include "rsyslog.h"
 #include "testbench.h"
+#include <stdio.h>	/* must be last, else we get a zlib compile error on some platforms */
 
 MODULE_TYPE_TESTBENCH
 
