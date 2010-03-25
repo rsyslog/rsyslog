d102aab886fcde914677146caf20bd68ca41bfff
Rainer Gerhards <rgerhards@adiscon.com>
2010-03-25 15:23:39 +0100

    new feature: $OMFileAsyncWriting directive added
    it permits to specifiy if asynchronous writing should be done or not

--- tests/wr_large.sh
+++ tests/wr_large.sh
@@ -4,13 +4,9 @@
 # added 2010-03-10 by Rgerhards
 #
 # This file is part of the rsyslog project, released  under GPLv3
-echo ===============================================================================
-echo TEST: \[asynwr_large.sh\]: test for async file writing for large message sets
+cat rsyslog.action.1.include
 source $srcdir/diag.sh init
-# uncomment for debugging support:
-#export RSYSLOG_DEBUG="debug nostdout noprintmutexaction"
-#export RSYSLOG_DEBUGLOG="log"
-source $srcdir/diag.sh startup asynwr_large.conf
+source $srcdir/diag.sh startup wr_large.conf
 # send 4000 messages of 10.000bytes plus header max, randomized
 source $srcdir/diag.sh tcpflood -m4000 -r -d10000 -P129
 sleep 1 # due to large messages, we need this time for the tcp receiver to settle...
