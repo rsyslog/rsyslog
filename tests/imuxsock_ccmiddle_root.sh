87bffff894a6ed82f126e6af33beaf53efb6b4c5
Rainer Gerhards <rgerhards@adiscon.com>
2011-02-22 16:10:21 +0100

    added one more test for imuxsock to autmatted test suite
    control character escaping is now also being tested

--- tests/imuxsock_ccmiddle_root.sh
+++ tests/imuxsock_ccmiddle_root.sh
@@ -1,18 +1,18 @@
 # note: we must be root and no other syslogd running in order to
 # carry out this test
-echo \[imuxsock_traillf_root.sh\]: test trailing LF handling in imuxsock
+echo \[imuxsock_ccmiddle_root.sh\]: test trailing LF handling in imuxsock
 echo This test must be run as root with no other active syslogd
 source $srcdir/diag.sh init
-source $srcdir/diag.sh startup imuxsock_traillf_root.conf
+source $srcdir/diag.sh startup imuxsock_ccmiddle_root.conf
 # send a message with trailing LF
-$srcdir/syslog_inject -l
+$srcdir/syslog_inject -c
 # the sleep below is needed to prevent too-early termination of rsyslogd
 $srcdir/msleep 100
 source $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
 source $srcdir/diag.sh wait-shutdown	# we need to wait until rsyslogd is finished!
-cmp rsyslog.out.log $srcdir/resultdata/imuxsock_traillf.log
+cmp rsyslog.out.log $srcdir/resultdata/imuxsock_ccmiddle.log
 if [ ! $? -eq 0 ]; then
-echo "imuxsock_traillf_root.sh failed"
+echo "imuxsock_ccmiddle_root.sh failed"
 exit 1
 fi;
 source $srcdir/diag.sh exit
--- tests/syslog_lf.c
+++
@@ -1,9 +0,0 @@
-/* This tool deliberately logs a message with the a trailing LF */
-#include <stdio.h>
-#include <syslog.h>
-
-int main()
-{
-	syslog(LOG_NOTICE, "test\n");
-	return 0;
-}
