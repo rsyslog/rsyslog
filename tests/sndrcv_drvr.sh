a2a3c58c6c0d0f4e2cbe053979ed4feb32298ac5
Rainer Gerhards <rgerhards@adiscon.com>
2011-04-04 09:35:32 +0200

    activated TLS send/receive test in testbench
    This was possible because in v6 we do no longer have the TLS startup
    delay.

--- tests/sndrcv_drvr.sh
+++ tests/sndrcv_drvr.sh
@@ -10,13 +10,6 @@
 # names are automatically generated. 
 # So: $1 config file name, $2 number of messages
 #
-# A note on TLS testing: the current testsuite (in git!) already contains
-# TLS test cases. However, getting these test cases correct is not simple.
-# That's not a problem with the code itself, but rater a problem with
-# synchronization in the test environment. So I have deciced to keep the
-# TLS tests in, but not yet actually utilize them. This is most probably
-# left as an excercise for future (devel) releases. -- rgerhards, 2009-11-11
-#
 # added 2009-11-11 by Rgerhards
 # This file is part of the rsyslog project, released  under GPLv3
 # uncomment for debugging support:
@@ -30,21 +23,18 @@ source $srcdir/diag.sh wait-startup
 #valgrind="valgrind"
 source $srcdir/diag.sh startup $1_sender.conf 2
 source $srcdir/diag.sh wait-startup 2
-# may be needed by TLS (once we do it): sleep 30
 
 # now inject the messages into instance 2. It will connect to instance 1,
 # and that instance will record the data.
 source $srcdir/diag.sh tcpflood -m$2 -i1
 sleep 2 # make sure all data is received in input buffers
 # shut down sender when everything is sent, receiver continues to run concurrently
-# may be needed by TLS (once we do it): sleep 60
 source $srcdir/diag.sh shutdown-when-empty 2
 source $srcdir/diag.sh wait-shutdown 2
 # now it is time to stop the receiver as well
 source $srcdir/diag.sh shutdown-when-empty
 source $srcdir/diag.sh wait-shutdown
 
-# may be needed by TLS (once we do it): sleep 60
 # do the final check
 source $srcdir/diag.sh seq-check 1 $2
 source $srcdir/diag.sh exit
