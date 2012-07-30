575686bd68a47fc5d9f59c1ed610f6680e1bb6fa
Rainer Gerhards <rgerhards@adiscon.com>
2012-07-30 11:01:11 +0200

    omelasticsearch: mileston: bulk reply is parsed

--- plugins/omelasticsearch/omelasticsearch.c
+++ plugins/omelasticsearch/omelasticsearch.c
@@ -382,6 +382,53 @@ finalize_it:
 }
 
 static inline rsRetVal
+checkResultBulkmode(instanceData *pData, cJSON *root)
+{
+	int i;
+	int numitems;
+	cJSON *items;
+	cJSON *item;
+	cJSON *create;
+	cJSON *ok;
+	DEFiRet;
+
+	items = cJSON_GetObjectItem(root, "items");
+	if(items == NULL || items->type != cJSON_Array) {
+		DBGPRINTF("omelasticsearch: error in elasticsearch reply: "
+			  "bulkmode insert does not return array, reply is: %s\n",
+			  pData->reply);
+		ABORT_FINALIZE(RS_RET_DATAFAIL);
+	}
+	numitems = cJSON_GetArraySize(items);
+DBGPRINTF("omelasticsearch: %d items in reply\n", numitems);
+	for(i = 0 ; i < numitems ; ++i) {
+		item = cJSON_GetArrayItem(items, i);
+		if(item == NULL)  {
+			DBGPRINTF("omelasticsearch: error in elasticsearch reply: "
+				  "cannot obtain reply array item %d\n", i);
+			ABORT_FINALIZE(RS_RET_DATAFAIL);
+		}
+		create = cJSON_GetObjectItem(item, "create");
+		if(create == NULL || create->type != cJSON_Object) {
+			DBGPRINTF("omelasticsearch: error in elasticsearch reply: "
+				  "cannot obtain 'create' item for #%d\n", i);
+			ABORT_FINALIZE(RS_RET_DATAFAIL);
+		}
+		ok = cJSON_GetObjectItem(create, "ok");
+		if(ok == NULL || ok->type != cJSON_True) {
+			DBGPRINTF("omelasticsearch: error in elasticsearch reply: "
+				  "item ok (%p) not ok\n", ok);
+			ABORT_FINALIZE(RS_RET_DATAFAIL);
+		}
+dbgprintf("omelasticsearch: item %d is OK\n", i);
+	}
+
+finalize_it:
+	RETiRet;
+}
+
+
+static inline rsRetVal
 checkResult(instanceData *pData)
 {
 	cJSON *root;
@@ -395,8 +442,7 @@ checkResult(instanceData *pData)
 	}
 
 	if(pData->bulkmode) {
-		//TODO: implement
-		//iRet = checkResultBulkmode(pData, root);
+		iRet = checkResultBulkmode(pData, root);
 	} else {
 		ok = cJSON_GetObjectItem(root, "ok");
 		if(ok == NULL || ok->type != cJSON_True) {
