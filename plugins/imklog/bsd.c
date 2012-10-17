51754401f72376ea5f1c5b74b9c1772ab6f7ae4f
Marius Tomaschewski <mt@suse.de>
2012-10-17 11:55:34 +0200

    imklog: use memmove to remove kernel timestamp

--- plugins/imklog/bsd.c
+++ plugins/imklog/bsd.c
@@ -120,7 +120,7 @@ submitSyslog(int pri, uchar *buf)
 	/* we have a timestamp */
 	DBGPRINTF("kernel timestamp is %ld %ld\n", secs, nsecs);
 	bufsize= strlen((char*)buf);
-	memcpy(buf+3, buf+i, bufsize - i + 1);
+	memmove(buf+3, buf+i, bufsize - i + 1);
 
 	clock_gettime(CLOCK_MONOTONIC, &monotonic);
 	clock_gettime(CLOCK_REALTIME, &realtime);
