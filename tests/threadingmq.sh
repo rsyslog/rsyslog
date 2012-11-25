7893c9b19d8f13a8037c6be4ebc2052701d3af37
Rainer Gerhards <rgerhards@adiscon.com>
2012-11-25 17:03:19 +0100

    prepare 7.3.4 release

--- tests/threadingmq.sh
+++ tests/threadingmq.sh
@@ -11,6 +11,9 @@ source $srcdir/diag.sh init
 source $srcdir/diag.sh startup threadingmq.conf
 source $srcdir/diag.sh injectmsg 0 100000
 source $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
+# we give an extra seconds for things to settle, especially
+# important on slower test machines
+./msleep 1000
 source $srcdir/diag.sh wait-shutdown
 source $srcdir/diag.sh seq-check 0 99999
 source $srcdir/diag.sh exit
