98745101a103d1aef7a6c79cec8708729a2895fa
Rainer Gerhards <rgerhards@adiscon.com>
2013-09-18 10:10:56 +0200

    bugfix: imfile "facility" input parameter improperly handled
    caused facility not to be set, and severity to be overwritten with
    the facility value.
    Thanks to forum user dmunny for reporting this bug.

--- plugins/imfile/imfile.c
+++ plugins/imfile/imfile.c
@@ -473,7 +473,7 @@ CODESTARTnewInpInst
 		} else if(!strcmp(inppblk.descr[i].name, "severity")) {
 			inst->iSeverity = pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "facility")) {
-			inst->iSeverity = pvals[i].val.d.n;
+			inst->iFacility = pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "readmode")) {
 			inst->readMode = pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "maxlinesatonce")) {
