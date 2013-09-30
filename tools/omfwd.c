4b4ae57f8bf1e2c59d8887219648dcecc5ca2c1f
Rainer Gerhards <rgerhards@adiscon.com>
2013-09-30 10:07:18 +0200

    nit: remove already done TODO comment

--- tools/omfwd.c
+++ tools/omfwd.c
@@ -21,9 +21,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * TODO v6 config:
- * - permitted peer *list*
  */
 #include "config.h"
 #include "rsyslog.h"
