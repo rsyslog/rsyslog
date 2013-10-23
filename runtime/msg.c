a8ac45583ec470acc68f140bb68f4ec7d3ccac55
Rainer Gerhards <rgerhards@adiscon.com>
2013-10-23 18:34:19 +0200

    (temporarily?) re-enable global vars in string templates

--- runtime/msg.c
+++ runtime/msg.c
@@ -540,6 +540,8 @@ propNameToID(uchar *pName, propid_t *pPropID)
 		*pPropID = PROP_CEE;
 	} else if(!strncmp((char*) pName, "$.", 2) || pName[0] == '.') {
 		*pPropID = PROP_LOCAL_VAR;
+	} else if(!strncmp((char*) pName, "$/", 2) || pName[0] == '/') {
+		*pPropID = PROP_GLOBAL_VAR;
 	} else {
 		DBGPRINTF("PROP_INVALID for name '%s'\n", pName);
 		*pPropID = PROP_INVALID;
