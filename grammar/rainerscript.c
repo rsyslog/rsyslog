5b9a6992f73bbbba847402946443b001f8b639df
Rainer Gerhards <rgerhards@adiscon.com>
2013-10-25 17:01:42 +0200

    bugfix: memory leak in strlen() RainerScript function
    Thanks to Gregoire Seux for reportig this bug.
    closes: http://bugzilla.adiscon.com/show_bug.cgi?id=486

--- grammar/rainerscript.c
+++ grammar/rainerscript.c
@@ -1359,6 +1359,7 @@ doFuncCall(struct cnffunc *func, struct var *ret, void* usrptr)
 			estr = var2String(&r[0], &bMustFree);
 			ret->d.n = es_strlen(estr);
 			if(bMustFree) es_deleteStr(estr);
+			if(r[0].datatype == 'S') es_deleteStr(r[0].d.estr);
 		}
 		ret->datatype = 'N';
 		break;
