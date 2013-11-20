2c3bce4e31608572b8d469237c086da6656afa8a
Rainer Gerhards <rgerhards@adiscon.com>
2013-11-20 14:14:46 +0100

    testbench: improve test

--- tests/rs_optimizer_pri.sh
+++ tests/rs_optimizer_pri.sh
@@ -9,7 +9,8 @@ echo \[rs_optimizer_pri.sh\]: testing RainerScript PRI optimizer
 source $srcdir/diag.sh init
 source $srcdir/diag.sh startup rs_optimizer_pri.conf
 sleep 1
-source $srcdir/diag.sh injectmsg 0 100
+source $srcdir/diag.sh tcpflood -m100 # correct facility
+source $srcdir/diag.sh tcpflood -m100 -P175 # incorrect facility --> must be ignored
 source $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
 source $srcdir/diag.sh wait-shutdown
 source $srcdir/diag.sh seq-check 0 99
