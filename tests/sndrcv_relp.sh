ed27d5c764453e3d665df841088c0e8488ef1260
Rainer Gerhards <rgerhards@adiscon.com>
2013-12-10 17:04:58 +0100

    testbench: add test for im/omrelp

---
+++ tests/sndrcv_relp.sh
@@ -0,0 +1,5 @@
+# added 2013-12-10 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+echo ===============================================================================
+echo \[sndrcv_relp.sh\]: testing sending and receiving via relp
+source $srcdir/sndrcv_drvr.sh sndrcv_relp 50000
