01e98f3934a2557717535a602e839f8842731cd4
Rainer Gerhards <rgerhards@adiscon.com>
2014-01-24 11:29:42 +0100

    python skeleton: add ability to specify max batch size; add license

--- plugins/external/skeletons/python/plugin.py
+++ plugins/external/skeletons/python/plugin.py
@@ -1,13 +1,33 @@
 #! /usr/bin/python
 
 """A skeleton for a python rsyslog output plugin
-"""
+   Copyright (C) 2014 by Adiscon GmbH
 
-pollPeriod = 0.75 # the number of seconds between polling for new messages
+   This file is part of rsyslog.
+  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+         http://www.apache.org/licenses/LICENSE-2.0
+         -or-
+         see COPYING.ASL20 in the source distribution
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 import sys
 import select
 
+# skeleton config parameters
+pollPeriod = 0.75 # the number of seconds between polling for new messages
+maxAtOnce = 1024  # max nbr of messages that are processed within one batch
+
+# App logic global variables
 outfile = "" # "define" global var that the app code needs
 
 def onInit():
@@ -50,13 +70,16 @@ keepRunning = 1
 while keepRunning == 1:
 	msgs = []
 	while keepRunning and sys.stdin in select.select([sys.stdin], [], [], pollPeriod)[0]:
+	        msgsInBatch = 0
 		while keepRunning and sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
 			line = sys.stdin.readline()
 			if line:
 				msgs.append(line)
 			else: # an empty line means stdin has been closed
-				print('eof')
 				keepRunning = 0
+			msgsInBatch = msgsInBatch + 1
+			if msgsInBatch >= maxAtOnce:
+				break;
 		if len(msgs) > 0:
 			onReceive(msgs)
 onExit()
