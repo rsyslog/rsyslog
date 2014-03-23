1ecec05fb158c8aa58feb5ceaedcc1aeabca7a9b
Rainer Gerhards <rgerhards@adiscon.com>
2014-03-23 11:35:04 +0100

    report error when preparing for non-anon TLS and this is unsupported
    The error is reported when the processes own certificate is set,
    which also turns off anon TLS. We now check if TLS AUTH is supported
    and, if not, return an error. This enables better error tracking
    in the client, as otherwise the problem is reported much later
    or even not at all (in the case of a relp server).

--- src/tcp.c
+++ src/tcp.c
@@ -482,8 +482,12 @@ relpTcpSetPrivKey(relpTcp_t *pThis, char *cert)
 	if(cert == NULL) {
 		pThis->privKeyFile = NULL;
 	} else {
-		if((pThis->privKeyFile = strdup(cert)) == NULL)
-			ABORT_FINALIZE(RELP_RET_OUT_OF_MEMORY);
+#		ifdef HAVE_GNUTLS_CERTIFICATE_SET_VERIFY_FUNCTION  
+			if((pThis->privKeyFile = strdup(cert)) == NULL)
+				ABORT_FINALIZE(RELP_RET_OUT_OF_MEMORY);
+#		else
+			ABORT_FINALIZE(RELP_RET_ERR_NO_TLS_AUTH);
+#		endif
 	}
 finalize_it:
 	LEAVE_RELPFUNC;
