094c9e9626884400423c3da220b6f7f8115e3405
Rainer Gerhards <rgerhards@adiscon.com>
2014-03-25 15:24:49 +0100

    bugfix: ompipe used invalid default template
    This is a regression from an old change (didn't track it down precisely,
    but over a year ago). It used the Forwarding template instead of
    the file template (so we have a full syslog header). This fix corrects
    it back to previous behaviour, but new scripts that used the wrong
    format may now need to have the RSYSLOG_ForwardingFormat template
    explicitely be applied.
    closes: https://github.com/rsyslog/rsyslog/issues/50

--- tools/ompipe.c
+++ tools/ompipe.c
@@ -353,17 +353,8 @@ CODESTARTnewActInst
 	}
 
 	CHKiRet(OMSRsetEntry(*ppOMSR, 0, (uchar*)strdup((pData->tplName == NULL) ? 
-						"RSYSLOG_ForwardFormat" : (char*)pData->tplName),
+						"RSYSLOG_FileFormat" : (char*)pData->tplName),
 						OMSR_NO_RQD_TPL_OPTS));
-	/* Old flawed template code
-	if(pData->tplName == NULL) {
-		CHKiRet(OMSRsetEntry(*ppOMSR, 0, (uchar*) "RSYSLOG_FileFormat",
-			OMSR_NO_RQD_TPL_OPTS));
-	} else {
-		CHKiRet(OMSRsetEntry(*ppOMSR, 0,
-			(uchar*) strdup((char*) pData->tplName),
-			OMSR_NO_RQD_TPL_OPTS));
-	}*/
 CODE_STD_FINALIZERnewActInst
 	cnfparamvalsDestruct(pvals, &actpblk);
 ENDnewActInst
