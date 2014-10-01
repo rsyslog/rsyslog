4ee5c784982b122a7314ae5f0e18265fa97ba777
Rainer Gerhards <rgerhards@adiscon.com>
2014-10-01 12:27:29 +0200

    testbench: add invalid facility tests

--- tests/fac_invld4_rfc5424.sh
+++ tests/fac_invld4_rfc5424.sh
@@ -3,7 +3,7 @@
 # This file is part of the rsyslog project, released under ASL 2.0
 source $srcdir/diag.sh init
 source $srcdir/diag.sh startup fac_invld4_rfc5424.conf
-source $srcdir/diag.sh tcpflood -y -m1000 -P 1011
+source $srcdir/diag.sh tcpflood -y -m1000 -P 8000000000000000000000000000000
 source $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
 source $srcdir/diag.sh wait-shutdown       # and wait for it to terminate
 source $srcdir/diag.sh seq-check 0 999 
