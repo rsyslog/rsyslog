7a5df2a25a8f6d9ed8c73a2926e165951d9f0d56
Tomas Heinrich <theinric@redhat.com>
2014-10-14 17:31:53 +0200

    Make liblogging-stdlog optional

--- runtime/glbl.c
+++ runtime/glbl.c
@@ -888,6 +888,15 @@ glblProcessCnf(struct cnfobj *o)
 			continue;
 		if(!strcmp(paramblk.descr[i].name, "processinternalmessages")) {
 			bProcessInternalMessages = (int) cnfparamvals[i].val.d.n;
+#ifndef HAVE_LIBLOGGING_STDLOG
+			if(bProcessInternalMessages != 1) {
+				bProcessInternalMessages = 1;
+				errmsg.LogError(0, RS_RET_ERR, "rsyslog wasn't "
+					"compiled with liblogging-stdlog support. "
+					"The 'ProcessInternalMessages' parameter "
+					"is ignored.\n");
+			}
+#endif
 		} else if(!strcmp(paramblk.descr[i].name, "stdlog.channelspec")) {
 			stdlog_chanspec = (uchar*)
 				es_str2cstr(cnfparamvals[i].val.d.estr, NULL);
