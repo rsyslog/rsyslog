9bda7c84ec1921d04acac1a3741facd75c2aa388
Rainer Gerhards <rgerhards@adiscon.com>
2014-11-05 17:36:11 +0100

    testbench: add UDP send/receive test with RainerScript config format

---
+++ tests/sndrcv_udp_nonstdpt_v6.sh
@@ -0,0 +1,5 @@
+# added 2014-11-05 by Rgerhards
+# This file is part of the rsyslog project, released  under ASL 2.0
+echo ===============================================================================
+echo \[sndrcv_udp_nonstdpt_v6.sh\]: testing sending and receiving via udp
+source $srcdir/sndrcv_drvr.sh sndrcv_udp_nonstdpt_v6 500
