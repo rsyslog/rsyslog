bbb793d140913b1633083e99cf64167d1ac4fc9e
Rainer Gerhards <rgerhards@adiscon.com>
2014-12-04 12:24:43 +0100

    testbench: improve imuxsock coverage
    so far, all test required root privileges, which made testing
    quite complicated and resulted in no testing of this module
    on the testbench machines at all. Now we can also run without
    root privileges.
    
    To support the new tests, test tools were also improved.

--- tests/syslog_caller.c
+++ tests/syslog_caller.c
@@ -1,40 +1,42 @@
-/* A very primitive testing tool that just emits a number of
- * messages to the system log socket. Currently sufficient, but
- * obviously room for improvement. 
- *
- * It is highly suggested NOT to "base" any derivative work
- * on this tool ;)
+/* A testing tool that just emits a number of
+ * messages to the system log socket.
  *
  * Options
  *
  * -s severity (0..7 accoding to syslog spec, r "rolling", default 6)
  * -m number of messages to generate (default 500)
+ * -C liblognorm-stdlog channel description
+ * -f message format to use
  *
  * Part of the testbench for rsyslog.
  *
- * Copyright 2010 Rainer Gerhards and Adiscon GmbH.
+ * Copyright 2010-2014 Rainer Gerhards and Adiscon GmbH.
  *
  * This file is part of rsyslog.
  *
- * Rsyslog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Rsyslog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Rsyslog.  If not, see <http://www.gnu.org/licenses/>.
- *
- * A copy of the GPL can be found in the file "COPYING" in this distribution.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *       -or-
+ *       see COPYING.ASL20 in the source distribution
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <syslog.h>
+#include <errno.h>
+#include <string.h>
+#include <liblogging/stdlog.h>
+
+static enum { FMT_NATIVE, FMT_SYSLOG_INJECT_L, FMT_SYSLOG_INJECT_C
+	} fmt = FMT_NATIVE;
 
 static void usage(void)
 {
@@ -43,6 +45,23 @@ static void usage(void)
 }
 
 
+/* buffer must be large "enough" [4K?] */
+static void
+genMsg(char *buf, const int sev, const int iRun)
+{
+	switch(fmt) {
+	case FMT_NATIVE:
+		sprintf(buf, "test message nbr %d, severity=%d", iRun, sev);
+		break;
+	case FMT_SYSLOG_INJECT_L:
+		sprintf(buf, "test\n");
+		break;
+	case FMT_SYSLOG_INJECT_C:
+		sprintf(buf, "test 1\t2");
+		break;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -50,26 +69,49 @@ int main(int argc, char *argv[])
 	int bRollingSev = 0;
 	int sev = 6;
 	int msgs = 500;
+	stdlog_channel_t logchan = NULL;
+	const char *chandesc = "syslog:";
+	char msgbuf[4096];
 
-	while((opt = getopt(argc, argv, "m:s:")) != -1) {
+	stdlog_init(STDLOG_USE_DFLT_OPTS);
+	while((opt = getopt(argc, argv, "m:s:C:f:")) != -1) {
 		switch (opt) {
 		case 's':	if(*optarg == 'r') {
 					bRollingSev = 1;
 					sev = 0;
 				} else
-					sev = atoi(optarg);
+					sev = atoi(optarg) % 8;
 				break;
 		case 'm':	msgs = atoi(optarg);
 				break;
+		case 'C':	chandesc = optarg;
+				break;
+		case 'f':	if(!strcmp(optarg, "syslog_inject-l"))
+					fmt = FMT_SYSLOG_INJECT_L;
+				else if(!strcmp(optarg, "syslog_inject-c"))
+					fmt = FMT_SYSLOG_INJECT_C;
+				else
+					usage();
+				break;
 		default:	usage();
+				exit(1);
 				break;
 		}
 	}
 
+	if((logchan = stdlog_open(argv[0], 0, STDLOG_LOCAL1, chandesc)) == NULL) {
+		fprintf(stderr, "error opening logchannel '%s': %s\n",
+			chandesc, strerror(errno));
+		exit(1);
+	}
 	for(i = 0 ; i < msgs ; ++i) {
-		syslog(sev % 8, "test message nbr %d, severity=%d", i, sev % 8);
+		genMsg(msgbuf, sev, i);
+		if(stdlog_log(logchan, sev, "%s", msgbuf) != 0) {
+			perror("error writing log record");
+			exit(1);
+		}
 		if(bRollingSev)
-			sev++;
+			sev = (sev + 1) % 8;
 	}
 	return(0);
 }
