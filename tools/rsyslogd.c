4e6f06a863e3a405444855c79f391d5195c5a2e6
Rainer Gerhards <rgerhards@adiscon.com>
2015-01-09 10:43:13 +0100

    cosmetic: silence compiler warning
    The warning was invalid, as parentPipeFD was initialized under the
    same condition that guarded it's use.

--- tools/rsyslogd.c
+++ tools/rsyslogd.c
@@ -3,7 +3,7 @@
  * because it was either written from scratch by me (rgerhards) or
  * contributors who agreed to ASL 2.0.
  *
- * Copyright 2004-2014 Rainer Gerhards and Adiscon
+ * Copyright 2004-2015 Rainer Gerhards and Adiscon
  *
  * This file is part of rsyslog.
  *
@@ -1028,7 +1028,7 @@ initAll(int argc, char **argv)
 	int bChDirRoot = 1; /* change the current working directory to "/"? */
 	char *arg;	/* for command line option processing */
 	char cwdbuf[128]; /* buffer to obtain/display current working directory */
-	int parentPipeFD; /* fd of pipe to parent, if auto-backgrounding */
+	int parentPipeFD = 0; /* fd of pipe to parent, if auto-backgrounding */
 	DEFiRet;
 
 	/* first, parse the command line options. We do not carry out any actual work, just
