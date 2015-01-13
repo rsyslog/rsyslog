7c097d363a62e1ed8f1a9e38326aa348eaed9d85
Rainer Gerhards <rgerhards@adiscon.com>
2015-01-13 09:54:41 +0100

    bugfixes for maxMessageSize global parameter
    closely related, thus done in a single commit:
    
    - bugfix: invalid data size for iMaxLine global property
      It was defined as int, but inside the config system it was declared as
      size type, which uses int64_t. With legacy config statements, this could
      lead to misadressing, which usually meant the another config variable was
      overwritten (depending on memory layout).
      closes https://github.com/rsyslog/rsyslog/issues/205
    
    - bugfix: negative values for maxMessageSize global parameter were permitted

--- runtime/glbl.c
+++ runtime/glbl.c
@@ -7,7 +7,7 @@
  *
  * Module begun 2008-04-16 by Rainer Gerhards
  *
- * Copyright 2008-2014 Rainer Gerhards and Adiscon GmbH.
+ * Copyright 2008-2015 Rainer Gerhards and Adiscon GmbH.
  *
  * This file is part of the rsyslog runtime library.
  *
@@ -177,6 +177,12 @@ static struct cnfparamvals *cnfparamvals = NULL;
  * each time a new config load begins (TODO: create interface?)
  */
 
+static int
+GetMaxLine(void)
+{
+	return(iMaxLine);
+}
+
 /* define a macro for the simple properties' set and get functions
  * (which are always the same). This is only suitable for pretty
  * simple cases which require neither checks nor memory allocation.
@@ -199,7 +205,6 @@ static dataType Get##nameFunc(void) \
 SIMP_PROP(OptimizeUniProc, bOptimizeUniProc, int)
 SIMP_PROP(PreserveFQDN, bPreserveFQDN, int)
 SIMP_PROP(mainqCnfObj, mainqCnfObj, struct cnfobj *)
-SIMP_PROP(MaxLine, iMaxLine, int)
 SIMP_PROP(DropMalPTRMsgs, bDropMalPTRMsgs, int)
 SIMP_PROP(StripDomains, StripDomains, char**)
 SIMP_PROP(LocalHosts, LocalHosts, char**)
@@ -346,6 +351,32 @@ finalize_it:
 	RETiRet;
 }
 
+
+/* This function is used both by legacy and RainerScript conf. It is a real setter. */
+static rsRetVal
+setMaxLine(const int64_t iNew)
+{
+	if(iNew < 128) {
+		errmsg.LogError(0, RS_RET_INVALID_VALUE, "maxMessageSize tried to set "
+				"to %lld, but cannot be less than 128 - set to 128 "
+				"instead", (long long) iNew);
+		iMaxLine = 128;
+	} else if(iNew > (int64_t) INT_MAX) {
+		errmsg.LogError(0, RS_RET_INVALID_VALUE, "maxMessageSize larger than "
+				"INT_MAX (%d) - reduced to INT_MAX", INT_MAX);
+		iMaxLine = INT_MAX;
+	} else {
+		iMaxLine = (int) iNew;
+	}
+}
+
+static rsRetVal
+legacySetMaxMessageSize(void __attribute__((unused)) *pVal, int64_t iNew)
+{
+	DEFiRet;
+	iRet = setMaxLine(iNew);
+	RETiRet;
+}
 
 static rsRetVal
 setDebugFile(void __attribute__((unused)) *pVal, uchar *pNewVal)
@@ -673,6 +704,7 @@ CODESTARTobjQueryInterface(glbl)
 	pIf->GetDefPFFamily = getDefPFFamily;
 	pIf->SetDisableDNS = setDisableDNS;
 	pIf->GetDisableDNS = getDisableDNS;
+	pIf->GetMaxLine = GetMaxLine;
 	pIf->SetOption_DisallowWarning = setOption_DisallowWarning;
 	pIf->GetOption_DisallowWarning = getOption_DisallowWarning;
 	pIf->SetParseHOSTNAMEandTAG = setParseHOSTNAMEandTAG;
@@ -680,7 +712,6 @@ CODESTARTobjQueryInterface(glbl)
 #define SIMP_PROP(name) \
 	pIf->Get##name = Get##name; \
 	pIf->Set##name = Set##name;
-	SIMP_PROP(MaxLine);
 	SIMP_PROP(OptimizeUniProc);
 	SIMP_PROP(PreserveFQDN);
 	SIMP_PROP(DropMalPTRMsgs);
@@ -707,7 +738,6 @@ CODESTARTobjQueryInterface(glbl)
 #undef	SIMP_PROP
 finalize_it:
 ENDobjQueryInterface(glbl)
-
 
 /* Reset config variables to default values.
  * rgerhards, 2008-04-17
@@ -1006,7 +1036,7 @@ glblDoneLoadCnf(void)
 		} else if(!strcmp(paramblk.descr[i].name, "action.reportsuspensioncontinuation")) {
 			bActionReportSuspensionCont = (int) cnfparamvals[i].val.d.n;
 		} else if(!strcmp(paramblk.descr[i].name, "maxmessagesize")) {
-			iMaxLine = (int) cnfparamvals[i].val.d.n;
+			setMaxLine(cnfparamvals[i].val.d.n);
 		} else if(!strcmp(paramblk.descr[i].name, "debug.onshutdown")) {
 			glblDebugOnShutdown = (int) cnfparamvals[i].val.d.n;
 			errmsg.LogError(0, RS_RET_OK, "debug: onShutdown set to %d", glblDebugOnShutdown);
@@ -1094,7 +1124,7 @@ BEGINAbstractObjClassInit(glbl, 1, OBJ_IS_CORE_MODULE) /* class, version */
 	CHKiRet(regCfSysLineHdlr((uchar *)"localhostipif", 0, eCmdHdlrGetWord, setLocalHostIPIF, NULL, NULL));
 	CHKiRet(regCfSysLineHdlr((uchar *)"optimizeforuniprocessor", 0, eCmdHdlrBinary, NULL, &bOptimizeUniProc, NULL));
 	CHKiRet(regCfSysLineHdlr((uchar *)"preservefqdn", 0, eCmdHdlrBinary, NULL, &bPreserveFQDN, NULL));
-	CHKiRet(regCfSysLineHdlr((uchar *)"maxmessagesize", 0, eCmdHdlrSize, NULL, &iMaxLine, NULL));
+	CHKiRet(regCfSysLineHdlr((uchar *)"maxmessagesize", 0, eCmdHdlrSize, legacySetMaxMessageSize, NULL, NULL));
 
 	/* Deprecated parser config options */
 	CHKiRet(regCfSysLineHdlr((uchar *)"controlcharacterescapeprefix", 0, eCmdHdlrGetChar, NULL, &cCCEscapeChar, NULL));
