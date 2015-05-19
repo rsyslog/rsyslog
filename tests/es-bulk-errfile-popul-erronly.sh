aad0ac85fdea372ac2a04717b922acad34d6bdff
Tim Eifler <teifler@adiscon.com>
2015-05-20 00:23:49 +0200

    Shortening names of elasticsearch tests

--- tests/es-bulk-errfile-popul-erronly.sh
+++ tests/es-bulk-errfile-popul-erronly.sh
@@ -1,18 +1,18 @@
 # This file is part of the rsyslog project, released under ASL 2.0
 echo ===============================================================================
-echo \[elasticsearch-bulk-errorfile-populated\]: basic test for elasticsearch functionality
+echo \[es-bulk-errfile-popul-erronly\]: basic test for elasticsearch functionality
 source $srcdir/diag.sh init
 source $srcdir/diag.sh es-init
 echo '{ "name" : "foo" }
 {"name": bar"}
 {"name": "baz"}
 {"name": foz"}' > inESData.inputfile
-source $srcdir/diag.sh startup elasticsearch-bulk-errorfile-populated-default-format.conf
+source $srcdir/diag.sh startup es-bulk-errfile-popul-erronly.conf
 source $srcdir/diag.sh shutdown-when-empty
 source $srcdir/diag.sh wait-shutdown
 rm -f inESData.inputfile
 
-python $srcdir/elasticsearch-error-format-check.py default
+python $srcdir/elasticsearch-error-format-check.py erroronly
 
 if [ $? -ne 0 ]
 then
