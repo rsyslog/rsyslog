67b02c17f1138094b3b811263c2324f066c6fdf7
Rainer Gerhards <rgerhards@adiscon.com>
2015-11-24 15:54:13 +0100

    testbench: fix test which used valgrind even if forbidden
    Some tests used valgrind explicitely even when the configure
    option forbid it's use in the testbench. This breaks, for example,
    testbench runs with clang's address sanitizer.

--- tests/json_null_array-vg.sh
+++ tests/json_null_array-vg.sh
@@ -4,11 +4,11 @@
 echo ===============================================================================
 echo \[json_null_array.sh\]: test for json containung \"null\" value
 . $srcdir/diag.sh init
-. $srcdir/diag.sh startup json_null_array.conf
+. $srcdir/diag.sh startup-vg json_null_array.conf
 . $srcdir/diag.sh tcpflood -m 1 -M "\"<167>Mar  6 16:57:54 172.20.245.8 test: @cee: { \\\"array\\\": [0, 1, null, 2, 3, null, 4] }\""
 echo doing shutdown
 . $srcdir/diag.sh shutdown-when-empty
 echo wait on shutdown
-. $srcdir/diag.sh wait-shutdown
+. $srcdir/diag.sh wait-shutdown-vg
 . $srcdir/diag.sh seq-check 0 4
 . $srcdir/diag.sh exit
