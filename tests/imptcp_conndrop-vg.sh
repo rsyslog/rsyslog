d782c9108b4b37d244fc9d79d4f46a4a2be9a28a
Andre Lorbach <alorbach@adiscon.com>
2015-12-01 15:26:26 +0100

    testsuite: Set sleeptime to a higher value in imptcp conndrop tests
    Using 10 seconds now like in imtcp tests. Otherwise a false fail
    could be generated if the machine was to slow.
    
    closes https://github.com/rsyslog/rsyslog/issues/263

--- tests/imptcp_conndrop-vg.sh
+++ tests/imptcp_conndrop-vg.sh
@@ -7,7 +7,7 @@ echo TEST: \[imptcp_conndrop-vg.sh\]: test imptcp with random connection drops
 . $srcdir/diag.sh startup-vg imptcp_conndrop.conf
 # 100 byte messages to gain more practical data use
 . $srcdir/diag.sh tcpflood -c20 -m50000 -r -d100 -P129 -D
-sleep 4 # due to large messages, we need this time for the tcp receiver to settle...
+sleep 10 # due to large messages, we need this time for the tcp receiver to settle...
 . $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
 . $srcdir/diag.sh wait-shutdown-vg    # and wait for it to terminate
 . $srcdir/diag.sh check-exit-vg
