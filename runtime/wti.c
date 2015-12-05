cb93b5e7d85990cf4390bdc18809380af93fd140
Rainer Gerhards <rgerhards@adiscon.com>
2015-12-05 18:23:30 +0100

    nitfix: error status was not correctly passed
    but it was extermely unlikely that this could ever happen in practice
    
    detected by clang static analyzer

--- runtime/wti.c
+++ runtime/wti.c
@@ -470,7 +470,7 @@ BEGINObjClassInit(wti, 1, OBJ_IS_CORE_MODULE) /* one is the object version (most
 	r = pthread_key_create(&thrd_wti_key, NULL);
 	if(r != 0) {
 		dbgprintf("wti.c: pthread_key_create failed\n");
-		iRet = RS_RET_ERR;
+		ABORT_FINALIZE(RS_RET_ERR);
 	}
 ENDObjClassInit(wti)
 
