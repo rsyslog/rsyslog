601375fe656c1e3c30e0e5670dca71eb175363c0
Rainer Gerhards <rgerhards@adiscon.com>
2015-12-08 10:05:27 +0100

    mmgrok: fix too-large malloc

--- contrib/mmgrok/mmgrok.c
+++ contrib/mmgrok/mmgrok.c
@@ -218,9 +218,9 @@ msg_to_json(GList *list,instanceData *pData)
     }
     for(;it;it= it->next)
     {
-        char  *key = (char *)malloc(sizeof(char *)*((result_t *)it->data)->key_len);
+        char  *key = (char *)malloc(sizeof(char)*((result_t *)it->data)->key_len+1);
         sprintf(key,"%.*s",((result_t *)it->data)->key_len,((result_t *)it->data)->key);
-	char  *value = (char *)malloc(sizeof(char *)*((result_t *)it->data)->value_len);
+	char  *value = (char *)malloc(sizeof(char)*((result_t *)it->data)->value_len+1);
         sprintf(value,"%.*s", ((result_t*)it->data)->value_len,((result_t*)it->data)->value);	
 	jval = json_object_new_string(value);
 	json_object_object_add(json,key,jval);
