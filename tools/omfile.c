6c97d17239617906b432b7fdf322a7219f1e6fa4
Rainer Gerhards <rgerhards@adiscon.com>
2015-12-13 10:32:37 +0100

    refactor
    for discussion, see also https://github.com/rsyslog/rsyslog/pull/684

--- tools/omfile.c
+++ tools/omfile.c
@@ -409,7 +409,7 @@ static rsRetVal cflineParseOutchannel(instanceData *pData, uchar* p, omodStringR
 	i = 0;
 	/* get outchannel name */
 	while(*p && *p != ';' && *p != ' ' &&
-	      i < ((sizeof(szBuf) / sizeof(uchar)) - sizeof(uchar))) {
+	      i < (sizeof(szBuf) - 1) ) {
 	      szBuf[i++] = *p++;
 	}
 	szBuf[i] = '\0';
