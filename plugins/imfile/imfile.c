eddf173e53f8b883c55869515645903bc936442e
Zhiyong Zhao <zhaozhiyong@jd.com>
2016-01-11 15:17:19 +0800

    Handling the condition of inotify instance's failed initialization

--- plugins/imfile/imfile.c
+++ plugins/imfile/imfile.c
@@ -1798,6 +1798,10 @@ do_inotify()
 	CHKiRet(wdmapInit());
 	CHKiRet(dirsInit());
 	ino_fd = inotify_init();
+        if(ino_fd < 0) {
+            errmsg.LogError(1, RS_RET_INOTIFY_INIT_FAILED, "imfile: Init inotify instance failed ");
+            return RS_RET_INOTIFY_INIT_FAILED;
+        }
 	DBGPRINTF("imfile: inotify fd %d\n", ino_fd);
 	in_setupInitialWatches();
 
