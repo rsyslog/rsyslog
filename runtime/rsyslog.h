0e17c41aa57d8bc44e393fdc2370f1719291cf0c
Rainer Gerhards <rgerhards@adiscon.com>
2016-02-16 12:55:11 +0100

    imfile: fix race during startup that could lead to some duplication
    If a to-be-monitored file was created after inotify was initialized
    but before startup was completed, the first chunk of data from this
    file could be duplicated. This should have happened very rarely in
    practice, but caused occasional testbench failures.
    
    closes https://github.com/rsyslog/rsyslog/issues/791

--- runtime/rsyslog.h
+++ runtime/rsyslog.h
@@ -446,6 +446,7 @@ enum rsRetVal_				/** return value. All methods return this if not specified oth
 	RS_RET_CONC_CTRL_ERR = -2428,/**< error in lock/unlock/condition/concurrent-modification operation */
 	RS_RET_SENDER_GONE_AWAY = -2429,/**< warning: sender not seen for configured amount of time */
 	RS_RET_SENDER_APPEARED = -2430,/**< info: new sender appeared */
+	RS_RET_FILE_ALREADY_IN_TABLE = -2431,/**< in imfile: table already contains to be added file */
 
 	/* RainerScript error messages (range 1000.. 1999) */
 	RS_RET_SYSVAR_NOT_FOUND = 1001, /**< system variable could not be found (maybe misspelled) */
