0e2343000d8cd1d34a44c8a212f146e7b14e1e75
Andre Lorbach <alorbach@adiscon.com>
2016-02-24 14:57:31 +0100

    testbench: Initial added rsgtutil tests using KSI to verify signed sample logdata

---
+++ tests/ksi-verify-long-vg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# rsgtutil configuration test 
+#	Verifies sample logdata against public ksi verification repository. 
+#
+# This is part of the rsyslog testbench, licensed under GPLv3
+#
+# Copyright 2016 Rainer Gerhards and Adiscon GmbH.
+RSYSLOG_KSI_BIN="http://verify.guardtime.com/ksi-publications.bin"
+
+echo \[ksi-verify-long.sh\]: testing rsgtutil verify with valgrind - long options
+. $srcdir/diag.sh init
+
+echo "running rsgtutil command with short options"
+valgrind $RS_TESTBENCH_VALGRIND_EXTRA_OPTS --log-fd=1 --error-exitcode=10 --malloc-fill=ff --free-fill=fe --leak-check=full ../tools/rsgtutil -t -s --publications-server $RSYSLOG_KSI_BIN $srcdir/testsuites/ksi-sample.log #> rsgtutil.out.log 2>&1
+
+RSYSLOGD_EXIT=$?
+if [ "$RSYSLOGD_EXIT" -ne "0" ]; then
+	echo "rsgtutil returned error: " $RSYSLOGD_EXIT
+	exit 1;
+fi
+
+# Cleanup temp files
+rm -f rsgtutil.out*.log 
+
+echo SUCCESS: rsgtutil verify function with valgrind- long options
---
+++ tests/ksi-verify-short.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# rsgtutil configuration test 
+#	Verifies sample logdata against public ksi verification repository. 
+#
+# This is part of the rsyslog testbench, licensed under GPLv3
+#
+# Copyright 2016 Rainer Gerhards and Adiscon GmbH.
+RSYSLOG_KSI_BIN="http://verify.guardtime.com/ksi-publications.bin"
+
+echo \[ksi-verify-short.sh\]: testing rsgtutil verify function - short options
+. $srcdir/diag.sh init
+
+echo "running rsgtutil command with short options"
+../tools/rsgtutil -t -s -P $RSYSLOG_KSI_BIN $srcdir/testsuites/ksi-sample.log #> rsgtutil.out.log 2>&1
+
+RSYSLOGD_EXIT=$?
+if [ "$RSYSLOGD_EXIT" -ne "0" ]; then
+	echo "rsgtutil returned error: " $RSYSLOGD_EXIT
+	exit 1;
+fi
+
+# Cleanup temp files
+rm -f rsgtutil.out*.log 
+
+echo SUCCESS: rsgtutil verify function - short options
