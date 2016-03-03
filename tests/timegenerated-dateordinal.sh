b8b4191873b9acd154e0417b37de62a85e8da148
Rainer Gerhards <rgerhards@adiscon.com>
2016-03-03 11:47:47 +0100

    testbench: add tests for date-ordinal formatting flag
    closes https://github.com/rsyslog/rsyslog/issues/835

---
+++ tests/timegenerated-dateordinal.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+# test many concurrent tcp connections
+# addd 2016-03-02 by RGerhards, released under ASL 2.0
+# Note: we run several subtests here in order to save us
+# from creating additional tests
+# requires faketime
+echo \[timegenerated-uxtimestamp\]: 
+export TZ=UTC+00:00
+faketime -f '2016-03-01 12:00:00' date
+if [ $? -ne 0 ]; then
+    echo "faketime command missing, skipping test"
+    exit 77
+fi
+. $srcdir/diag.sh init
+. $srcdir/diag.sh generate-conf
+. $srcdir/diag.sh add-conf '
+$ModLoad ../plugins/imtcp/.libs/imtcp
+$InputTCPServerRun 13514
+
+template(name="outfmt" type="string"
+	 string="%timegenerated:::date-ordinal%\n")
+:msg, contains, "msgnum:" action(type="omfile" template="outfmt"
+			         file="rsyslog.out.log")
+'
+
+
+echo "***SUBTEST: check 1970-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '1970-01-01 00:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2000-03-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2000-03-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "061" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2016-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2016-01-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2016-02-29"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2016-02-29 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "060" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2016-03-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2016-03-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "061" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2016-03-03"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2016-03-03 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "063" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2016-12-31"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2016-12-31 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "366" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2017-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2017-01-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2020-03-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2020-03-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "061" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2038-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2038-01-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2038-12-31"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2038-12-31 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "365" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2040-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2040-01-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2040-12-31"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2040-12-31 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "366" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+echo "***SUBTEST: check 2100-01-01"
+rm -f rsyslog.out.log	# do cleanup of previous subtest
+faketime -f '2100-01-01 12:00:00' $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo "001" | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid timestamps generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  exit 1
+fi;
+
+
+. $srcdir/diag.sh exit
