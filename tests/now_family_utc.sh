bfd4583b8cb0e168a6ad63388efeac12b7a52f3e
Thomas D <whissi@whissi.de>
2016-03-11 17:07:08 +0100

    testbench: Run year-2038 tests only on systems which supports those dates
    Fixes: https://github.com/rsyslog/rsyslog/issues/873

--- tests/now_family_utc.sh
+++ tests/now_family_utc.sh
@@ -3,12 +3,11 @@
 # addd 2016-01-12 by RGerhards, released under ASL 2.0
 # requires faketime
 echo \[now_family_utc\]: test \$NOW family of system properties
+
+. $srcdir/faketime_common.sh
+
 export TZ=TEST+06:30
-faketime '2016-01-01 01:00:00' date
-if [ $? -ne 0 ]; then
-    echo "faketime command missing, skipping test"
-    exit 77
-fi
+
 . $srcdir/diag.sh init
 faketime '2016-01-01 01:00:00' $srcdir/diag.sh startup now_family_utc.conf
 # what we send actually is irrelevant, as we just use system properties.
@@ -22,4 +21,6 @@ if [ ! $? -eq 0 ]; then
   cat rsyslog.out.log
   exit 1
 fi;
-#. $srcdir/diag.sh exit
+
+
+. $srcdir/diag.sh exit
