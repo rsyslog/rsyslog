c526dec75640fe5773d60358ebcb8b1fe5c9086a
Rainer Gerhards <rgerhards@adiscon.com>
2016-03-25 17:38:38 +0100

    testbench: add test for omrelp with default port

--- tests/sndrcv_relp_dflt_pt.sh
+++ tests/sndrcv_relp_dflt_pt.sh
@@ -2,5 +2,10 @@
 # added 2013-12-10 by Rgerhards
 # This file is part of the rsyslog project, released under ASL 2.0
 echo ===============================================================================
-echo \[sndrcv_relp.sh\]: testing sending and receiving via relp
+if [ `./omrelp_dflt_port` -lt 1024 ]; then
+    if [ "$EUID" -ne 0 ]; then
+	echo need to be root to run this test - skipping
+        exit 77
+    fi
+fi
 . $srcdir/sndrcv_drvr.sh sndrcv_relp_dflt_pt 50000
