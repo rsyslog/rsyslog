c28c44bb97bd372a1a3ddc14682160f0fd6ab016
Rainer Gerhards <rgerhards@adiscon.com>
2016-03-25 13:31:37 +0100

    imrelp: fix cosmetic memory leak
    this happens immediately before rsyslog terminates, so it does
    not really affect anything. However, it affects the testbench
    as memory checkers may report this leak (clang 3.6 address
    sanitizer was the first to do).

--- plugins/imrelp/imrelp.c
+++ plugins/imrelp/imrelp.c
@@ -589,6 +589,7 @@ CODESTARTfreeCnf
 		free(inst->pszInputName);
 		free(inst->pristring);
 		free(inst->authmode);
+		prop.Destruct(&inst->pInputName);
 		statsobj.Destruct(&(inst->data.stats));
 		for(i = 0 ; i <  inst->permittedPeers.nmemb ; ++i) {
 			free(inst->permittedPeers.name[i]);
