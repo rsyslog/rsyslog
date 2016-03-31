40fee6002018055b81dacbd2c1e48815ea25af64
Janmejay Singh <singh.janmejay@gmail.com>
2016-03-31 20:03:45 +0530

    support for foreach loop over object

--- tests/json_nonarray_looping.sh
+++ tests/json_nonarray_looping.sh
@@ -9,5 +9,6 @@ echo \[json_nonarray_looping.sh\]: test to assert attempt to iterate upon a non-
 echo doing shutdown
 . $srcdir/diag.sh shutdown-when-empty
 echo wait on shutdown
-. $srcdir/diag.sh wait-shutdown 
+. $srcdir/diag.sh wait-shutdown
+. $srcdir/diag.sh assert-content-missing 'quux'
 . $srcdir/diag.sh exit
