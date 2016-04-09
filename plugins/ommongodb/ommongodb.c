aafccded372576560009048dd38bcacde3ca05d4
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-09 14:14:33 +0200

    ommongodb: do not use json lib implementation details
    Some internal data elements are accessed, which is bad and no
    longer possible with newer versions of libfastjson and is bad
    style in any case (albeit "approved" by json-c).
    
    closes https://github.com/rsyslog/rsyslog/issues/943

--- plugins/ommongodb/ommongodb.c
+++ plugins/ommongodb/ommongodb.c
@@ -394,21 +394,28 @@ BSONAppendJSONObject(bson *doc, const gchar *name, struct json_object *json)
 	}
 }
 
+/* Note: this function assumes that at max a single sub-object exists. This
+ * may need to be extended to cover cases where multiple objects are contained.
+ * However, I am not sure about the original intent of this contribution and
+ * just came across it when refactoring the json calls. As everything seems
+ * to work since quite a while, I do not make any changes now.
+ * rgerhards, 2016-04-09
+ */
 static gboolean
 BSONAppendExtendedJSON(bson *doc, const gchar *name, struct json_object *json)
 {
-    struct lh_entry *entry;
+	struct json_object_iterator itEnd = json_object_iter_end(json);
+	struct json_object_iterator it = json_object_iter_begin(json);
 
-    entry = json_object_get_object(json)->head;
-    if (entry) {
-        char *key;
-        key = (char*)entry->k;
+	if (!json_object_iter_equal(&it, &itEnd)) {
+		const char *key;
+		key = json_object_iter_peek_name(&it);
         if (strcmp(key, "$date") == 0) {
             struct tm tm;
             gint64 ts;
             struct json_object *val;
 
-            val = (struct json_object*)entry->v;
+            val = json_object_iter_peek_value(&it);
 
             DBGPRINTF("ommongodb: extended json date detected %s", json_object_get_string(val));
 
