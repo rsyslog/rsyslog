fb6b44de0e24b5d3f04826b85de91c998c3b6e22
Rainer Gerhards <rgerhards@adiscon.com>
2016-04-22 18:21:07 +0200

    travis: do a special run which merges with master
    this guards against cross-PR regressions and also ensures that our
    newest travis settings are used

--- tests/CI/try_merge.sh
+++ tests/CI/try_merge.sh
@@ -2,11 +2,17 @@
 oldbranch=`git rev-parse --abbrev-ref HEAD`
 git config --global user.email "buildbot@rsyslog.com"
 git config --global user.name "buildbot"
+
+# we need to make sure we can pull in new code from the main
+# repo. Travis, for example, checks us out into a different repo.
+git remote add github https://github.com/rsyslog/rsyslog.git
+git fetch github &> /dev/null
+
+# create our interim branch and go...
 git branch -D tmp-CI
 git branch tmp-CI
 git checkout tmp-CI
-git fetch origin
-git merge --no-edit origin/master
+git merge --no-edit github/master
 if [ $? -ne 0 ]; then
     echo "======================================================================"
     echo "= FAIL: git merge, not doing any tests on the merge result           ="
@@ -24,11 +30,11 @@ echo "Note: failing tests may not be reproducible if master branch advances."
 echo "      However, they should not be taken lightly as they point into to"
 echo "      two conflicting changes to the codebase."
 
-# from here on, we want to see what's going on and we also want to abort
-# on any error.
-set -v
-set -e
-
-tests/CI/clang-check-sanitizer.sh
-
-git checkout ${oldbranch}
+if [ "$1" != "--merge-only" ]; then
+    # from here on, we want to see what's going on and we also want to abort
+    # on any error.
+    set -v
+    set -e
+    tests/CI/clang-check-sanitizer.sh
+    git checkout ${oldbranch}
+fi
