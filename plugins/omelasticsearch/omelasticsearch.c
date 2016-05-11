rsyslog/rsyslog

commit 5e14be8224ee43d44ec9e25a897c06c8b0510b27

omelasticsearch: Fix CURL ignoring HTTP errors

plugins/omelasticsearch/omelasticsearch.c
@@ -270,6 +270,8 @@ checkConn(wrkrInstanceData_t *pWrkrData)
 		DBGPRINTF("omelasticsearch: checkConn() curl_easy_init() failed\n");
 		ABORT_FINALIZE(RS_RET_SUSPENDED);
 	}
+	/* Fail on HTTP error */
+	curl_easy_setopt(curl, CURLOPT_FAILONERROR, TRUE);
 	/* Bodypart of request not needed, so set curl opt to nobody and httpget, otherwise lib-curl could sigsegv */
 	curl_easy_setopt(curl, CURLOPT_HTTPGET, TRUE);
 	curl_easy_setopt(curl, CURLOPT_NOBODY, TRUE);
@@ -1033,6 +1035,7 @@ curlPost(wrkrInstanceData_t *pWrkrData, uchar *message, int msglen, uchar **tpls
 		case CURLE_COULDNT_RESOLVE_PROXY:
 		case CURLE_COULDNT_CONNECT:
 		case CURLE_WRITE_ERROR:
+		case CURLE_HTTP_RETURNED_ERROR:
 			STATSCOUNTER_INC(indexHTTPReqFail, mutIndexHTTPReqFail);
 			indexHTTPFail += nmsgs;
 			DBGPRINTF("omelasticsearch: we are suspending ourselfs due "
@@ -1136,6 +1139,8 @@ curlSetup(wrkrInstanceData_t *pWrkrData, instanceData *pData)
 
 	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curlResult);
 	curl_easy_setopt(handle, CURLOPT_POST, 1);
+
+	curl_easy_setopt(handle, CURLOPT_FAILONERROR, TRUE);
 
 	pWrkrData->curlHandle = handle;
 	pWrkrData->postHeader = header;

commit 9e987d482f2c277a683935720501b086dcf703aa

bugfix omelasticsearch: error file functionality did not work
due to regression from 5e14be8224ee43d44ec9e25a897c06c8b0510b27
Reverting this commit.

plugins/omelasticsearch/omelasticsearch.c
@@ -275,8 +275,6 @@ checkConn(wrkrInstanceData_t *pWrkrData)
 		DBGPRINTF("omelasticsearch: checkConn() curl_easy_init() failed\n");
 		ABORT_FINALIZE(RS_RET_SUSPENDED);
 	}
-	/* Fail on HTTP error */
-	curl_easy_setopt(curl, CURLOPT_FAILONERROR, TRUE);
 	/* Bodypart of request not needed, so set curl opt to nobody and httpget, otherwise lib-curl could sigsegv */
 	curl_easy_setopt(curl, CURLOPT_HTTPGET, TRUE);
 	curl_easy_setopt(curl, CURLOPT_NOBODY, TRUE);
@@ -1062,7 +1060,6 @@ curlPost(wrkrInstanceData_t *pWrkrData, uchar *message, int msglen, uchar **tpls
 		case CURLE_COULDNT_RESOLVE_PROXY:
 		case CURLE_COULDNT_CONNECT:
 		case CURLE_WRITE_ERROR:
-		case CURLE_HTTP_RETURNED_ERROR:
 			STATSCOUNTER_INC(indexHTTPReqFail, mutIndexHTTPReqFail);
 			indexHTTPFail += nmsgs;
 			DBGPRINTF("omelasticsearch: we are suspending ourselfs due "
@@ -1163,8 +1160,6 @@ curlSetup(wrkrInstanceData_t *pWrkrData, instanceData *pData)
 	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curlResult);
 	curl_easy_setopt(handle, CURLOPT_POST, 1);
 	curl_easy_setopt(handle, CURLOPT_NOSIGNAL, TRUE);
-
-	curl_easy_setopt(handle, CURLOPT_FAILONERROR, TRUE);
 
 	pWrkrData->curlHandle = handle;
 	pWrkrData->postHeader = header;