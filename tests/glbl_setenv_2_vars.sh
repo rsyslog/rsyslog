2f119e426b87a21b40264497acf0df290586b325
Rainer Gerhards <rgerhards@adiscon.com>
2016-10-20 15:11:29 +0200

    testbench: add tests for new "environment" global parameter

---
+++ tests/glbl_setenv_2_vars.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is part of the rsyslog testbench, licensed under ASL 2.0
+. $srcdir/diag.sh init
+. $srcdir/diag.sh generate-conf
+. $srcdir/diag.sh add-conf '
+global(environment=["http_proxy=http://127.0.0.1", "SECOND=OK OK"])
+
+set $!prx = getenv("http_proxy");
+set $!second = getenv("SECOND");
+
+template(name="outfmt" type="string" string="%$!prx%, %$!second%\n")
+:msg, contains, "msgnum:" action(type="omfile" template="outfmt"
+			         file="rsyslog.out.log")
+'
+. $srcdir/diag.sh startup
+. $srcdir/diag.sh injectmsg  0 1
+. $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
+. $srcdir/diag.sh wait-shutdown    # we need to wait until rsyslogd is finished!
+
+echo 'http://127.0.0.1, OK OK' | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid content seen, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  . $srcdir/diag.sh error-exit 1
+fi;
+
+. $srcdir/diag.sh exit
