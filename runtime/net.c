547dfa156a66ae2db203d69616ca76316d04eafa
mostolog <mostolog@users.noreply.github.com>
2016-10-21 08:12:16 +0200

    Update net.c

--- runtime/net.c
+++ runtime/net.c
@@ -1216,7 +1216,7 @@ closeUDPListenSockets(int *pSockArr)
  * param rcvbuf indicates desired rcvbuf size; 0 means OS default
  */
 static int *
-create_udp_socket(uchar *hostname, uchar *pszPort, int bIsServer, int rcvbuf, int ipfreebind)
+create_udp_socket(uchar *hostname, uchar *pszPort, int bIsServer, unsigned int rcvbuf, int ipfreebind)
 {
         struct addrinfo hints, *res, *r;
         int error, maxs, *s, *socks, on = 1;
