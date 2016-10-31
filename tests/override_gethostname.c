7fa116c7f47abb3c470136d7e91b8b457b7a5b3e
Rainer Gerhards <rgerhards@adiscon.com>
2016-10-31 09:09:37 +0100

    testbench: improve pilot program for API override
    among others, this is necessary to pass CI, as we otherwise (rightfully)
    get compiler warnings

--- tests/override_gethostname.c
+++ tests/override_gethostname.c
@@ -1,11 +1,9 @@
-//#define _GNU_SOURCE
-//#include <dlfcn.h>
+// we need this for dlsym(): #include <dlfcn.h>
 #include <stdio.h>
  
 
-int gethostname(char *name, size_t len)
+int gethostname(char *name, size_t __attribute__((unused)) len)
 {
-	//*name++ = 'i';
 	*name = '\0';
 	return 0;
 }
@@ -13,6 +11,12 @@ int gethostname(char *name, size_t len)
 static void __attribute__((constructor))
 my_init(void)
 {
-  printf("loaded\n");
-  //orig_etry = dlsym(RTLD_NEXT, "original_entry_point");
+	/* we currently do not need this entry point, but keep it as
+	 * a "template". It can be used, e.g. to emit some diagnostic
+	 * information:
+	printf("loaded\n");
+	 * or - more importantly - obtain a pointer to the overriden
+	 * API:
+	orig_etry = dlsym(RTLD_NEXT, "original_entry_point");
+	*/
 }
