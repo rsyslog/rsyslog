99aecbdb85fc62a6b1aa38f2b3ebe1c8e83932c0
Rainer Gerhards <rgerhards@adiscon.com>
2016-11-02 19:06:50 +0100

    testbench: add test for object persisting
    see also https://github.com/rsyslog/rsyslog/issues/1239

---
+++ tests/imfile-persist-state-1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# added 2016-11-02 by rgerhards
+# This is part of the rsyslog testbench, licensed under ASL 2.0
+. $srcdir/diag.sh init
+. $srcdir/diag.sh generate-conf
+. $srcdir/diag.sh add-conf '
+global(workDirectory="test-spool")
+
+module(load="../plugins/imfile/.libs/imfile")
+
+input(	type="imfile"
+	file="./rsyslog.input"
+	tag="file:"
+	startmsg.regex="^msgnum"
+	PersistStateInterval="1"
+)
+
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+if $msg contains "msgnum:" then
+	action(type="omfile" file="rsyslog.out.log" template="outfmt")
+'
+# generate input file first. Note that rsyslog processes it as
+# soon as it start up (so the file should exist at that point).
+./inputfilegen 5 4000 > rsyslog.input
+. $srcdir/diag.sh startup
+. $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
+. $srcdir/diag.sh wait-shutdown	# we need to wait until rsyslogd is finished!
+. $srcdir/diag.sh seq-check 0 3
+. $srcdir/diag.sh exit
--- tests/inputfilegen.c
+++ tests/inputfilegen.c
@@ -1,24 +1,38 @@
 /* generate an input file suitable for use by the testbench
  * Copyright (C) 2011 by Rainer Gerhards and Adiscon GmbH.
  * usage: ./inputfilegen num-lines > file
- * Part of rsyslog, licensed under GPLv3
+ * Part of rsyslog, licensed under ASL 2.0
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
+	int extradata = 0;
+	char *pdata = "";
 	int nmsgs;
 	int i;
 
-	if(argc != 2) {
-		fprintf(stderr, "usage: inputfilegen num-messages\n");
+	if(argc < 2 && argc > 3) {
+		fprintf(stderr, "usage: inputfilegen num-messages [extra-data]\n");
 		return 1;
 	}
 
 	nmsgs = atoi(argv[1]);
-	for(i = 0 ; i < nmsgs ; ++i) {
-		printf("msgnum:%8.8d:\n", i);
+	if(argc == 3) {
+		extradata = atoi(argv[2]);
+		if((pdata = malloc(extradata+1)) == NULL) {
+			perror("malloc extradata");
+			return 2;
+		}
+		memset(pdata, 'X', extradata);
+		pdata[extradata] = '\0';
 	}
+	for(i = 0 ; i < nmsgs ; ++i) {
+		printf("msgnum:%8.8d:%s\n", i, pdata);
+	}
+	if(argc == 3)
+		free(pdata);
 	return 0;
 }
