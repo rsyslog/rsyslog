9e5ff2948963d16c22360125833c4d0789a6e9e5
Andre Lorbach <alorbach@adiscon.com>
2017-01-17 11:09:16 +0100

    imfile: Added testcase for multilevel wildcard directories
    Also fixed a bug in new wildcard dir handling
    found using this test.

---
+++ tests/imfile-wildcards-dirs-multi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This is part of the rsyslog testbench, licensed under GPLv3
+echo [imfile-wildcards-dirs2.sh]
+
+export IMFILEINPUTFILES="10"
+export IMFILEINPUTFILESSTEPS="5"
+export IMFILEINPUTFILESALL=$(($IMFILEINPUTFILES * $IMFILEINPUTFILESSTEPS))
+
+. $srcdir/diag.sh init
+# generate input files first. Note that rsyslog processes it as
+# soon as it start up (so the file should exist at that point).
+
+# Start rsyslog now before adding more files
+. $srcdir/diag.sh startup imfile-wildcards-dirs.conf
+# sleep a little to give rsyslog a chance to begin processing
+sleep 1
+
+for j in `seq 1 $IMFILEINPUTFILESSTEPS`;
+do
+	echo "Loop Num $j"
+
+	for i in `seq 1 $IMFILEINPUTFILES`;
+	do
+		mkdir rsyslog.input.dir$i
+		mkdir rsyslog.input.dir$i/dir$i
+		./inputfilegen -m 1 > rsyslog.input.dir$i/dir$i/file.logfile
+	done
+	ls -d rsyslog.input.*
+
+	# Delete all but first!
+	for i in `seq 1 $IMFILEINPUTFILES`;
+	do
+		rm -r rsyslog.input.dir$i
+	done
+done
+
+# sleep a little to give rsyslog a chance for processing
+sleep 1
+
+. $srcdir/diag.sh shutdown-when-empty # shut down rsyslogd when done processing messages
+. $srcdir/diag.sh wait-shutdown	# we need to wait until rsyslogd is finished!
+. $srcdir/diag.sh content-check-with-count "HEADER msgnum:00000000:" $IMFILEINPUTFILESALL
+. $srcdir/diag.sh exit
