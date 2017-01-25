315fa5259cb020a40f955d36919ef6c374a71cd6
Dan Molik <dan@d3fy.net>
2017-01-25 12:31:04 -0500

    validate mmdblookup test
    - streamline testing with ruleset and mmnormalize

--- tests/mmdb.sh
+++ tests/mmdb.sh
@@ -2,13 +2,16 @@
 # added 2014-11-17 by singh.janmejay 
 # This file is part of the rsyslog project, released under ASL 2.0 
 echo =============================================================================== 
-echo \[mmdb.sh\]: test for mmdb 
-. $srcdir/diag.sh init 
-. $srcdir/diag.sh startup mmdb.conf 
-. $srcdir/diag.sh tcpflood -m 1 -j '202.106.0.20'
+echo \[mmdb.sh\]: test for mmdb
+# uncomment for debugging support:
+#export RSYSLOG_DEBUG="debug nostdout"
+#export RSYSLOG_DEBUGLOG="log"
+. $srcdir/diag.sh init
+. $srcdir/diag.sh startup mmdb.conf
+. $srcdir/diag.sh tcpflood -m 1 -j "202.106.0.20\ "
 echo doing shutdown 
-. $srcdir/diag.sh shutdown-when-empty 
+. $srcdir/diag.sh shutdown-when-empty
 echo wait on shutdown 
-. $srcdir/diag.sh wait-shutdown  
-. $srcdir/diag.sh content-check '"city" : "Beijing"' 
+. $srcdir/diag.sh wait-shutdown
+. $srcdir/diag.sh content-check '{ "city": "Beijing" }'
 . $srcdir/diag.sh exit 
