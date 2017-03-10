rsyslog/rsyslog

commit e305bdad9ead35691db203e9ab408832bd3ac99b

deprecate -u option

tools/rsyslogd.c
@@ -1050,6 +1050,7 @@ initAll(int argc, char **argv)
 		case 'T': /* chroot on startup (primarily for testing) */
 		case 'u': /* misc user settings */
 		case 'w': /* disable disallowed host warnings */
+		case 'r':
 		case 'x': /* disable dns for remote messages */
 			CHKiRet(bufOptAdd(ch, optarg));
 			break;
@@ -1184,14 +1185,26 @@ initAll(int argc, char **argv)
 			break;
 		case 'u':		/* misc user settings */
 			iHelperUOpt = atoi(arg);
-			if(iHelperUOpt & 0x01)
+			if(iHelperUOpt & 0x01) {
+				fprintf (stderr, "rsyslogd: the -u command line option will go away "
+					 "soon.\n"
+					 "For the 0x01 bit, please use the "
+					 "global(net.parseHostnamdAndTag=\"off\") "
+					 "configuration parameter instead.\n");
 				glbl.SetParseHOSTNAMEandTAG(0);
+			}
 			if(iHelperUOpt & 0x02)
+				fprintf (stderr, "rsyslogd: the -u command line option will go away "
+					 "soon.\n"
+					 "For the 0x02 bit, please use the -C option instead.");
 				bChDirRoot = 0;
+			break;
+		case 'C':
+			bChDirRoot = 0;
 			break;
 		case 'w':		/* disable disallowed host warnigs */
 			fprintf (stderr, "rsyslogd: the -w command line option will go away "
-				 "soon.\nPlease use the global(net.permitACLWarning=\"off\") "
+				 "soon.\nPlease use the global(net.permitWarning=\"off\") "
 				 "configuration parameter instead.\n");
 			glbl.SetOption_DisallowWarning(0);
 			break;

runtime/glbl.c
@@ -141,6 +141,7 @@ static struct cnfparamdescr cnfparamdescr[] = {
 	{ "parser.escape8bitcharactersonreceive", eCmdHdlrBinary, 0},
 	{ "parser.escapecontrolcharactertab", eCmdHdlrBinary, 0},
 	{ "parser.escapecontrolcharacterscstyle", eCmdHdlrBinary, 0 },
+	{ "parser.parsehostnameandtag", eCmdHdlrBinary, 0 },
 	{ "stdlog.channelspec", eCmdHdlrString, 0 },
 	{ "janitor.interval", eCmdHdlrPositiveInt, 0 },
 	{ "net.ipprotocol", eCmdHdlrGetWord, 0 },
@@ -191,7 +192,6 @@ static dataType Get##nameFunc(void) \
 	return(nameVar); \
 }
 
-SIMP_PROP(ParseHOSTNAMEandTAG, bParseHOSTNAMEandTAG, int)
 SIMP_PROP(OptimizeUniProc, bOptimizeUniProc, int)
 SIMP_PROP(PreserveFQDN, bPreserveFQDN, int)
 SIMP_PROP(mainqCnfObj, mainqCnfObj, struct cnfobj *)
@@ -365,9 +365,8 @@ setDebugLevel(void __attribute__((unused)) *pVal, int level)
 static rsRetVal
 setDisableDNS(int val)
 {
-	DEFiRet;
 	bDisableDNS = val;
-	RETiRet;
+	return RS_RET_OK;
 }
 
 static int
@@ -379,15 +378,27 @@ getDisableDNS(void)
 static rsRetVal
 setOption_DisallowWarning(int val)
 {
-	DEFiRet;
 	option_DisallowWarning = val;
-	RETiRet;
+	return RS_RET_OK;
 }
 
 static int
 getOption_DisallowWarning(void)
 {
 	return option_DisallowWarning;
+}
+
+static rsRetVal
+setParseHOSTNAMEandTAG(int val)
+{
+	bParseHOSTNAMEandTAG = val;
+	return RS_RET_OK;
+}
+
+static int
+getParseHOSTNAMEandTAG(void)
+{
+	return bParseHOSTNAMEandTAG;
 }
 
 static rsRetVal
@@ -660,12 +671,13 @@ CODESTARTobjQueryInterface(glbl)
 	pIf->GetDisableDNS = getDisableDNS;
 	pIf->SetOption_DisallowWarning = setOption_DisallowWarning;
 	pIf->GetOption_DisallowWarning = getOption_DisallowWarning;
+	pIf->SetParseHOSTNAMEandTAG = setParseHOSTNAMEandTAG;
+	pIf->GetParseHOSTNAMEandTAG = getParseHOSTNAMEandTAG;
 #define SIMP_PROP(name) \
 	pIf->Get##name = Get##name; \
 	pIf->Set##name = Set##name;
 	SIMP_PROP(MaxLine);
 	SIMP_PROP(OptimizeUniProc);
-	SIMP_PROP(ParseHOSTNAMEandTAG);
 	SIMP_PROP(PreserveFQDN);
 	SIMP_PROP(DropMalPTRMsgs);
 	SIMP_PROP(mainqCnfObj);
@@ -992,6 +1004,8 @@ glblDoneLoadCnf(void)
 			bEscapeTab = (int) cnfparamvals[i].val.d.n;
 		} else if(!strcmp(paramblk.descr[i].name, "parser.escapecontrolcharacterscstyle")) {
 			bParserEscapeCCCStyle = (int) cnfparamvals[i].val.d.n;
+		} else if(!strcmp(paramblk.descr[i].name, "parser.parsehostnameandtag")) {
+			bParseHOSTNAMEandTAG = (int) cnfparamvals[i].val.d.n;
 		} else if(!strcmp(paramblk.descr[i].name, "debug.logfile")) {
 			if(pszAltDbgFileName == NULL) {
 				pszAltDbgFileName = es_str2cstr(cnfparamvals[i].val.d.estr, NULL);

tools/rsyslogd.8
@@ -11,7 +11,6 @@ rsyslogd \- reliable and extended syslogd
 .RB [ " \-f "
 .I config file
 ]
-.br
 .RB [ " \-i "
 .I pid file
 ]
@@ -19,10 +18,7 @@ rsyslogd \- reliable and extended syslogd
 .RB [ " \-N "
 .I level
 ]
-.br
-.RB [ " \-u "
-.I userlevel
-]
+.RB [ " \-C " ]
 .RB [ " \-v " ]
 .LP
 .SH DESCRIPTION
@@ -119,17 +115,10 @@ verbosity (this is a forward-compatibility option).
 is started and controlled by
 .BR init (8).
 .TP
-.BI "\-u " "userlevel"
-This is a "catch all" option for some very seldomly-used user settings.
-The "userlevel" variable selects multiple things. Add the specific values
-to get the combined effect of them.
-A value of 1 prevents rsyslogd from parsing hostnames and tags inside
-messages.
-A value of 2 prevents rsyslogd from changing to the root directory. This
+.BI "\-C"
+This prevents rsyslogd from changing to the root directory. This
 is almost never a good idea in production use. This option was introduced
 in support of the internal testbed.
-To combine these two features, use a userlevel of 3 (1+2). Whenever you use
-an -u option, make sure you really understand what you do and why you do it.
 .TP
 .B "\-v"
 Print version and exit.

commit 769b040815ab3bc068de5fb5251dab6cdb6b787c

fix missing braces around if stmt
Fixes: e305bdad9ead ("deprecate -u option")
Signed-off-by: Nikola Pajkovsky <npajkovsky@suse.cz>

tools/rsyslogd.c
@@ -1365,11 +1365,12 @@ initAll(int argc, char **argv)
 					 "configuration parameter instead.\n");
 				glbl.SetParseHOSTNAMEandTAG(0);
 			}
-			if(iHelperUOpt & 0x02)
+			if(iHelperUOpt & 0x02) {
 				fprintf (stderr, "rsyslogd: the -u command line option will go away "
 					 "soon.\n"
 					 "For the 0x02 bit, please use the -C option instead.");
 				bChDirRoot = 0;
+			}
 			break;
 		case 'C':
 			bChDirRoot = 0;