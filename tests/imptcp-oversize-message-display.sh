6253435e0a6178c9962381892b060663cd42e3af
Jan Gerhards <jgerhards@adiscon.com>
2017-04-04 15:11:29 +0200

    modified imptcp-oversize-display test for clarity
    due to an earlier test cycl a test message contained two headers. to avoid confusion, the latter header has beenreplacerd with the letter 'k'.

--- tests/imptcp-oversize-message-display.sh
+++ tests/imptcp-oversize-message-display.sh
@@ -13,7 +13,7 @@ action(type="omfile" file="rsyslog.out.log")
 
 '
 . $srcdir/diag.sh startup
-. $srcdir/diag.sh tcpflood -m1 -M "\"<120> 2011-03-01T11:22:12Z host tag: this is a way too long message that has to be truncatedtest1 test2 test3 test4 test5 abcdefghijklmn test8 test9 test10 test11 test12 test13 test14 test15 \n<120> 2011-03-01T11:22:12Z host tag: testtestetstetstetstetstetsstetstetsytetestetste\""
+. $srcdir/diag.sh tcpflood -m1 -M "\"<120> 2011-03-01T11:22:12Z host tag: this is a way too long message that has to be truncatedtest1 test2 test3 test4 test5 abcdefghijklmn test8 test9 test10 test11 test12 test13 test14 test15 kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk tag: testtestetstetstetstetstetsstetstetsytetestetste\""
 . $srcdir/diag.sh shutdown-when-empty
 . $srcdir/diag.sh wait-shutdown
 
