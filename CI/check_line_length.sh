4dbc37243f2e1ab3ebd33d98f69441a8111ac50f
PascalWithopf <pwithopf@adiscon.com>
2017-06-27 11:20:54 +0200

    codestyle: enable check for whitespaces

--- CI/check_line_length.sh
+++ CI/check_line_length.sh
@@ -5,5 +5,5 @@ git clone https://github.com/rsyslog/codestyle
 cd codestyle
 gcc --std=c99 stylecheck.c -o stylecheck
 cd ../..
-find -name "*.[ch]" | xargs _tmp_stylecheck/codestyle/stylecheck -w -f -l 120 && \
+find -name "*.[ch]" | xargs _tmp_stylecheck/codestyle/stylecheck -l 120 && \
 	_tmp_stylecheck/codestyle/stylecheck plugins/imfile/imfile.c
