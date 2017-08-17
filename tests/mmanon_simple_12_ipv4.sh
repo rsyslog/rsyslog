21d64f3e8acfc6cd9683f7d11441174bd2116836
Jan Gerhards <jgerhards@adiscon.com>
2017-08-17 13:31:43 +0200

    mmanon: rewrite ipv4-anonymization
    for more information, see https://github.com/rsyslog/rsyslog/issues/1723
    
    closes https://github.com/rsyslog/rsyslog/issues/1717
    closes https://github.com/rsyslog/rsyslog/issues/1720

---
+++ tests/mmanon_simple_12_ipv4.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# add 2016-11-22 by Jan Gerhards, released under ASL 2.0
+. $srcdir/diag.sh init
+. $srcdir/diag.sh generate-conf
+. $srcdir/diag.sh add-conf '
+template(name="outfmt" type="string" string="%msg%\n")
+
+module(load="../plugins/mmanon/.libs/mmanon")
+module(load="../plugins/imtcp/.libs/imtcp")
+input(type="imtcp" port="13514" ruleset="testing")
+
+ruleset(name="testing") {
+	action(type="mmanon" ipv4.bits="12" ipv4.mode="simple")
+	action(type="omfile" file="./rsyslog.out.log" template="outfmt")
+}
+action(type="omfile" file="rsyslog2.out.log")'
+
+. $srcdir/diag.sh startup
+. $srcdir/diag.sh tcpflood -m1 -M "\"<129>Mar 10 01:00:00 172.20.245.8 tag: 1.1.1.8
+<129>Mar 10 01:00:00 172.20.245.8 tag: 0.0.0.0
+<129>Mar 10 01:00:00 172.20.245.8 tag: 172.0.234.255
+<129>Mar 10 01:00:00 172.20.245.8 tag: 111.1.1.8.\""
+
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown
+echo ' 1.1.x.x
+ 0.0.x.x
+ 172.0.xxx.xxx
+ 111.1.x.x.' | cmp rsyslog.out.log
+if [ ! $? -eq 0 ]; then
+  echo "invalid response generated, rsyslog.out.log is:"
+  cat rsyslog.out.log
+  . $srcdir/diag.sh error-exit  1
+fi;
+
+grep 'invalid number of ipv4 bits in simple mode, corrected to 16' rsyslog2.out.log > /dev/null
+if [ $? -ne 0 ]; then
+  echo "invalid response generated, rsyslog2.out.log is:"
+  cat rsyslog2.out.log
+  . $srcdir/diag.sh error-exit  1
+fi;
+
+. $srcdir/diag.sh exit
