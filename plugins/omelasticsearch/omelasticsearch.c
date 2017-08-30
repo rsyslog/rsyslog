rsyslog/rsyslog

commit 462be8b2ee4747a5317930d11018ce5ccb3726e0

omelasticsearch: avoid ES5 warnings while sending json
ES5 is generating warnings when sending json without the proper header:

$ curl -i -H "Content-Type: text/json" -XGET 'http://elasticsearch5:9200/' -d '{}\n'
HTTP/1.1 200 OK
Warning: 299 Elasticsearch-5.4.3-eed30a8 "Content type detection for rest requests is deprecated. Specify the content type using the [Content-Type] header." "Wed, 26 Jul 2017 14:33:28 GMT"

no issue on previous version

this patch correctly sets the header as application/json. It works for all versions (tested on ES2 and ES5)
we also handle the bulkmode where it should be set to application/x-ndjson

Fixes: #1642
Signed-off-by: William Dauchy <w.dauchy@criteo.com>

plugins/omelasticsearch/omelasticsearch.c
@@ -1322,10 +1322,17 @@ curlPostSetup(CURL *handle, HEADER *header, uchar* authBuf)
 	}
 }
 
+#define CONTENT_JSON "Content-Type: application/json; charset=utf-8"
+#define CONTENT_JSON_BULK "Content-Type: application/x-ndjson; charset=utf-8"
+
 static rsRetVal
 curlSetup(wrkrInstanceData_t *pWrkrData, instanceData *pData)
 {
-	pWrkrData->curlHeader = curl_slist_append(NULL, "Content-Type: text/json; charset=utf-8");
+	if (pData->bulkmode) {
+		pWrkrData->curlHeader = curl_slist_append(NULL, CONTENT_JSON_BULK);
+	} else {
+		pWrkrData->curlHeader = curl_slist_append(NULL, CONTENT_JSON);
+	}
 	pWrkrData->curlPostHandle = curl_easy_init();
 	if (pWrkrData->curlPostHandle == NULL) {
 		return RS_RET_OBJ_CREATION_FAILED;

commit 50d8e5cb391d8e0b984c725066a688da8b723b61

omelasticsearch: avoid ES5 warnings while sending json in bulkmode
my original patch attented to fix an deprecation warnings while sending
json related to content type.
I however wanted to improve my commit and support the mentioned x-ndjson
content type in case of bulk mode.
see
https://www.elastic.co/guide/en/elasticsearch/reference/master/docs-bulk.html

However, firstly my patch was wrong, since, in bulk mode, the x-ndjson was
set even for connection checks. Secondly I discovered that ES community
kind of disagree of what is the correct behavior:
 in RFC 6648 the x- prefix is already decided as "deprecated"
see https://github.com/elastic/elasticsearch/issues/25718

So I decided to put only application/json as content type; it is
supported for both simple request or bulk requests containing multiple
json. We can improve it when there is a final decision about it.

Re-Fixes: #1642
Fixes: commit 462be8b2ee4 (omelasticsearch: avoid ES5 warnings while sending json)
Signed-off-by: William Dauchy <w.dauchy@criteo.com>

plugins/omelasticsearch/omelasticsearch.c
@@ -1323,16 +1323,11 @@ curlPostSetup(CURL *handle, HEADER *header, uchar* authBuf)
 }
 
 #define CONTENT_JSON "Content-Type: application/json; charset=utf-8"
-#define CONTENT_JSON_BULK "Content-Type: application/x-ndjson; charset=utf-8"
 
 static rsRetVal
 curlSetup(wrkrInstanceData_t *pWrkrData, instanceData *pData)
 {
-	if (pData->bulkmode) {
-		pWrkrData->curlHeader = curl_slist_append(NULL, CONTENT_JSON_BULK);
-	} else {
-		pWrkrData->curlHeader = curl_slist_append(NULL, CONTENT_JSON);
-	}
+	pWrkrData->curlHeader = curl_slist_append(NULL, CONTENT_JSON);
 	pWrkrData->curlPostHandle = curl_easy_init();
 	if (pWrkrData->curlPostHandle == NULL) {
 		return RS_RET_OBJ_CREATION_FAILED;