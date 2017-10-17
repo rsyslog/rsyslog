0bb3fced61e51f73281b3c83c46951c8314a6639
Thomas Deutschmann <whissi@whissi.de>
2017-10-17 17:02:13 +0200

    imgssapi: Incooperate changes from tcpsrv subsystem
    Commit 5c5a7a3ed509dfb39b451c806646ab67782f6eda introduced an additional parameter
    "oserr".
    
    Fixes: https://github.com/rsyslog/rsyslog/issues/1838

--- plugins/imgssapi/imgssapi.c
+++ plugins/imgssapi/imgssapi.c
@@ -291,7 +291,7 @@ finalize_it:
 
 
 static rsRetVal
-doRcvData(tcps_sess_t *pSess, char *buf, size_t lenBuf, ssize_t *piLenRcvd)
+doRcvData(tcps_sess_t *pSess, char *buf, size_t lenBuf, ssize_t *piLenRcvd, int *const oserr)
 {
 	DEFiRet;
 	int allowedMethods;
@@ -307,7 +307,7 @@ doRcvData(tcps_sess_t *pSess, char *buf, size_t lenBuf, ssize_t *piLenRcvd)
 		CHKiRet(TCPSessGSSRecv(pSess, buf, lenBuf, piLenRcvd));
 	} else {
 		*piLenRcvd = lenBuf;
-		CHKiRet(netstrm.Rcv(pSess->pStrm, (uchar*) buf, piLenRcvd) != RS_RET_OK);
+		CHKiRet(netstrm.Rcv(pSess->pStrm, (uchar*) buf, piLenRcvd, oserr) != RS_RET_OK);
 	}
 
 finalize_it:
