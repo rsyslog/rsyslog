8feb5299840671ee39c3981633c8015dedb871b3
Rainer Gerhards <rgerhards@adiscon.com>
2017-10-23 17:29:52 +0200

    CI: use clang 5.0 for static analyzer

--- tests/CI/clang-analyzer-build.sh
+++ tests/CI/clang-analyzer-build.sh
@@ -2,21 +2,33 @@
 #set -e
 #set -v
 
-export CC=clang-4.0
-export SCANBUILD=scan-build-4.0
+export CC=clang-5.0
+export SCANBUILD=scan-build-5.0
 #export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
-#autoreconf -fiv
-#scan-build-4.0 ./configure --prefix=/usr/local --infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc --localstatedir=/var/lib --disable-dependency-tracking --docdir=/usr/share/doc/rsyslog ...
-$SCANBUILD ./configure --disable-generate-man-pages --enable-debug --enable-testbench --enable-imdiag --enable-imfile --enable-impstats --enable-imptcp --enable-mmanon --enable-mmaudit --enable-mmfields --enable-mmjsonparse --enable-mmpstrucdata --enable-mmsequence --enable-mmutf8fix --enable-mail --enable-omprog --enable-omruleset --enable-omstdout --enable-omuxsock --enable-pmaixforwardedfrom --enable-pmciscoios --enable-pmcisconames --enable-pmlastmsg --enable-pmsnare --enable-libgcrypt --enable-mmnormalize --disable-omudpspoof --enable-relp --disable-snmp --disable-mmsnmptrapd --enable-gnutls --enable-usertools=no --enable-mysql --enable-valgrind --enable-omjournal --enable-omczmq  --enable-imczmq
+echo "CC $CC, SCANBUILD $SCANBUILD"
+
+if [ -n "$SCAN_BUILD_REPORT_DIR" ]
+then
+  export CURR_REPORT=`date +%y-%m-%d_%H-%M-%S`
+  export REPORT_DIR="$SCAN_BUILD_REPORT_DIR/$CURR_REPORT"
+  export REPORT_OPT="-o $REPORT_DIR"
+fi
+env | grep REPORT
+$SCANBUILD ./configure --disable-generate-man-pages --enable-debug --enable-testbench --enable-imdiag --enable-imfile --enable-impstats --enable-imptcp --enable-mmanon --enable-mmaudit --enable-mmfields --enable-mmjsonparse --enable-mmpstrucdata --enable-mmsequence --enable-mmutf8fix --enable-mail --enable-omprog --enable-omruleset --enable-omstdout --enable-omuxsock --enable-pmaixforwardedfrom --enable-pmciscoios --enable-pmcisconames --enable-pmlastmsg --enable-pmsnare --enable-libgcrypt --enable-mmnormalize --disable-omudpspoof --enable-relp --disable-snmp --disable-mmsnmptrapd --enable-gnutls --enable-usertools=no --enable-mysql --enable-valgrind --enable-omjournal --enable-omczmq --enable-imczmq --enable-elasticsearch
 make clean
 # we don't want to run the testbench, but we want to build testbench tools
 # make check currently produces issues
-#scan-build-4.0 make check TESTS="" -j
-rm -r /tmp/scan-build-results
-rm analyzer-result.tar.gz
-$SCANBUILD -o /tmp/scan-build-results --status-bugs make -j4
+# $SCANBUILD make check TESTS="" -j
+#$SCANBUILD -o /tmp/scan-build-results --status-bugs make -j4
+$SCANBUILD $REPORT_OPT --status-bugs make -j4
 RESULT=$?
 echo scan-build result: $RESULT
-tar cf analyzer-result.tar.gz /tmp/scan-build-results
-rm -r /tmp/scan-build-results
+if [ $RESULT -eq 1 ]
+then
+   if [ -n "$SCAN_BUILD_REPORT_DIR" ]
+   then
+      echo "scan-build report URL: ${SCAN_BUILD_REPORT_BASEURL}${CURR_REPORT}" > report_url
+   fi
+fi
+ls -l $REPORT_DIR
 exit $RESULT
