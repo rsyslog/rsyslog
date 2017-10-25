rsyslog/rsyslog

commit 92ac8016e4cd6a2d7a791233b613c8c74746d08b

Made switching to persistent journal in runtime possible, now you can continue logging after switch to persistent journal without need to restart rsyslog service. Bumped correspondidng systemd version dependency.

plugins/imjournal/imjournal.c
@@ -115,6 +115,25 @@ static int bPidFallBack;
 static ratelimit_t *ratelimiter = NULL;
 static sd_journal *j;
 
+static rsRetVal persistJournalState(void);
+static rsRetVal loadJournalState(void);
+
+static rsRetVal openJournal(sd_journal* jj) {
+	DEFiRet;
+
+	if (sd_journal_open(&jj, SD_JOURNAL_LOCAL_ONLY) < 0)
+		iRet = RS_RET_IO_ERROR;
+	RETiRet;
+}
+
+static void closeJournal(sd_journal* jj) {
+
+	if (cs.stateFile) { /* can't persist without a state file */
+		persistJournalState();
+	}
+	sd_journal_close(jj);
+}
+
 
 /* ugly workaround to handle facility numbers; values
  * derived from names need to be eight times smaller,
@@ -443,43 +462,64 @@ persistJournalState (void)
 }
 
 
+static rsRetVal skipOldMessages(void);
 /* Polls the journal for new messages. Similar to sd_journal_wait()
  * except for the special handling of EINTR.
  */
+
+#define POLL_TIMEOUT 1000 /* timeout for poll is 1s */
+
 static rsRetVal
 pollJournal(void)
 {
 	DEFiRet;
 	struct pollfd pollfd;
-	int r;
+	int pr = 0;
+	int jr = 0;
 
 	pollfd.fd = sd_journal_get_fd(j);
 	pollfd.events = sd_journal_get_events(j);
-	r = poll(&pollfd, 1, -1);
-	if (r == -1) {
+	pr = poll(&pollfd, 1, POLL_TIMEOUT);
+	if (pr == -1) {
 		if (errno == EINTR) {
 			/* EINTR is also received during termination
 			 * so return now to check the term state.
 			 */
-			ABORT_FINALIZE(RS_RET_OK);
+		ABORT_FINALIZE(RS_RET_OK);
 		} else {
 			char errStr[256];
 
 			rs_strerror_r(errno, errStr, sizeof(errStr));
-			errmsg.LogError(0, RS_RET_ERR,
+			LogError(0, RS_RET_ERR,
 				"poll() failed: '%s'", errStr);
 			ABORT_FINALIZE(RS_RET_ERR);
 		}
 	}
 
-	assert(r == 1);
+	jr = sd_journal_process(j);
 
-	r = sd_journal_process(j);
-	if (r < 0) {
+	if (pr == 1 && jr == SD_JOURNAL_INVALIDATE) {
+		/* do not persist stateFile sd_journal_get_cursor will fail! */
+		char* tmp = cs.stateFile;
+		cs.stateFile = NULL;
+		closeJournal(j);
+		cs.stateFile = tmp;
+
+		iRet = openJournal(j);
+		if (iRet != RS_RET_OK) {
+			char errStr[256];
+			rs_strerror_r(errno, errStr, sizeof(errStr));
+			LogError(0, RS_RET_IO_ERROR,
+				"sd_journal_open() failed: '%s'", errStr);
+			ABORT_FINALIZE(RS_RET_ERR);
+		}
+
+		iRet = loadJournalState();
+		LogMsg(0, RS_RET_OK, LOG_NOTICE, "imjournal: journal reloaded...");
+	} else if (jr < 0) {
 		char errStr[256];
-
 		rs_strerror_r(errno, errStr, sizeof(errStr));
-		errmsg.LogError(0, RS_RET_ERR,
+		LogError(0, RS_RET_ERR,
 			"sd_journal_process() failed: '%s'", errStr);
 		ABORT_FINALIZE(RS_RET_ERR);
 	}
@@ -719,20 +759,13 @@ ENDfreeCnf
 /* open journal */
 BEGINwillRun
 CODESTARTwillRun
-	int ret;
-	ret = sd_journal_open(&j, SD_JOURNAL_LOCAL_ONLY);
-	if (ret < 0) {
-		iRet = RS_RET_IO_ERROR;
-	}
+	iRet = openJournal(j);
 ENDwillRun
 
 /* close journal */
 BEGINafterRun
 CODESTARTafterRun
-	if (cs.stateFile) { /* can't persist without a state file */
-		persistJournalState();
-	}
-	sd_journal_close(j);
+	closeJournal(j);
 	ratelimitDestruct(ratelimiter);
 ENDafterRun
 

configure.ac
@@ -460,7 +460,7 @@ AC_ARG_ENABLE(imjournal,
         [enable_imjournal="no"]
 )
 if test "x$enable_imjournal" = "xyes"; then
-	PKG_CHECK_MODULES([LIBSYSTEMD_JOURNAL], [libsystemd >= 209] ,, [
+	PKG_CHECK_MODULES([LIBSYSTEMD_JOURNAL], [libsystemd >= 234] ,, [
 	    PKG_CHECK_MODULES([LIBSYSTEMD_JOURNAL], [libsystemd-journal >= 197])
 	])
 fi

commit 4736e53d471ac45024333588fcdf5bce5f8c61b8

imjournal bugfix: module did not work at all
The open function was broken by commit 92ac801 (v8.30.0),
resulting in no data being ever read from the journal.

patch bases on the idea of Radovan Sroka given here:
https://github.com/rsyslog/rsyslog/issues/1895#issuecomment-339017357
but follows the current imjournal-paradigm of having the journal
handle inside a global variable.

see also https://github.com/rsyslog/rsyslog/issues/1895
closes https://github.com/rsyslog/rsyslog/issues/1897

plugins/imjournal/imjournal.c
@@ -118,20 +118,20 @@ static sd_journal *j;
 static rsRetVal persistJournalState(void);
 static rsRetVal loadJournalState(void);
 
-static rsRetVal openJournal(sd_journal* jj) {
+static rsRetVal openJournal(void) {
 	DEFiRet;
 
-	if (sd_journal_open(&jj, SD_JOURNAL_LOCAL_ONLY) < 0)
+	if (sd_journal_open(&j, SD_JOURNAL_LOCAL_ONLY) < 0)
 		iRet = RS_RET_IO_ERROR;
 	RETiRet;
 }
 
-static void closeJournal(sd_journal* jj) {
+static void closeJournal(void) {
 
 	if (cs.stateFile) { /* can't persist without a state file */
 		persistJournalState();
 	}
-	sd_journal_close(jj);
+	sd_journal_close(j);
 }
 
 
@@ -513,10 +513,10 @@ pollJournal(void)
 		/* do not persist stateFile sd_journal_get_cursor will fail! */
 		char* tmp = cs.stateFile;
 		cs.stateFile = NULL;
-		closeJournal(j);
+		closeJournal();
 		cs.stateFile = tmp;
 
-		iRet = openJournal(j);
+		iRet = openJournal();
 		if (iRet != RS_RET_OK) {
 			char errStr[256];
 			rs_strerror_r(errno, errStr, sizeof(errStr));
@@ -773,13 +773,13 @@ ENDfreeCnf
 /* open journal */
 BEGINwillRun
 CODESTARTwillRun
-	iRet = openJournal(j);
+	iRet = openJournal();
 ENDwillRun
 
 /* close journal */
 BEGINafterRun
 CODESTARTafterRun
-	closeJournal(j);
+	closeJournal();
 	ratelimitDestruct(ratelimiter);
 ENDafterRun
 