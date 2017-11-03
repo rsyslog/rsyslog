rsyslog/rsyslog

commit 96b5fce0acd57e8ae39f053b9e20cb0c28911b80

Resolves #1171 omelasticsearch high availability addressing of ElasticSearch cluster Changing server parameter to accept an array of servers/uris and port to defaultPort if not specified on a given uri. Adding dedicated curl handle for checkConn and refactoring the curl setup a bit, using ES health check with timeout. Added checkConnFailure stat counting and HA test

tests/Makefile.am
@@ -349,6 +349,7 @@ endif
 if ENABLE_ELASTICSEARCH_TESTS
 TESTS +=  \
 	es-basic.sh \
+	es-basic-ha.sh \
 	es-basic-bulk.sh \
 	es-maxbytes-bulk.sh \
 	es-basic-errfile-empty.sh \

tests/es-basic-ha.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+# This file is part of the rsyslog project, released under ASL 2.0
+echo ===============================================================================
+echo \[es-basic-ha.sh\]: basic test for elasticsearch high availability functionality
+. $srcdir/diag.sh init
+. $srcdir/diag.sh es-init
+. $srcdir/diag.sh startup es-basic-ha.conf
+. $srcdir/diag.sh injectmsg  0 100
+. $srcdir/diag.sh wait-queueempty
+. $srcdir/diag.sh wait-for-stats-flush 'rsyslog.out.stats.log'
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown 
+. $srcdir/diag.sh es-getdata 100
+. $srcdir/diag.sh seq-check  0 99
+# The configuration makes every other request from message #3 fail checkConn (N/2-1)
+. $srcdir/diag.sh custom-content-check '"failed.checkConn": 49' 'rsyslog.out.stats.log'
+. $srcdir/diag.sh exit

tests/testsuites/es-basic-ha.conf
@@ -1,0 +1,16 @@
+$IncludeConfig diag-common.conf
+
+template(name="tpl" type="string"
+	 string="{\"msgnum\":\"%msg:F,58:2%\"}")
+
+
+ruleset(name="stats") {
+  action(type="omfile" file="./rsyslog.out.stats.log")
+}
+module(load="../plugins/impstats/.libs/impstats" interval="1" severity="7" resetCounters="off" Ruleset="stats" bracketing="on" format="json")
+
+module(load="../plugins/omelasticsearch/.libs/omelasticsearch")
+:msg, contains, "msgnum:" action(type="omelasticsearch"
+				 server=["localhost", "http://localhost/", "localhost:9201"]
+				 template="tpl"
+				 searchIndex="rsyslog_testbench")

commit 22375d3b339f6339d0262861c00d445232a9d858

omelasticsearch: fix memory leak and potential misadressing
Commit 96b5fce introduced regressions, leading to potential misadressing
and a very probable memory leak. This commit fixes the issues and
hardens the module to better detect some error cases in the
future.

It also adds valgrind-based testbench tests which ensure that no
pointer errors exist. If they had been in place, the regressions
would never have been undetected.

Note that commit 96b5fce was introduced in 8.23.0 on Nov, 15th 2016.

Many thanks to Yaroslav Bo for alerting me on the root problem and
providing a very good analysis and guidance.

see also https://github.com/rsyslog/rsyslog/issues/1906
closes https://github.com/rsyslog/rsyslog/issues/1962
see also https://github.com/rsyslog/rsyslog/issues/1964

plugins/omelasticsearch/omelasticsearch.c
@@ -362,6 +362,11 @@ incrementServerIndex(wrkrInstanceData_t *pWrkrData)
 	pWrkrData->serverIndex = (pWrkrData->serverIndex + 1) % pWrkrData->pData->numServers;
 }
 
+
+/* checks if connection to ES can be established; also iterates over
+ * potential servers to support high availability (HA) feature. If it
+ * needs to switch server, will record new one in curl handle.
+ */
 static rsRetVal ATTR_NONNULL()
 checkConn(wrkrInstanceData_t *const pWrkrData)
 {
@@ -517,7 +522,7 @@ done:
 }
 
 
-static rsRetVal
+static rsRetVal ATTR_NONNULL()
 setPostURL(wrkrInstanceData_t *pWrkrData, instanceData *pData, uchar **tpls)
 {
 	uchar *searchIndex = NULL;
@@ -1202,8 +1207,14 @@ curlPost(wrkrInstanceData_t *pWrkrData, uchar *message, int msglen, uchar **tpls
 	pWrkrData->reply = NULL;
 	pWrkrData->replyLen = 0;
 
-	//CHKiRet(checkConn(pWrkrData));
+	if(pWrkrData->pData->numServers > 1) {
+		/* needs to be called to support ES HA feature */
+		CHKiRet(checkConn(pWrkrData));
+	}
 	CHKiRet(setPostURL(pWrkrData, pWrkrData->pData, tpls));
+
+	pWrkrData->reply = NULL;
+	pWrkrData->replyLen = 0;
 
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, (char *)message);
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, msglen);
@@ -1272,18 +1283,21 @@ CODESTARTdoAction
 	if(pWrkrData->pData->bulkmode) {
 		const size_t nBytes = computeMessageSize(pWrkrData, ppString[0], ppString);
 
-		/* If max bytes is set and this next message will put us over the limit, submit the current buffer and reset */
-		if (pWrkrData->pData->maxbytes > 0 && es_strlen(pWrkrData->batch.data) + nBytes > pWrkrData->pData->maxbytes ) {
-			dbgprintf("omelasticsearch: maxbytes limit reached, submitting partial batch of %d "
-			"elements.\n", pWrkrData->batch.nmemb);
+		/* If max bytes is set and this next message will put us over the limit,
+		* submit the current buffer and reset */
+		if(pWrkrData->pData->maxbytes > 0
+			&& es_strlen(pWrkrData->batch.data) + nBytes > pWrkrData->pData->maxbytes ) {
+			dbgprintf("omelasticsearch: maxbytes limit reached, submitting partial "
+			"batch of %d elements.\n", pWrkrData->batch.nmemb);
 			CHKiRet(submitBatch(pWrkrData));
 			initializeBatch(pWrkrData);
 		}
 		CHKiRet(buildBatch(pWrkrData, ppString[0], ppString));
 
-		/* If there is only one item in the batch, all previous items have been submitted or this is the first item
-		   for this transaction. Return previous committed so that all items leading up to the current (exclusive)
-		   are not replayed should a failure occur anywhere else in the transaction. */
+		/* If there is only one item in the batch, all previous items have been
+	 	 * submitted or this is the first item for this transaction. Return previous
+		 * committed so that all items leading up to the current (exclusive)
+		 * are not replayed should a failure occur anywhere else in the transaction. */
 		iRet = pWrkrData->batch.nmemb == 1 ? RS_RET_PREVIOUS_COMMITTED : RS_RET_DEFER_COMMIT;
 	} else {
 		CHKiRet(curlPost(pWrkrData, ppString[0], strlen((char*)ppString[0]),
@@ -1299,7 +1313,8 @@ CODESTARTendTransaction
 	if (pWrkrData->batch.data != NULL && pWrkrData->batch.nmemb > 0) {
 		CHKiRet(submitBatch(pWrkrData));
 	} else {
-		dbgprintf("omelasticsearch: endTransaction, pWrkrData->batch.data is NULL, nothing to send. \n");
+		dbgprintf("omelasticsearch: endTransaction, pWrkrData->batch.data is NULL, "
+			"nothing to send. \n");
 	}
 finalize_it:
 ENDendTransaction
@@ -1644,8 +1659,6 @@ CODESTARTnewActInst
 		pData->searchIndex = (uchar*) strdup("system");
 	if(pData->searchType == NULL)
 		pData->searchType = (uchar*) strdup("events");
-	//assert(pData->searchIndex != NULL);
-	//assert(pData->searchType != NULL);
 
 CODE_STD_FINALIZERnewActInst
 	cnfparamvalsDestruct(pvals, &actpblk);

tests/Makefile.am
@@ -469,7 +469,9 @@ TESTS +=  \
 endif
 if HAVE_VALGRIND
 TESTS +=  \
-	es-basic-vg.sh
+	es-basic-vg.sh \
+	es-basic-bulk-vg.sh \
+	es-basic-ha-vg.sh
 endif
 endif
 
@@ -1058,6 +1060,9 @@ EXTRA_DIST= \
 	testsuites/es-bulk-errfile-popul-erronly-interleaved.conf \
 	es-bulk-errfile-popul-def-interleaved.sh \
 	testsuites/es-bulk-errfile-popul-def-interleaved.conf \
+	es-basic-vg.sh \
+	es-basic-bulk-vg.sh \
+	es-basic-ha-vg.sh
 	linkedlistqueue.sh \
 	testsuites/linkedlistqueue.conf \
 	da-mainmsg-q.sh \

tests/es-basic-bulk-vg.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+# This file is part of the rsyslog project, released under ASL 2.0
+echo ===============================================================================
+echo \[es-basic-bulk\]: basic test for elasticsearch functionality
+. $srcdir/diag.sh init
+. $srcdir/diag.sh es-init
+. $srcdir/diag.sh startup-vg es-basic-bulk.conf
+. $srcdir/diag.sh injectmsg  0 10000
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown-vg
+. $srcdir/diag.sh es-getdata 10000
+. $srcdir/diag.sh seq-check  0 9999
+. $srcdir/diag.sh exit

tests/es-basic-ha-vg.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+# This file is part of the rsyslog project, released under ASL 2.0
+echo ===============================================================================
+echo \[es-basic-ha.sh\]: basic test for elasticsearch high availability functionality
+. $srcdir/diag.sh init
+. $srcdir/diag.sh es-init
+. $srcdir/diag.sh startup-vg es-basic-ha.conf
+. $srcdir/diag.sh injectmsg  0 100
+. $srcdir/diag.sh wait-queueempty
+. $srcdir/diag.sh wait-for-stats-flush 'rsyslog.out.stats.log'
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown-vg
+. $srcdir/diag.sh es-getdata 100
+. $srcdir/diag.sh seq-check  0 99
+# The configuration makes every other request from message #3 fail checkConn (N/2-1)
+. $srcdir/diag.sh custom-content-check '"failed.checkConn": 49' 'rsyslog.out.stats.log'
+. $srcdir/diag.sh exit

tests/es-basic-vg.sh
@@ -1,0 +1,11 @@
+#!/bin/bash
+# This file is part of the rsyslog project, released under ASL 2.0
+. $srcdir/diag.sh init
+. $srcdir/diag.sh es-init
+. $srcdir/diag.sh startup-vg es-basic.conf
+. $srcdir/diag.sh injectmsg  0 10000
+. $srcdir/diag.sh shutdown-when-empty
+. $srcdir/diag.sh wait-shutdown-vg
+. $srcdir/diag.sh es-getdata 10000
+. $srcdir/diag.sh seq-check  0 9999
+. $srcdir/diag.sh exit