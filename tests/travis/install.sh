9b78440a06c94f2c04542e4635da5ef6a73dea98
Hugo SOSZYNSKI <hugo.soszynski@advens.fr>
2017-11-09 14:29:39 +0100

    Trying to not specify the prefix

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -qq --force-yes libestr-dev librelp-dev libfastjson-dev lib
 	liblognorm-dev \
 	libcurl4-gnutls-dev
 sudo apt-get install -qq python-docutils
-env
+
 if [ "$DISTRIB_CODENAME" == "trusty" ] || [ "$DISTRIB_CODENAME" == "precise" ]; then
 	set -ex
 	WANT_MAXMIND=1.2.0
@@ -18,7 +18,7 @@ if [ "$DISTRIB_CODENAME" == "trusty" ] || [ "$DISTRIB_CODENAME" == "precise" ]; 
 	wget https://github.com/mongodb/mongo-c-driver/releases/download/1.1.5/mongo-c-driver-1.1.5.tar.gz
 	tar -xzf mongo-c-driver-1.1.5.tar.gz
 	cd mongo-c-driver-1.1.5/
-	./configure --prefix=/usr --libdir=/usr/lib64 --enable-ssl
+	./configure --enable-ssl
 	make
 	sudo make install
 	set +x
