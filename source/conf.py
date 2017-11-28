de991ff81f8a4ef253d125deda0f1240e0b6f528
Rainer Gerhards <rgerhards@adiscon.com>
2017-11-28 09:42:07 +0100

    fix copyright

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'rsyslog'
-copyright = u'2008-2017, Rainer Gerhards and Adiscon'
+copyright = u'2008-2017, Rainer Gerhards and Others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
