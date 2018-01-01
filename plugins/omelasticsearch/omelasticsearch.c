92ea253d4e5b4e4a3ff0c7af16239eb3a2a5497a
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-01 18:13:52 +0100

    omelasticsearch: theoretical NULL pointer access
    commit 0fbe873438e introduced an unchecked realloc, which under
    extreme conditions (total out-of-memory condition) could lead to
    a NULL pointer access. This commit fixes the issue be avoiding the
    need for realloc() in the first place.
    
    Note that commit 0fbe873438e was merged today, so there is no
    problem in any released code.

--- plugins/omelasticsearch/omelasticsearch.c
+++ plugins/omelasticsearch/omelasticsearch.c
@@ -1089,8 +1089,10 @@ writeDataError(wrkrInstanceData_t *const pWrkrData,
 	 */
 	DBGPRINTF("omelasticsearch: error record: '%s'\n", rendered);
 	toWrite = strlen(rendered) + 1;
-	rendered = realloc(rendered, toWrite);
-	strcat(rendered, "\n");
+	/* Note: we overwrite the '\0' terminator with '\n' -- so we avoid
+	 * caling malloc() -- write() does NOT need '\0'!
+	 */
+	rendered[toWrite-1] = '\n'; /* NO LONGER A STRING! */
 	wrRet = write(pData->fdErrFile, rendered, toWrite);
 	if(wrRet != (ssize_t) toWrite) {
 		DBGPRINTF("omelasticsearch: error %d writing error file, write returns %lld\n",
