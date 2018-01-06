e134b545b334d0245cfd0957ff5cbaebf6ba5064
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-06 11:40:07 +0100

    mmpstrucdata: make case preservation of SD_IDs optional
    commit 5cee06a60 introduced case-perservation, but did not make
    it optional. This broke existing behavior and thus potentially
    existing configurations. This commit introduces a new parameter
    "sd_name.lowercase" which now controls this behavior.
    
    Note that 5cee06a60 is merged to master branch together with this
    commit, so no released code ever had the potential regression.
    
    see also https://github.com/rsyslog/rsyslog/pull/2181

--- tests/mmpstrucdata-case.sh
+++ tests/mmpstrucdata-case.sh
@@ -3,6 +3,7 @@
 # correctly parsed.
 # This file is part of the rsyslog project, released  under ASL 2.0
 # rgerhards, 2015-04-30
+. $srcdir/diag.sh init
 
 uname
 if [ `uname` = "FreeBSD" ] ; then
@@ -10,9 +11,6 @@ if [ `uname` = "FreeBSD" ] ; then
    exit 77
 fi
 
-echo ===============================================================================
-echo \[mmpstrucdata-invalid.sh\]: testing mmpstrucdata with invalid SD
-. $srcdir/diag.sh init
 . $srcdir/diag.sh startup mmpstrucdata-case.conf
 . $srcdir/diag.sh wait-startup
 . $srcdir/diag.sh tcpflood -m100 -M "\"<161>1 2003-03-01T01:00:00.000Z mymachine.example.com tcpflood - tag [tcpflood@32473 eventID=\\\"1011\\\"] valid structured data\""
