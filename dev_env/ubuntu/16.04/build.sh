79d154a5f389a537c0126a1491198f2a4daa04ef
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-13 17:05:24 +0100

    dev/ubuntu: split container into two
    one with regular rsyslog libraries (libfastjson etc) and one via
    the libraries compilied from current git master branch.

--- dev_env/ubuntu/16.04/build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf common
-cp -r ../../common common
-docker build --rm -t rsyslog/rsyslog_dev_ubuntu:16.04 .
-rm -rf common
---
+++ dev_env/ubuntu/base/16.04/build.sh
@@ -0,0 +1,1 @@
+docker build $1 -t rsyslog/rsyslog_dev_base_ubuntu:16.04 .
