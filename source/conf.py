rsyslog/rsyslog

commit de28b1a5d2f9c5b606ca4065354f1578f7e9cf61

Build process: dynamic version, release labels
- Toss (now) redundant release build script
- Clean version and release values used as labels for stable branch and release tag builds
- Build ALL branches from origin, not just local branches
- Build tarball of latest release tag
- Stable release +1 version label, date + commit hash used as labels for all other branches
- Use path provided by command-line for build output or use hard-coded default
- Add epub as standard build format

refs rsyslog/rsyslog-doc#424

source/conf.py
@@ -13,6 +13,7 @@
 
 import datetime
 import os
+import re
 import subprocess
 import sys
 
@@ -67,12 +68,23 @@ project = u'rsyslog'
 copyright = u'2008-2017, Rainer Gerhards and Others'
 author = u'Rainer Gerhards and Others'
 
+# Generate the current stable version number from the latest git tag
+git_tag_output = subprocess.check_output(['git', 'tag', '--list', "v*"]).decode("utf-8").strip()
+git_tag_list = re.sub('[A-Za-z]', '', git_tag_output).split('\n')
+git_tag_list.sort(key=lambda s: map(int, s.split('.')))
+git_tag_latest = git_tag_list[-1]
+
+
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '8.32'
+current_version = git_tag_latest[:-2]
+
+# Break apart 'x.y' value, increment y and then concatenate into 'x.y' again
+version = "{}.{}".format(int(current_version[:1]), int(current_version[-2:]) + 1)
+
 
 DATE = datetime.date.today()
 TODAY = DATE.strftime('%Y%m%d')

build.sh
@@ -1,27 +1,154 @@
-#!/bin/sh
+#!/bin/bash
 
+# Do not allow use of unitilized variables
+set -u
+
+# Exit if any statement returns a non-true value
+set -e
+
+echo ""
 echo "This script is intended to run with the clean repo version of the code."
 echo "Run the sphinx-build command manually if you want to see your uncommited changes."
-echo "If you run this command with uncommitted and un-pushed changes, you will lose those changes,"
-echo "or end up with merge conflicts."
+echo "If you run this script with uncommitted and un-pushed changes, YOU WILL LOSE THOSE CHANGES!"
 echo ""
 echo "Press Enter to continue or Ctrl-C to cancel...."
-read
+read -r REPLY
 
-MASTERBRANCH=v8-devel
 
-for version in $(git branch | cut -c3-)
-  do
-    ver=$MASTERBRANCH
-    [ "$version" = master ] || ver=$version
-    echo "Checkout Branch $version"
-    git checkout $version
-    echo "Fetch Branch $version"
-    git fetch origin $version
-    echo "Reset Branch $version"
+###############################################################
+# Refresh local content (needs to run first)
+##############################################################
+
+echo "Fetching latest changes from origin ..."
+git fetch origin --prune
+
+# We need the tags also. The standard "git fetch origin" call does
+# not appear to also grab the tags.
+git fetch origin --tags
+
+
+##############################################################
+# Setup
+##############################################################
+
+# Formats that are built by default for new releases
+declare -a formats
+formats=(
+  epub
+  html
+)
+
+branches=($(git branch -a | grep remotes | grep origin | grep -v "HEAD" | sed -e 's/ //g' -e 's#remotes/origin/##g'))
+latest_stable_branch=$(git branch -a | grep remotes | grep origin | grep -v "HEAD" | sed -e 's/ //g' -e 's#remotes/origin/##g' | grep -E 'v[0-9]+' | tail -n 1)
+dev_branches=($(git branch -a | grep remotes | grep origin | grep -v "HEAD" | sed -e 's/ //g' -e 's#remotes/origin/##g' | grep -v "$latest_stable_branch"))
+
+# Build release docs for latest official stable version
+latest_stable_tag=$(git tag --list 'v*' | sort --version-sort | grep -Eo '^v[.0-9]+$' | tail -n 1)
+
+# The latest stable tag, but without the leading 'v'
+latest_stable_version=$(echo $latest_stable_tag | sed "s/[A-Za-z]//g")
+
+# tarball representing the documentation for the latest stable release
+doc_tarball="rsyslog-doc-${latest_stable_version}.tar.gz"
+
+
+# Allow user to pass in the location for generated files
+if [[ -z ${1+x} ]]; then
+    output_dir="/tmp/rsyslog-doc-builds"
+    echo -e "\nWARNING: Output directory not specified, falling back to default: $output_dir"
+else
+    output_dir=$1
+    echo "Generated files will be placed in ${output_dir}"
+    # otherwise if user didn't specify, go ahead and set a default
+
+fi
+
+
+##############################################################
+# Functions
+##############################################################
+
+prep_branch_for_build() {
+
+    branch=$1
+
+    echo "Reset Branch $branch"
     git reset --hard
-    echo "Pull Branch $version"
-    git pull origin $version
-    echo "Build $version"
-    sphinx-build -b html source $ver
+
+    echo "Tossing all non-committed changes"
+    git clean --force -d
+
+    echo "Checkout Branch $branch"
+    git checkout origin/$branch ||
+      { echo "[!] Checkout $branch failed... aborting"; exit 1; }
+
+    echo "Pull Branch $branch"
+    git pull origin $branch ||
+      { echo "[!] Pull $branch failed... aborting"; exit 1; }
+
+}
+
+
+###############################################################
+# Prep work
+###############################################################
+
+mkdir -p ${output_dir}
+
+
+
+###############################################################
+# Build formats for each dev branch
+##############################################################
+
+# Prep each branch for fresh build
+for branch in "${dev_branches[@]}"
+do
+    prep_branch_for_build $branch
+
+    for format in "${formats[@]}"
+    do
+        echo "Building $format for $branch branch"
+
+        # Use values calculated by conf.py (geared towards dev builds) instead
+        # of overriding them.
+        sphinx-build -b $format source $output_dir/$branch ||
+          { echo "[!] sphinx-build $format failed for $branch branch ... aborting"; exit 1; }
+
+    done
 done
+
+
+###############################################################
+# Build formats for stable branch
+###############################################################
+
+prep_branch_for_build $latest_stable_branch
+
+for format in "${formats[@]}"
+do
+    echo "Building $format for $latest_stable_branch branch"
+
+    # Override verbose release value calculated by conf.py that is intended
+    # for dev builds (version, date, commit hash)
+    sphinx-build -b $format source $output_dir/$latest_stable_branch -D release="$latest_stable_version" ||
+      { echo "[!] sphinx-build $format failed for $latest_stable_branch branch ... aborting"; exit 1; }
+done
+
+
+###############################################################
+# Build latest stable tag
+###############################################################
+
+git reset --hard
+git clean --force -d
+git checkout $latest_stable_tag
+
+# Unless we override the "release" option, the default settings within
+# the conf.py file will label generated content with specific date/commit
+# info for easy visual reference
+sphinx-build -b html source build -D release="$latest_stable_version" ||
+    { echo echo "[!] sphinx-build failed for $latest_stable_tag tag ... aborting"; exit 1; }
+
+tar -czf $output_dir/$doc_tarball source build LICENSE README.md build.sh ||
+    { echo echo "[!] tarball creation failed for $latest_stable_tag tag ... aborting"; exit 1; }

release_build.sh
@@ -1,21 +1,0 @@
-#!/bin/sh
-
-# this is used to create an rsyslog release tarball.
-# it must be executed from the root of the rsyslog-doc
-# project.
-
-docfile=rsyslog-doc.tar.gz
-
-[ ! -d ./build ] || rm -rf ./build
-
-sphinx-build -b html source build || {
-	echo "sphinx-build failed... aborting"
-	exit 1
-}
-
-[ ! -e ./$docfile ] || rm -rf ./$docfile
-
-tar -czf $docfile build source LICENSE README.md build.sh || {
-	echo "Failed to create rsyslog-doc.tar.gz tarball..."
-	exit 1
-}

commit 9571a99d9312ebd60453d9de38b3b095495a93ae

Sphinx build conf: backout de28b1a changes
Commit de28b1a introduced dynamic versioning of content,
but failed to take into account building the docs
from the tarball instead of from a local git repo.

As a quick fix, this commit changes the conf back
to an equivalent static version so that the doc
sources build as-is without the git dependency.

refs rsyslog/rsyslog-doc#504, rsyslog/rsyslog-doc#424

source/conf.py
@@ -65,40 +65,19 @@ project = u'rsyslog'
 copyright = u'2008-2017, Rainer Gerhards and Others'
 author = u'Rainer Gerhards and Others'
 
-# Generate the current stable version number from the latest git tag
-git_tag_output = subprocess.check_output(['git', 'tag', '--list', "v*"]).decode("utf-8").strip()
-git_tag_list = re.sub('[A-Za-z]', '', git_tag_output).split('\n')
-git_tag_list.sort(key=lambda s: [int(u) for u in s.split('.')])
-git_tag_latest = git_tag_list[-1]
-
+DATE = datetime.date.today()
+TODAY = DATE.strftime('%Y%m%d')
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-current_version = git_tag_latest[:-2]
+version = '8.32'
 
-# Break apart 'x.y' value, increment y and then concatenate into 'x.y' again
-version = "{}.{}".format(int(current_version[:1]), int(current_version[-2:]) + 1)
-
-
-DATE = datetime.date.today()
-TODAY = DATE.strftime('%Y%m%d')
-
-# e.g., v8.29.0-98-g07d02c6 (after decoding)
-# https://stackoverflow.com/questions/2502833/store-output-of-subprocess-popen-call-in-a-string
-git_describe_output = subprocess.check_output(['git', 'describe']).decode("utf-8")
-
-# Grab the last portion of the strings, then strip off leading 'g'
-commit = git_describe_output.split('-')[-1][1:]
-
-# This is displayed in multiple prominent locations in the docs
-# where it is useful to be able to tell at a glance when the files
-# were generated and from what commit.
-#
 # The full version, including alpha/beta/rc tags.
-release = "{}-{}-{}".format(version, TODAY, commit)
+release = '8.32.0'
+
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.