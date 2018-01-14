a8426f507a7fca9749b98b82870776b2271578a4
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-14 17:31:12 +0100

    travis: do not unnecessarily run checks under cron

--- devtools/ci/travis/do-run.sh
+++ devtools/ci/travis/do-run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+	if [ "$DO_CRON" != "YES" ]; then
+		echo cron job not executed under non-cron run
+		exit 0 # this must not run under PRs
+	fi
+	exit
+fi
+
 export RSYSLOG_HOME=`pwd`
 echo running tests/travis/$RUN, home: $RSYSLOG_HOME
 $RSYSLOG_HOME/tests/travis/$RUN
