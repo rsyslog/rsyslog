9da5eb8cd6399023e2a912b2b0cae43a037b2028
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-17 17:46:15 +0100

    devtools: permit to override default container

--- devtools/devcontainer.sh
+++ devtools/devcontainer.sh
@@ -8,14 +8,16 @@ if [ "$RSYSLOG_HOME" == "" ]; then
 	echo info: RSYSLOG_HOME not set, using $RSYSLOG_HOME
 fi
 
-DEV_CONTAINER=`cat $RSYSLOG_HOME/devtools/default_dev_container`
+if [ -z "$RSYSLOG_DEV_CONTAINER" ]; then
+	RSYSLOG_DEV_CONTAINER=`cat $RSYSLOG_HOME/devtools/default_dev_container`
+fi
 
 printf "/rsyslog is mapped to $RSYSLOG_HOME\n"
-docker pull $DEV_CONTAINER
+docker pull $RSYSLOG_DEV_CONTAINER
 docker run \
 	-u `id -u`:`id -g` \
 	-e RSYSLOG_CONFIGURE_OPTIONS_EXTRA \
 	-e CC \
 	-e CFLAGS \
 	$DOCKER_RUN_EXTRA_FLAGS \
-	-v "$RSYSLOG_HOME":/rsyslog $DEV_CONTAINER  $*
+	-v "$RSYSLOG_HOME":/rsyslog $RSYSLOG_DEV_CONTAINER $*
