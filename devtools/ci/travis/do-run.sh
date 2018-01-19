9052f535fdc4cb0a5e32eb929873d618f41ba159
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-19 08:58:30 +0100

    travis fix: cron job was not executed

--- devtools/ci/travis/do-run.sh
+++ devtools/ci/travis/do-run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-	if [ "$DO_CRON" != "YES" ]; then
-		echo cron job not executed under non-cron run
-		exit 0 # this must not run under PRs
-	fi
-	exit
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ] &&  [ "$DO_CRON" != "YES" ]; then
+	echo cron job not executed under non-cron run
+	exit 0 # this must not run under PRs
 fi
 
 export RSYSLOG_HOME=`pwd`
