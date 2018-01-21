9a95684126c5280367a8b1bd6aa6f45820051d21
deoren <deoren@users.noreply.github.com>
2018-01-21 00:52:33 -0600

    Add footer for every page
    A footer has been created and is included by all pages via an
    include statement within the rst_epilog build conf option.
    This footer is an attempt to display common "go here for help"
    options that should be made prominent for newcomers to the
    project.

--- source/conf.py
+++ source/conf.py
@@ -72,6 +72,14 @@ rst_prolog = """
 
 """
 
+# http://www.sphinx-doc.org/en/stable/config.html#confval-rst_epilog
+# This will be included at the end of every source file that is read.
+rst_epilog = """
+
+.. include:: /footer.inc.rst
+
+"""
+
 
 
 
