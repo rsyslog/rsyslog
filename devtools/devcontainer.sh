de88a5984dbe9c70b674ab0577e7cc73727b2166
Rainer Gerhards <rgerhards@adiscon.com>
2018-01-30 08:09:41 +0100

    devtools: run dev container under current user credentials

--- devtools/devcontainer.sh
+++ devtools/devcontainer.sh
@@ -7,7 +7,9 @@ DEV_CONTAINER=`cat $RSYSLOG_HOME/devtools/default_dev_container`
 
 printf "/rsyslog is mapped to $RSYSLOG_HOME\n"
 docker pull $DEV_CONTAINER
-docker run -e RSYSLOG_CONFIGURE_OPTIONS_EXTRA \
+docker run \
+	-u `id -u`:`id -g` \
+	-e RSYSLOG_CONFIGURE_OPTIONS_EXTRA \
 	-e CC \
 	-e CFLAGS \
 	-v "$RSYSLOG_HOME":/rsyslog -ti $DEV_CONTAINER  $*
