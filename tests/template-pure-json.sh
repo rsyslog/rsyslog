93bebb1f1f6ba2ae257666b47738c80112f8569f
Rainer Gerhards <rgerhards@adiscon.com>
2018-02-12 12:59:27 +0100

    template: add option to generate json "container"
    This enables easy JSON generation via template.
    
    This commit also corrects an issue with the constant "jsonf"
    format. That was recently added, and the implementation problem
    only became visible when used inside a larger json object. No
    officially released code is affected, thought - so it rellay
    is just a side-note.
    
    closes https://github.com/rsyslog/rsyslog/issues/2347

--- tests/template-pure-json.sh
+++ tests/template-pure-json.sh
@@ -3,9 +3,9 @@
 . $srcdir/diag.sh init
 . $srcdir/diag.sh generate-conf
 . $srcdir/diag.sh add-conf '
-template(name="outfmt" type="list") {
+template(name="outfmt" type="list" option.jsonf="on") {
+	 property(outname="message" name="msg" format="jsonf")
 	 constant(outname="@version" value="1" format="jsonf")
-	 constant(value="\n")
 }
 
 local4.* action(type="omfile" file="rsyslog.out.log" template="outfmt")
@@ -14,5 +14,5 @@ local4.* action(type="omfile" file="rsyslog.out.log" template="outfmt")
 . $srcdir/diag.sh injectmsg 0 1
 . $srcdir/diag.sh shutdown-when-empty
 . $srcdir/diag.sh wait-shutdown
-. $srcdir/diag.sh content-cmp '"@version": "1"'
+. $srcdir/diag.sh content-cmp '{"message":" msgnum:00000000:", "@version": "1"}'
 . $srcdir/diag.sh exit
