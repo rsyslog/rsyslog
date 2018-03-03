cd38e5460f4d1e7273b3794f51b0cfa46e6fbcb0
Rainer Gerhards <rgerhards@adiscon.com>
2018-03-03 17:35:27 +0100

    devcontainer: use some sensible defaults if params not set

--- devtools/devcontainer.sh
+++ devtools/devcontainer.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # This scripts uses an rsyslog development container to execute given
 # command inside it.
-
 set -e
+
+if [ "$RSYSLOG_HOME" == "" ]; then
+	export RSYSLOG_HOME=$(pwd)
+	echo info: RSYSLOG_HOME not set, using $RSYSLOG_HOME
+fi
+
 DEV_CONTAINER=`cat $RSYSLOG_HOME/devtools/default_dev_container`
 
 printf "/rsyslog is mapped to $RSYSLOG_HOME\n"
@@ -12,4 +17,5 @@ docker run \
 	-e RSYSLOG_CONFIGURE_OPTIONS_EXTRA \
 	-e CC \
 	-e CFLAGS \
+	$DOCKER_RUN_EXTRA_FLAGS \
 	-v "$RSYSLOG_HOME":/rsyslog $DEV_CONTAINER  $*
