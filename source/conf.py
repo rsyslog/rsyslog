811488348d468c18997554d1e82d84d556240539
Rainer Gerhards <rgerhards@adiscon.com>
2018-05-15 08:23:28 +0200

    preparing for 8.35.0 release

--- source/conf.py
+++ source/conf.py
@@ -94,8 +94,8 @@ rst_epilog = """
 ###############################################################################
 
 
-version = '8.34'
-release = '8.34.0'
+version = '8.35'
+release = '8.35.0'
 
 
 # For this to be true, it means that we are not attempting to build from
