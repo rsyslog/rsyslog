8d49eb61d40fd876ebb9a4b9ab95ee295b089fb8
Rainer Gerhards <rgerhards@adiscon.com>
2018-06-15 16:29:22 +0200

    testbench: run test that require IPv6 stack only when available
    First test was introduced in openssl work; failed when no IPv6 was
    present.  Now it skips.

--- tests/sndrcv_tls_ossl_anon_ipv6.sh
+++ tests/sndrcv_tls_ossl_anon_ipv6.sh
@@ -3,4 +3,5 @@
 # This file is part of the rsyslog project, released  under ASL 2.0
 echo ===============================================================================
 echo \[sndrcv_tls_ossl_anon_ipv6.sh\]: testing sending and receiving via TLS with anon auth using bare ipv6, no SNI
+. $srcdir/diag.sh check-ipv6-available
 . $srcdir/sndrcv_drvr.sh sndrcv_tls_ossl_anon_ipv6 10000
