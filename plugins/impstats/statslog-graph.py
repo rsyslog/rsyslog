c8da318ded499a3bbfda47d2ac9cc9dc99a4541c
Andre Lorbach <alorbach@adiscon.com>
2018-07-02 11:54:56 +0200

    impstats: Updated python based statslog analyzer sample scripts.
    Changed files from ANSi to UTF8

--- plugins/impstats/statslog-graph.py
+++ plugins/impstats/statslog-graph.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 	* Copyright (C) 2013 Adiscon GmbH.
+# 	* Copyright (C) 2013-2018 Adiscon GmbH.
 #	* This file is part of RSyslog
 #	* 
 #	* This script processes csv stats logfiles created by statslog-splitter.py and creates graphs 
@@ -14,12 +14,14 @@
 import sys
 import datetime
 import time
-import os
+#import os
 import pygal 
 
 # Set default variables
 szInput = ""
 szOutputFile = ""
+szChartName = ""
+szCounters = ""
 bHelpOutput = False
 nMaxDataCount = 25
 bUseDateTime = True
@@ -29,11 +31,14 @@ bConvertPng = False
 bLogarithmicChart = False
 bFilledLineChart = False
 bChartCalcDelta = False
+bDeltaIgnoreFirstValue = False
+bDeltaIgnoreNegativeValues = False
 
 # Init variables
 aFields = []
 aData = {}
 aMajorXData = []
+aCounters = None
 
 # Helper variables
 nDataRecordCound = 0 
@@ -55,6 +60,10 @@ for arg in sys.argv: # [-4:]:
 		bUseDateTime = False
 	elif arg.find("--convertpng") != -1:
 		bConvertPng = True
+	elif arg.find("--deltaignorefirstvalue") != -1:
+		bDeltaIgnoreFirstValue = True
+	elif arg.find("--deltaignorenegativevalues") != -1:
+		bDeltaIgnoreNegativeValues = True
 	elif arg.find("--linechart") != -1:
 		bLineChart = True
 		bBarChart = False
@@ -67,6 +76,8 @@ for arg in sys.argv: # [-4:]:
 		bFilledLineChart = True
 	elif arg.find("--chartscalcdelta") != -1:
 		bChartCalcDelta = True
+	elif arg.find("--counters=") != -1:
+		szCounters = arg[11:]
 	elif arg.find("--h") != -1 or arg.find("-h") != -1 or arg.find("--help") != -1:
 		bHelpOutput = True
 
@@ -105,86 +116,111 @@ else:
 		print "Error, no input file specified!"
 		sys.exit(0)
 
+	# Generate Counters Array if necessary
+	if len(szCounters) > 0: 
+		aCounters = szCounters.strip().split(";")
+
+	# Open Errorlog on init
+	errorlog = open(szOutputFile + ".corrupted.log", 'w')
+
 	# Process inputfile
-	inputfile = open(szInput, 'r')
-	aLineDataPrev = [] # Helper variable for previous line!
-	for line in inputfile.readlines():
-		if nLineCount == 0: 
-			aFields = line.strip().split(";")
-			# remove last item if empty
-			if len(aFields[len(aFields)-1]) == 0: 
-				aFields.pop()
-			#print aFields
-			#sys.exit(0)
-			
-			#Init data arrays
-			for field in aFields:
-				aData[field] = []
-		else:
-			aLineData = line.strip().split(";")
-			# remove last item if empty
-			if len(aLineData[len(aLineData)-1]) == 0: 
-				aLineData.pop()
+	with open(szInput) as inputfile:
+		aLineDataPrev = [] # Helper variable for previous line!
+		for line in inputfile:
+			if nLineCount == 0: 
+				aFields = line.strip().split(";")
+				# remove last item if empty
+				if len(aFields[len(aFields)-1]) == 0: 
+					aFields.pop()
 
-			# Loop Through line data
-			iFieldNum = 0
-			for field in aFields:
-				if iFieldNum == 0:
-					if bUseDateTime: 
-						aData[field].append( datetime.datetime.strptime(aLineData[iFieldNum],"%Y/%b/%d %H:%M:%S") )
+				#print aFields
+				#sys.exit(0)
+
+				#Init data arrays
+				for field in aFields:
+					aData[field] = []
+			else:
+				aLineData = line.strip().split(";")
+				# remove last item if empty
+				if len(aLineData[len(aLineData)-1]) == 0: 
+					aLineData.pop()
+
+				# Loop Through line data
+				iFieldNum = 0
+				for field in aFields:
+					if iFieldNum == 0:
+						if bUseDateTime: 
+							aData[field].append( datetime.datetime.strptime(aLineData[iFieldNum],"%Y/%b/%d %H:%M:%S") )
+						else:
+							# Convert Time String into UNIX Timestamp
+							myDateTime = datetime.datetime.strptime(aLineData[iFieldNum],"%Y/%b/%d %H:%M:%S")
+							iTimeStamp = int(time.mktime(myDateTime.timetuple()))
+
+							# Init Start Seconds 
+							if iStartSeconds == 0: 	 
+								iStartSeconds = iTimeStamp
+
+							# Set data field
+							aData[field].append( iTimeStamp - iStartSeconds )
+					elif iFieldNum == 2 and szChartName == "":
+						szChartName = aLineData[iFieldNum]
+					elif iFieldNum > 2:
+						# Check if we need to calculate Deltas!
+						if bChartCalcDelta and len(aLineDataPrev) > 0 and field.find("size") == -1: # Do not calc delta für SIZE!
+							try:
+								iPreviousVal = int(aLineDataPrev[iFieldNum])
+								iCurrentVal = int(aLineData[iFieldNum])
+								if iCurrentVal != 0:	# Calc DELTA
+									iCurrentDelta = iCurrentVal - iPreviousVal
+									if bDeltaIgnoreNegativeValues:
+										if iCurrentDelta >= 0:
+											aData[field].append(iCurrentDelta)
+										else:
+											aData[field].append(0)
+									else:
+										aData[field].append(iCurrentDelta)
+								else:	# Don't Calc delta value!
+									aData[field].append( iCurrentVal )
+							except ValueError:
+								errorlog.write("Error, Field not an INTEGER: " + aLineData[iFieldNum])
+						else:
+							try:			# Try as INTEGER
+								aData[field].append( int(aLineData[iFieldNum]) )
+							except ValueError:	# Not an INTEGER, skip value
+								errorlog.write("Error, Field not an INTEGER: " + aLineData[iFieldNum])
 					else:
-						# Convert Time String into UNIX Timestamp
-						myDateTime = datetime.datetime.strptime(aLineData[iFieldNum],"%Y/%b/%d %H:%M:%S")
-						iTimeStamp = int(time.mktime(myDateTime.timetuple()))
+						aData[field].append( aLineData[iFieldNum] )
 
-						# Init Start Seconds 
-						if iStartSeconds == 0: 	 
-							iStartSeconds = iTimeStamp
+					iFieldNum += 1
+					# print aData[field[nLineCount]] 
+				
+				# Increment counter
+				nDataRecordCound += 1
 
-						# Set data field
-						aData[field].append( iTimeStamp - iStartSeconds )
-				elif iFieldNum > 2:
-					# Check if we need to calculate Deltas!
-					if bChartCalcDelta and len(aLineDataPrev) > 0: 
-						iPreviousVal = int(aLineDataPrev[iFieldNum])
-						iCurrentVal = int(aLineData[iFieldNum])
-						if iCurrentVal != 0:	# Calc DELTA
-							aData[field].append(iCurrentVal - iPreviousVal)
-						else:			# Don't Calc delta value!
-							aData[field].append( iCurrentVal )
-					else:
-						aData[field].append( int(aLineData[iFieldNum]) )
-				else:
-					aData[field].append( aLineData[iFieldNum] )
+				# in case deltas need to be calculated, Store current line into previous line 
+				if bChartCalcDelta: 
+					aLineDataPrev = aLineData
 
-				iFieldNum += 1
-				# print aData[field[nLineCount]] 
-			
 			# Increment counter
-			nDataRecordCound += 1
+			nLineCount += 1
 
-			# in case deltas need to be calculated, Store current line into previous line 
-			if bChartCalcDelta: 
-				aLineDataPrev = aLineData
+	#		if nLineCount > 25:
+	#			break
 
-		# Increment counter
-		nLineCount += 1
-
-#		if nLineCount > 25:
-#			break
-
-#	if nMaxDataCount > 0: 
-#		# Check if we need to reduce the data amount
-#		nTotalDataCount = len( aData[aFields[0]] )
-#		nDataStepCount = nTotalDataCount / (nMaxDataCount)
-#		if nTotalDataCount > nMaxDataCount:
-#			for iDataNum in reversed(range(0, nTotalDataCount)):
-#				# Remove all entries who 
-#				if iDataNum % nDataStepCount == 0:
-#					aMajorXData.append( aData[aFields[0]][iDataNum] )
+	#	if nMaxDataCount > 0: 
+	#		# Check if we need to reduce the data amount
+	#		nTotalDataCount = len( aData[aFields[0]] )
+	#		nDataStepCount = nTotalDataCount / (nMaxDataCount)
+	#		if nTotalDataCount > nMaxDataCount:
+	#			for iDataNum in reversed(range(0, nTotalDataCount)):
+	#				# Remove all entries who 
+	#				if iDataNum % nDataStepCount == 0:
+	#					aMajorXData.append( aData[aFields[0]][iDataNum] )
 			
 	# Import Style
 #	from pygal.style import LightSolarizedStyle
+#	from pygal.style import DefaultStyle
+	from pygal.style import RotateStyle
 
 	# Create Config object
 	from pygal import Config
@@ -204,42 +240,49 @@ else:
 	if nMaxDataCount > 0: 
 		chartCfg.show_minor_x_labels=False
 		chartCfg.x_labels_major_count=nMaxDataCount
-	chartCfg.js = [	'svg.jquery.js','pygal-tooltips.js' ] # Use script from local
-#	chartCfg.style = LightSolarizedStyle
-	chartCfg.print_values = False
+	#chartCfg.style = DefaultStyle #LightSolarizedStyle
+	chartCfg.style = RotateStyle('#FF0000')
+	chartCfg.print_values = True #False
 	chartCfg.print_zeroes = True
 	chartCfg.no_data_text = "All values are 0"
 	if bLogarithmicChart: 
 		chartCfg.logarithmic=True	# Makes chart Y-Axis data more readable
+	
+	# remove first item if configured!
+	if bDeltaIgnoreFirstValue: 
+		for iChartNum in range(3, len(aFields) ):
+			if aCounters is None or aFields[iChartNum] in aCounters:
+				aData[ aFields[iChartNum] ].pop(0)
 
 	# Create Linechart
 	if bLineChart:
 		myChart = pygal.Line(chartCfg)
-		myChart.title = 'Line Chart of "' + szInput + '"'
+		myChart.title = 'Line Chart of "' + szChartName + '"'
 		myChart.x_title = "Time elasped in seconds"
 		myChart.x_labels = map(str, aData[aFields[0]] ) 
 #		myChart.x_labels_major = map(str, aMajorXData )
 		for iChartNum in range(3, len(aFields) ):
-			myChart.add(aFields[iChartNum], aData[ aFields[iChartNum] ])  # Add some values
+			if aCounters is None or aFields[iChartNum] in aCounters:
+				myChart.add(aFields[iChartNum], aData[ aFields[iChartNum] ])  # Add some values
 	# Create BarChart
 	elif bBarChart: 
 		myChart = pygal.Bar(chartCfg)
-		myChart.title = 'Bar Chart of "' + szInput + '"'
+		myChart.title = 'Bar Chart of "' + szChartName + '"'
 		myChart.x_title = "Time elasped in seconds"
 		myChart.x_labels = map(str, aData[aFields[0]] ) 
 #		myChart.x_labels_major = map(str, aMajorXData )
 		for iChartNum in range(3, len(aFields) ):
-			myChart.add(aFields[iChartNum], aData[ aFields[iChartNum] ])  # Add some values
+			if aCounters is None or aFields[iChartNum] in aCounters:
+				myChart.add(aFields[iChartNum], aData[ aFields[iChartNum] ])  # Add some values
+#		for iChartNum in range(3, len(aFields) ):
+#			myChart.add(aFields[iChartNum], aData[ aFields[iChartNum] ])  # Add some values
 	
 	# Render Chart now and output to file!
-	myChart.render_to_file(szOutputFile)
-
-	# Convert to PNG and remove SVG
 	if bConvertPng: 
 		szPngFileName = szOutputFile[:-4] + ".png"
-		iReturn = os.system("cairosvg " + szOutputFile + " -f png -o " + szPngFileName)
-		print "File SVG converted to PNG: '" + szPngFileName + "', return value of cairosvg: " + str(iReturn)
-		os.remove(szOutputFile)
+		myChart.render_to_png(szPngFileName)
+	else: 
+		myChart.render_to_file(szOutputFile)
 
 	# Finished
 	sys.exit(0)
