869d765596f42407677d7b1ce4d656dd5d70116a
Rainer Gerhards <rgerhards@adiscon.com>
2018-07-14 11:34:01 +0200

    testbench: fix tests that look awfully wrong
    These tests indicated they terminate rsyslog forcefully without
    drainging the queues, but then checked if they were drained (all
    messages processed). That does not make sense, and I cannot
    envison why I wrote this in the first place. So I assume some
    copy&paste problem was the root of that.

--- tests/arrayqueue.sh
+++ tests/arrayqueue.sh
@@ -2,18 +2,12 @@
 # Test for fixedArray queue mode
 # added 2009-05-20 by rgerhards
 # This file is part of the rsyslog project, released  under ASL 2.0
-echo ===============================================================================
-echo \[arrayqueue.sh\]: testing queue fixedArray queue mode
 . $srcdir/diag.sh init
 . $srcdir/diag.sh startup arrayqueue.conf
 
 # 40000 messages should be enough
 . $srcdir/diag.sh injectmsg  0 40000
-
-# terminate *now* (don't wait for queue to drain!)
-kill `cat rsyslog.pid`
-
-# now wait until rsyslog.pid is gone (and the process finished)
+. $srcdir/diag.sh shutdown-when-empty
 . $srcdir/diag.sh wait-shutdown 
 . $srcdir/diag.sh seq-check 0 39999
 . $srcdir/diag.sh exit
--- tests/linkedlistqueue.sh
+++ tests/linkedlistqueue.sh
@@ -2,17 +2,10 @@
 # Test for Linkedlist queue mode
 # added 2009-05-20 by rgerhards
 # This file is part of the rsyslog project, released  under GPLv3
-echo \[linkedlistqueue.sh\]: testing queue Linkedlist queue mode
 . $srcdir/diag.sh init
 . $srcdir/diag.sh startup linkedlistqueue.conf
-
-# 40000 messages should be enough
 . $srcdir/diag.sh injectmsg  0 40000
-
-# terminate *now* (don't wait for queue to drain)
-kill `cat rsyslog.pid`
-
-# now wait until rsyslog.pid is gone (and the process finished)
+. $srcdir/diag.sh shutdown-when-empty
 . $srcdir/diag.sh wait-shutdown 
 . $srcdir/diag.sh seq-check 0 39999
 . $srcdir/diag.sh exit
