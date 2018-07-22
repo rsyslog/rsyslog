c5658b0a4004b6a0714c80d5a386395631d93046
Rainer Gerhards <rgerhards@adiscon.com>
2018-07-22 14:36:41 +0200

    create dev env for opensuse tumbleweed

--- dev_env/suse/base/tumbleweed/setup-projects.sh
+++ dev_env/suse/base/tumbleweed/setup-projects.sh
@@ -5,13 +5,6 @@
 # end config settings
 export CI_HOME=`pwd`
 echo CI_HOME: $CI_HOME
-mkdir $CI_HOME/proj
-cd $CI_HOME/proj
-git clone https://github.com/rsyslog/libestr.git
-cd libestr
-git pull
-autoreconf -fvi && ./configure --prefix=/usr/local && make -j2 || exit $?
-$SUDO make -j2 install || exit $?
 
 printf "\n\n================== libfastjson =======================\n\n"
 cd $CI_HOME/proj
@@ -21,14 +14,6 @@ git pull
 autoreconf -fvi && ./configure --prefix=/usr/local --disable-journal && make -j2 || exit $?
 $SUDO make -j2 install || exit $?
 
-
-printf "\n\n================== liblogging =======================\n\n"
-cd $CI_HOME/proj
-git clone https://github.com/rsyslog/liblogging.git
-cd liblogging
-git pull
-autoreconf -fvi && ./configure --prefix=/usr/local --disable-journal && make -j2 || exit $?
-$SUDO make -j2 install || exit $?
 
 printf "\n\n================== liblognorm =======================\n\n"
 cd $CI_HOME/proj
@@ -45,19 +30,3 @@ cd librelp
 git pull
 autoreconf -fvi && ./configure --prefix=/usr/local --enable-compile-warnings=yes && make -j2 || exit $?
 $SUDO make -j2 install || exit $?
-
-
-### main project ###
-printf "\n\n================== rsyslog =======================\n\n"
-printf "This is primarily built to make sure that the helper\n"
-printf "projects are correctly built and installed\n\n"
-
-cd $CI_HOME/proj
-git clone https://github.com/rsyslog/rsyslog.git
-cd rsyslog
-git pull
-mkdir utils
-echo "./configure --prefix=/usr/local $RSYSLOG_CONFIGURE_OPTIONS" --enable-compile-warnings=yes > utils/conf
-chmod +x utils/conf
-
-autoreconf -fvi && utils/conf && make -j2 || exit $?
