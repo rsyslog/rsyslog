82c3003728948fa776b71f2b0d89b892170c4198
Rainer Gerhards <rgerhards@adiscon.com>
2018-07-31 18:35:44 +0200

    squash: simplify test

--- tests/rscript_backticks-vg.sh
+++ tests/rscript_backticks-vg.sh
@@ -2,15 +2,12 @@
 . $srcdir/diag.sh init
 generate_conf
 add_conf '
-#module(load="../plugins/imtcp/.libs/imtcp")
-#input(type="imtcp" port="13514")
 template(name="outfmt" type="string" string="%msg:F,58:2%\n")
 
 if `echo $DO_WORK` == "on" and $msg contains "msgnum:" then
-	action(type="omfile" file=`echo $OUTFILE` template="outfmt")
+	action(type="omfile" file=`echo $RSYSLOG_OUT_LOG` template="outfmt")
 '
 export DO_WORK=on
-export OUTFILE=rsyslog.out.log
 startup_vg
 . $srcdir/diag.sh injectmsg 0 1000
 #. $srcdir/diag.sh tcpflood -m10
