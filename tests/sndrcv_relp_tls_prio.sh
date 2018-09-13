b7b4453cff8c75f0a723d77cf105766dd2d429bc
Rainer Gerhards <rgerhards@adiscon.com>
2018-09-13 13:24:14 +0200

    testbench: make imrelp tests faster and avoid imtcp
    imtcp was primarily used for historical reasons and is actually
    to inferior method of injection messages for such cases

--- tests/sndrcv_relp_tls_prio.sh
+++ tests/sndrcv_relp_tls_prio.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # added 2013-12-10 by Rgerhards
 # This file is part of the rsyslog project, released under ASL 2.0
-echo ===============================================================================
-echo \[sndrcv_relp_tls_prio.sh\]: testing sending and receiving via relp with TLS enabled and priority string set
+. $srcdir/diag.sh init
+echo testing sending and receiving via relp with TLS enabled and priority string set
 
 # uncomment for debugging support:
-. $srcdir/diag.sh init
 # start up the instances
 #export RSYSLOG_DEBUG="debug nostdout noprintmutexaction"
 export RSYSLOG_DEBUGLOG="log"
@@ -23,32 +22,23 @@ startup
 export RSYSLOG_DEBUGLOG="log2"
 #valgrind="valgrind"
 generate_conf 2
-export TCPFLOOD_PORT="$(get_free_port)" # TODO: move to diag.sh
 add_conf '
 module(load="../plugins/omrelp/.libs/omrelp")
-module(load="../plugins/imtcp/.libs/imtcp")
-input(type="imtcp" port="'$TCPFLOOD_PORT'")	/* this port for tcpflood! */
 
 action(type="omrelp" target="127.0.0.1" port="'$PORT_RCVR'" tls="on" tls.prioritystring="NORMAL:+ANON-DH")
 ' 2
 startup 2
-# may be needed by TLS (once we do it): sleep 30
 
 # now inject the messages into instance 2. It will connect to instance 1,
 # and that instance will record the data.
-tcpflood -m50000 -i1
-sleep 5 # make sure all data is received in input buffers
-# shut down sender when everything is sent, receiver continues to run concurrently
-# may be needed by TLS (once we do it): sleep 60
+injectmsg 1 50000
+
+# shut down sender
 shutdown_when_empty 2
 wait_shutdown 2
 # now it is time to stop the receiver as well
 shutdown_when_empty
 wait_shutdown
 
-# may be needed by TLS (once we do it): sleep 60
-# do the final check
 seq_check 1 50000
-
-unset PORT_RCVR # TODO: move to exit_test()?
 exit_test
