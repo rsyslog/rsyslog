26db406974215f7ce26552c29a0edb47e46c01ee
Rainer Gerhards <rgerhards@adiscon.com>
2018-09-26 18:55:31 +0200

    cleanup: remove no longer needed code
    array mode templates are no longer supported
    
    If they are needed again, check v7 for importing code

--- template.h
+++ template.h
@@ -163,7 +163,6 @@ int tplRequiresDateCall(struct template *pTpl);
  * BEFORE msg.h, even if your code file does not actually need it.
  * rgerhards, 2007-08-06
  */
-rsRetVal tplToArray(struct template *pTpl, smsg_t *pMsg, uchar*** ppArr, struct syslogTime *ttNow);
 rsRetVal tplToJSON(struct template *pTpl, smsg_t *pMsg, struct json_object **, struct syslogTime *ttNow);
 rsRetVal doEscape(uchar **pp, rs_size_t *pLen, unsigned short *pbMustBeFreed, int escapeMode);
 rsRetVal
