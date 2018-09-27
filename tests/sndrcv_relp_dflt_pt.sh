d6be99a85f9240d9077347b598e5238146e74ead
Rainer Gerhards <rgerhards@adiscon.com>
2018-09-27 11:56:12 +0200

    testbench: re-enable test sndrcv_relp_dflt_pt.sh
    Test was originally only executed when root tests were enabled,
    which routinely was not the case. There was not even a hard reason
    to handle it that way. Also, the test had a couple of defects which
    have also been fixed.
    
    closes https://github.com/rsyslog/rsyslog/issues/3064

--- tests/sndrcv_relp_dflt_pt.sh
+++ tests/sndrcv_relp_dflt_pt.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
+# this checks if omrelp works with the default port
+# Note: imrelp requires the port, so we cannot and must not
+# check the "default port"
 # added 2013-12-10 by Rgerhards
 # This file is part of the rsyslog project, released under ASL 2.0
-# the first test is redundant, but we keep it when we later make the port
-# configurable.
-if [ `./omrelp_dflt_port` -lt 1024 ]; then
+. $srcdir/diag.sh init
+relp_port=$(./omrelp_dflt_port)
+if [ $relp_port -lt 1024 ]; then
     if [ "$EUID" -ne 0 ]; then
+	echo relp default port $relp_port is priviledged
 	echo need to be root to run this test - skipping
         exit 77
     fi
 fi
 
-if [ `./omrelp_dflt_port` -ne 13515 ]; then
-    echo this test needs configure option --enable-omrelp-default-port=13515 to work
-    exit 77
-fi
-exit
-
 # uncomment for debugging support:
-. $srcdir/diag.sh init
 # start up the instances
 #export RSYSLOG_DEBUG="debug nostdout noprintmutexaction"
 export RSYSLOG_DEBUGLOG="log"
 generate_conf
-export PORT_RCVR="$(get_free_port)"
 add_conf '
 module(load="../plugins/imrelp/.libs/imrelp")
 # then SENDER sends to this port (not tcpflood!)
-input(type="imrelp" port="'$PORT_RCVR'")
+input(type="imrelp" port="'$relp_port'")
 
 $template outfmt,"%msg:F,58:2%\n"
 :msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
@@ -39,10 +35,9 @@ export TCPFLOOD_PORT="$(get_free_port)"
 add_conf '
 module(load="../plugins/omrelp/.libs/omrelp")
 
-action(type="omrelp" protocol="tcp" target="127.0.0.1" port="'$PORT_RCVR'")
+action(type="omrelp" target="127.0.0.1")
 ' 2
 startup 2
-# may be needed by TLS (once we do it): sleep 30
 
 # now inject the messages into instance 2. It will connect to instance 1,
 # and that instance will record the data.
