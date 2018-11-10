57c66a033655c51d37114c1266a30887d1003b98
Rainer Gerhards <rgerhards@adiscon.com>
2018-11-10 15:05:11 +0100

    buildbot: gather script shall not pickup $RSYSLOG_DYNNAME*.log files
    previously, it also picked test output files if they were left over. For
    some test this meant very verbose data could be sent to buildbot master.
    We have seen cases with over 20million lines, which made the log view
    impossible. This has now been corrected.

--- tests/CI/gather_all_logs.sh
+++ tests/CI/gather_all_logs.sh
@@ -3,4 +3,6 @@
 # tests.
 echo gather all logs left from make check.
 echo you only need these if make check is terminated due to timeout
-ls -tr tests/*.log | xargs cat
+# IMPORTANT: ! -name "rstb_*" excludes $RSYSLOGDYNNAME*.log
+( cd tests ;
+find . -maxdepth 1 -print0 -name "*.log" ! -name "rstb_*" -exec cat {} +; )
