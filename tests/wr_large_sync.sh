ae06bcce0b1e490c789e72227c579619553bad11
Rainer Gerhards <rgerhards@adiscon.com>
2018-12-19 12:48:08 +0100

    testbench: make some more test more robust against slow CI machines
    also fix an issue with tests that use non-standard plumbing

--- tests/wr_large_sync.sh
+++ tests/wr_large_sync.sh
@@ -16,8 +16,6 @@ $InputTCPServerRun '$TCPFLOOD_PORT'
 
 $template outfmt,"%msg:F,58:2%,%msg:F,58:3%,%msg:F,58:4%\n"
 template(name="dynfile" type="string" string=`echo $RSYSLOG_OUT_LOG`) # trick to use relative path names!
-$OMFileFlushOnTXEnd off
-$OMFileFlushInterval 2
 $OMFileIOBufferSize 256k
 $OMFileAsyncWriting off
 local0.* ?dynfile;outfmt
@@ -25,7 +23,7 @@ local0.* ?dynfile;outfmt
 startup
 # send 4000 messages of 10.000bytes plus header max, randomized
 tcpflood -m4000 -r -d10000 -P129
-sleep 1 # due to large messages, we need this time for the tcp receiver to settle...
+wait_file_lines $RSYSLOG_OUT_LOG 4000
 shutdown_when_empty # shut down rsyslogd when done processing messages
 wait_shutdown       # and wait for it to terminate
 seq_check 0 3999 -E
