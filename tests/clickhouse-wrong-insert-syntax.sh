25ba4a67b746d459ff8852b59751def8599347ef
Rainer Gerhards <rgerhards@adiscon.com>
2019-01-01 11:03:21 +0100

    testbench: remove some clickhouse test dependency against non-TLS
    some tests had "http:" included in their expected response, and thus
    failed on TLS-secured systems. This has been fixed.
    
    Note that the tests still depend on english language clickhouse error
    messages. We could probably improve here as well.

--- tests/clickhouse-wrong-insert-syntax.sh
+++ tests/clickhouse-wrong-insert-syntax.sh
@@ -20,12 +20,10 @@ add_conf '")
 clickhouse-client --query="CREATE TABLE IF NOT EXISTS rsyslog.wrongInsertSyntax ( id Int32, severity Int8, facility Int8, timestamp DateTime, ipaddress String, tag String, message String ) ENGINE = MergeTree() PARTITION BY severity Order By id"
 
 startup
-tcpflood -m1 -M "\"<129>Mar 10 01:00:00 172.20.245.8 tag: msgnum:00000001\""
+tcpflood -m1
 shutdown_when_empty
 wait_shutdown
 
-export EXPECTED="{ \"request\": { \"url\": \"http:\\/\\/localhost:8123\\/\", \"postdata\": \"INSERT INTO rsyslog.wrongInsertSyntax (id, severity, facility, timestamp, ipaddress, tag, message) VLUES (00000001, 1, 16, '1520643600', '127.0.0.1', 'tag:', ' msgnum:00000001')\" }, \"reply\": \"Code: 62, e.displayText() = DB::Exception: Syntax error: failed at position 100: VLUES (00000001, 1, 16, '1520643600', '127.0.0.1', 'tag:', ' msgnum:00000001'). Expected one of: FORMAT, VALUES, SELECT, WITH, e.what() = DB::Exception\\n\" }"
-cmp_exact $RSYSLOG_OUT_LOG
-
+content_check "DB::Exception: Syntax error"
 clickhouse-client --query="DROP TABLE rsyslog.wrongInsertSyntax"
 exit_test
