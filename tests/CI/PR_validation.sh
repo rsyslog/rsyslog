02f8ebd491dc10f7011e4db23538eead38454d44
Rainer Gerhards <rgerhards@adiscon.com>
2019-03-03 17:44:52 +0100

    CI: improve error messages in PR validation

--- tests/CI/PR_validation.sh
+++ tests/CI/PR_validation.sh
@@ -15,19 +15,38 @@ printf '%d commits in feature branch:\n' $(wc -l < gitlog)
 cat gitlog
 printf '\n'
 if [ $(wc -l < gitlog) -ge 5 ]; then
-	cat <<- EOF
-	This feature branch contains quite some commits. Consider squashing it.
+	cat >&2 <<- EOF
+	This feature branch contains quite some commits. Consider squashing them.
+
+	Note: you can use
+	$ git commit -a --amend
+	to apply changes to a previous commit. No new commit will be created if
+	done so.
 
 	EOF
 	exitcode=1
 fi
-if grep "Merge " gitlog; then
-	cat <<- EOF
+if grep -q "Merge " gitlog; then
+	grep "Merge " gitlog
+	cat >&2 <<- EOF
 	This feature branch contains merge commits. This almost always indicates that it
 	contains unwanted merges where a rebase should have been applied.
 	Please consider squashing the branch and/or rebasing it to current master.
 
 	EOF
 	exitcode=1
+fi
+
+if [ "$exitcode" != "0" ]; then
+	cat >&2 <<- EOF
+	======================================================================
+	You possibly wonder why this CI check complains. See
+
+	https://rainer.gerhards.net/2019/03/squash-your-pull-requests.html
+
+	for the reasons and ways to fix the problems. It also describes what
+	happens if you do/want/can not fix the problems found by this check.
+	======================================================================
+	EOF
 fi
 exit $exitcode
