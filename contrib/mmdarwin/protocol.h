a69978c78073ef74198d5f20cde7e0b7b9d745c1
Guillaume Catto <guillaume.catto@advens.fr>
2019-04-09 17:39:05 +0200

    Change license header to ASL 2.0

--- contrib/mmdarwin/protocol.h
+++ contrib/mmdarwin/protocol.h
@@ -1,15 +1,17 @@
-/// \file     protocol.h
-/// \authors  hsoszynski
-/// \version  1.0
-/// \date     05/06/18
-/// \license  GPLv3
-/// \brief    Copyright (c) 2018 Advens. All rights reserved.
-
-/*
- *        WARNING!
- * DO NOT MODIFY THIS FILE.
+/* Copyright 2019 Advens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+     * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 #ifndef DARWIN_PROTOCOL_H
 # define DARWIN_PROTOCOL_H
 
