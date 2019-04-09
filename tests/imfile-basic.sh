e81e1fcd1a357c9a19b9f1f15c3f51d295a4a514
Rainer Gerhards <rgerhards@adiscon.com>
2019-04-09 18:47:59 +0200

    imfile: emit error on startup if no working directory is set
    When the work directory has not been set or is invalid, state files
    are created in the root of the file system. This is neither expected
    nor desirable. We now complain loudly about this fact. For backwards
    compatibility reasons, we still need to support running imfile in
    this case.
    
    closes https://github.com/rsyslog/rsyslog/issues/1296

--- tests/imfile-basic.sh
+++ tests/imfile-basic.sh
@@ -3,12 +3,16 @@
 . ${srcdir:=.}/diag.sh init
 export NUMMESSAGES=50000
 generate_conf
+# NOTE: do NOT set a working directory!
 add_conf '
 module(load="../plugins/imfile/.libs/imfile")
 input(type="imfile" File="./'$RSYSLOG_DYNNAME'.input" tag="file:")
 
 template(name="outfmt" type="string" string="%msg:F,58:2%\n")
-:msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+if $msg contains "msgnum:" then
+	action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+else
+	action(type="omfile" file="'$RSYSLOG_DYNNAME'.othermsgs")
 '
 # make sure file exists when rsyslog starts up
 touch $RSYSLOG_DYNNAME.input
@@ -18,4 +22,5 @@ wait_file_lines
 shutdown_when_empty
 wait_shutdown
 seq_check
+content_check "imfile: no working directory set" $RSYSLOG_DYNNAME.othermsgs
 exit_test
