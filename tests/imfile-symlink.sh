5c6b2904873a603005deeaf36eee4019ad6fa4e9
Rainer Gerhards <rgerhards@adiscon.com>
2019-05-06 13:18:09 +0200

    testbench bugfix: imfile-symlink failed w/ parallel test run
    This test sometimes failed. It used a symlinkl to a hardcoded name
    rsyslog-link.*.log. This symlink was created but then disappears.
    The reason is that upon (every!) test exit, rsyslog-link.*.log is
    deleted. So a parallel test running the exit procedure just at the
    "right" time can removed that file.
    
    The bug is that the file name should be created using the tests's
    dynamic name. This is done now.
    
    closes https://github.com/rsyslog/rsyslog/issues/3550

--- tests/imfile-symlink.sh
+++ tests/imfile-symlink.sh
@@ -45,20 +45,18 @@ startup
 for i in $(seq 2 $IMFILEINPUTFILES);
 do
 	printf '\ncreating %s\n' $RSYSLOG_DYNNAME.targets/$i.log
-	set -x
 	./inputfilegen -m 1 -i $((i-1)) > $RSYSLOG_DYNNAME.targets/$i.log
 	ls -l $RSYSLOG_DYNNAME.targets/$i.log
-	ln -sv $RSYSLOG_DYNNAME.targets/$i.log rsyslog-link.$i.log
-	ln -sv rsyslog-link.$i.log $RSYSLOG_DYNNAME.input.$i.log
+	ln -sv $RSYSLOG_DYNNAME.targets/$i.log $RSYSLOG_DYNNAME.link.$i.log
+	ln -sv $RSYSLOG_DYNNAME.link.$i.log $RSYSLOG_DYNNAME.input.$i.log
 	printf '%s generated file %s\n' "$(tb_timestamp)" "$i"
-	ls -l rsyslog-link.$i.log
-	set +x
+	ls -l $RSYSLOG_DYNNAME.link.$i.log
 	# wait until this file has been processed
 	content_check_with_count "HEADER msgnum:000000" $i $IMFILECHECKTIMEOUT
 done
 
 ./inputfilegen -m $IMFILELASTINPUTLINES > $RSYSLOG_DYNNAME.input.$((IMFILEINPUTFILES + 1)).log
-ls -l $RSYSLOG_DYNNAME.input.* rsyslog-link.* $RSYSLOG_DYNNAME.targets
+ls -l $RSYSLOG_DYNNAME.input.* $RSYSLOG_DYNNAME.link.* $RSYSLOG_DYNNAME.targets
 
 # Content check with timeout
 content_check_with_count "input.11.log" $IMFILELASTINPUTLINES $IMFILECHECKTIMEOUT
