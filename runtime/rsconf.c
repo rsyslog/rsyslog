rsyslog/rsyslog

commit e22fb205a3e84f3e9def61201c82902eb92fce56

config processing: check disk queue file is unique
If the same name is specified for multiple queues, the queue files
will become corrupted. This commit adds a check during config parsing.
If duplicate names are detected the config parser errors out and the
related object is not created.

Note: this may look to a change-of-behaviour to some users. However,
this never worked and it was pure luck that these users did not run
into big problems (e.g. DA queues were never going to disk at the
same time). So it is acceptable to error out in this hard error case.

closes https://github.com/rsyslog/rsyslog/issues/1385

runtime/rsconf.c
@@ -635,6 +635,7 @@ static inline rsRetVal
 tellCoreConfigLoadDone(void)
 {
 	DBGPRINTF("telling rsyslog core that config load for %p is done\n", loadConf);
+	qqueueDoneLoadCnf();
 	return glblDoneLoadCnf();
 }
 

runtime/queue.c
@@ -149,6 +149,14 @@ static struct cnfparamblk pblk =
 	  cnfpdescr
 	};
 
+/* support to detect duplicate queue file names */
+struct queue_filename {
+	struct queue_filename *next;
+	const char *dirname;
+	const char *filename;
+};
+struct queue_filename *queue_filename_root = NULL;
+
 /* debug aid */
 #if 0
 static inline void displayBatchState(batch_t *pBatch)
@@ -160,6 +168,21 @@ static inline void displayBatchState(batch_t *pBatch)
 }
 #endif
 static rsRetVal qqueuePersist(qqueue_t *pThis, int bIsCheckpoint);
+
+/* do cleanup when config is loaded */
+void qqueueDoneLoadCnf(void)
+{
+	struct queue_filename *next, *del;
+	next = queue_filename_root;
+	while(next != NULL) {
+		del = next;
+		next = next->next;
+		free((void*) del->filename);
+		free((void*) del->dirname);
+		free((void*) del);
+	}
+}
+
 
 /***********************************************************************
  * we need a private data structure, the "to-delete" list. As C does
@@ -3216,6 +3239,47 @@ finalize_it:
 	RETiRet;
 }
 
+/* check the the queue file name is unique. */
+static rsRetVal ATTR_NONNULL()
+checkUniqueDiskFile(qqueue_t *const pThis)
+{
+	DEFiRet;
+	struct queue_filename *queue_fn_curr = queue_filename_root;
+	struct queue_filename *newetry = NULL;
+	const char *const curr_dirname = (pThis->pszSpoolDir == NULL) ? "" : (char*)pThis->pszSpoolDir;
+
+	if(pThis->pszFilePrefix == NULL) {
+		FINALIZE; /* no disk queue! */
+	}
+
+	while(queue_fn_curr != NULL) {
+		if(!strcmp((const char*) pThis->pszFilePrefix, queue_fn_curr->filename) &&
+			!strcmp(curr_dirname, queue_fn_curr->dirname)) {
+			parser_errmsg("queue directory '%s' and file name prefix '%s' already used. "
+				"This is not possible. Please make it unique.",
+				curr_dirname, pThis->pszFilePrefix);
+			ABORT_FINALIZE(RS_RET_ERR_QUEUE_FN_DUP);
+			}
+		queue_fn_curr = queue_fn_curr->next;
+	}
+
+	/* name ok, so let's add it to the list */
+	CHKmalloc(newetry = calloc(1, sizeof(struct queue_filename)));
+	CHKmalloc(newetry->filename = strdup((char*) pThis->pszFilePrefix));
+	CHKmalloc(newetry->dirname = strdup(curr_dirname));
+	newetry->next = queue_filename_root;
+	queue_filename_root = newetry;
+
+finalize_it:
+	if(iRet != RS_RET_OK) {
+		if(newetry != NULL) {
+			free((void*)newetry->filename);
+			free((void*)newetry);
+		}
+	}
+	RETiRet;
+}
+
 /* apply all params from param block to queue. Must be called before
  * finalizing. This supports the v6 config system. Defaults were already
  * set during queue creation. The pvals object is destructed by this
@@ -3318,6 +3382,9 @@ qqueueApplyCnfParam(qqueue_t *pThis, struct nvlst *lst)
 			  "param '%s'\n", pblk.descr[i].name);
 		}
 	}
+
+	checkUniqueDiskFile(pThis);
+
 	if(pThis->qType == QUEUETYPE_DISK) {
 		if(pThis->pszFilePrefix == NULL) {
 			LogError(0, RS_RET_QUEUE_DISK_NO_FN, "error on queue '%s', disk mode selected, but "

runtime/queue.h
@@ -211,6 +211,7 @@ void qqueueSetDefaultsRulesetQueue(qqueue_t *pThis);
 void qqueueSetDefaultsActionQueue(qqueue_t *pThis);
 void qqueueDbgPrint(qqueue_t *pThis);
 rsRetVal qqueueShutdownWorkers(qqueue_t *pThis);
+void qqueueDoneLoadCnf(void);
 
 PROTOTYPEObjClassInit(qqueue);
 PROTOTYPEpropSetMeth(qqueue, iPersistUpdCnt, int);

runtime/rsyslog.h
@@ -581,6 +581,7 @@ enum rsRetVal_				/** return value. All methods return this if not specified oth
 	RS_RET_RABBITMQ_LOGIN_ERR = -2448, /**< RabbitMQ Login error */
 	RS_RET_RABBITMQ_CHANNEL_ERR = -2449, /**< RabbitMQ Connection error */
 	RS_RET_NO_WRKDIR_SET = -2450, /**< working directory not set, but desired by functionality */
+	RS_RET_ERR_QUEUE_FN_DUP = -2451, /**< duplicate queue file name */
 
 	/* RainerScript error messages (range 1000.. 1999) */
 	RS_RET_SYSVAR_NOT_FOUND = 1001, /**< system variable could not be found (maybe misspelled) */

tests/Makefile.am
@@ -235,6 +235,7 @@ TESTS +=  \
 	diskqueue.sh \
 	diskqueue-fsync.sh \
 	diskqueue-full.sh \
+	diskqueue-non-unique-prefix.sh \
 	rulesetmultiqueue.sh \
 	rulesetmultiqueue-v6.sh \
 	manytcp.sh \
@@ -1492,6 +1493,7 @@ EXTRA_DIST= \
 	diskq-rfc5424.sh \
 	diskqueue-full.sh \
 	diskqueue.sh \
+	diskqueue-non-unique-prefix.sh \
 	arrayqueue.sh \
 	include-obj-text-from-file.sh \
 	include-obj-outside-control-flow-vg.sh \

tests/diskqueue-non-unique-prefix.sh
@@ -1,0 +1,29 @@
+#!/bin/bash
+# Test for config parser to check that disk queue file names are
+# unique.
+# added 2019-05-02 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+generate_conf
+add_conf '
+global(workDirectory="'${RSYSLOG_DYNNAME}'.spool")
+if 0 then {
+	action(type="omfile" file="'$RSYSLOG_DYNNAME'.notused"
+		queue.filename="qf1" queue.type="linkedList")
+	action(type="omfile" file="'$RSYSLOG_DYNNAME'.notused"
+		queue.filename="qf1" queue.type="linkedList")
+	action(type="omfile" file="'$RSYSLOG_DYNNAME'.notused"
+		queue.filename="qf2" queue.type="linkedList")
+	action(type="omfile" file="'$RSYSLOG_DYNNAME'.notused"
+		queue.spooldirectory="'$RSYSLOG_DYNNAME'.spool2"
+		queue.filename="qf2" queue.type="linkedList")
+}
+
+action(type="omfile" file="'$RSYSLOG_OUT_LOG'")
+'
+startup
+shutdown_when_empty
+wait_shutdown
+content_check "and file name prefix 'qf1' already used"
+check_not_present "and file name prefix 'qf2' already used"
+exit_test

commit 0c503198d2d8f4fadc67c57a5297eb8327a4a307

core bugfix: segfault on startup depending on queue file names
rsyslog will segfault on startup when a main queue file name has
been set and at least on other queue contains a file name. This
was cased by too-early freeing config error-detection data
structures. It is a regression caused by commit e22fb205a3.

Thanks to Wade Simmons for reporting this issue and providing
detailled analysis. That greatly helps fixing it quickly.

closes https://github.com/rsyslog/rsyslog/issues/3681

runtime/rsconf.c
@@ -635,7 +635,6 @@ static inline rsRetVal
 tellCoreConfigLoadDone(void)
 {
 	DBGPRINTF("telling rsyslog core that config load for %p is done\n", loadConf);
-	qqueueDoneLoadCnf();
 	return glblDoneLoadCnf();
 }
 
@@ -878,6 +877,7 @@ activate(rsconf_t *cnf)
 	CHKiRet(activateMainQueue());
 	/* finally let the inputs run... */
 	runInputModules();
+	qqueueDoneLoadCnf(); /* we no longer need config-load-only data structures */
 
 	dbgprintf("configuration %p activated\n", cnf);
 