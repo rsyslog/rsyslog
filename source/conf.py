731599da6d05d26ee5df2e3780580638d61ed730
Rainer Gerhards <rgerhards@adiscon.com>
2019-08-19 12:52:39 +0200

    prepare for 8.1908.0 scheduled release

--- source/conf.py
+++ source/conf.py
@@ -94,8 +94,8 @@ rst_epilog = """
 ###############################################################################
 
 
-version = '8'
-release = version + ' dev build'
+version = '8.1908'
+release = '8.1908.0'
 
 
 # For this to be true, it means that we are not attempting to build from
