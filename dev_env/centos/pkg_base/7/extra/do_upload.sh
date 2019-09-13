31c1aabb4409ce1ac419d6cb83d34c2d03e640bb
Andre lorbach <alorbach@adiscon.com>
2019-09-13 11:04:22 +0200

    rpm: Fixed dockerfile for pkg_base centos and added dockerfiles for buildbot

--- dev_env/centos/pkg_base/7/extra/do_upload.sh
+++ dev_env/centos/pkg_base/7/extra/do_upload.sh
@@ -1,4 +1,4 @@
-!#/bin/sh
+#!/bin/sh
 # Definitions common to these scripts
 source $(dirname "$0")/config.sh
 
@@ -7,12 +7,19 @@ BRANCHES="v5-stable v7-stable v8-stable testing"
 echo "-------------------------------------"
 echo "--- Upload RPM Packages               ---"
 echo "-------------------------------------"
-echo "Which BRANCH do you want to upload?--"
-select szBranch in $BRANCHES
-do
-        echo "Uploading Branch '$szBranch'
-        "
-        break;
-done
 
-rsync -au -e "ssh -i /private-files/.ssh/id_rsa" --progress $szYumRepoDir/$szBranch/* $REPOUSERNAME@$REPOURL/$szBranch/
+if [ -z $RPM_REPO ]; then
+	echo "Which REPO do you want to upload?--"
+	select szSubRepo in $REPOOPTIONS
+	do
+		break;
+	done
+else
+	echo "REPO is set to '$RPM_REPO'"
+	szSubRepo=$RPM_REPO
+fi
+
+echo "Uploading Branch '$szYumRepoDir/$szSubRepo/' to $REPOUSERNAME@$REPOURL/$szSubRepo/
+"
+
+rsync -au -e "ssh -i /private-files/.ssh/id_rsa" --progress $szYumRepoDir/$szSubRepo/* $REPOUSERNAME@$REPOURL/$szSubRepo/
