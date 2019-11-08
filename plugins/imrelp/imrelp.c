5f64540469c0a9fa5e0d114b58aefd930b65e820
Rainer Gerhards <rgerhards@adiscon.com>
2019-11-08 11:28:10 +0100

    imrelp: remove unsafe debug instrumentation
    dbgprintf, which is not signal safe, was called from a signal handler
    to get better understanding during debugging. While this usually works,
    it can occasionally (5%) lead to a hang during shutdown. We have now
    removed that debug info as it is no longer vital.
    
    Note: this could only happen during debug runs. Production mode was
    not affected. As such, this fix is only revelevant to developers.
    
    However, it caused some confusion in the following issue tracker.
    see also https://github.com/rsyslog/rsyslog/issues/3941

--- plugins/imrelp/imrelp.c
+++ plugins/imrelp/imrelp.c
@@ -802,10 +802,8 @@ static void
 doSIGTTIN(int __attribute__((unused)) sig)
 {
 	const int bTerminate = ATOMIC_FETCH_32BIT(&bTerminateInputs, &mutTerminateInputs);
-	DBGPRINTF("imrelp: awoken via SIGTTIN; bTerminateInputs: %d\n", bTerminate);
 	if(bTerminate) {
 		relpEngineSetStop(pRelpEngine);
-		DBGPRINTF("imrelp: termination requested via SIGTTIN - telling RELP engine\n");
 	}
 }
 
