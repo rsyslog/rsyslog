rsyslog/rsyslog

commit 47982120408e6626a446ee1bfbd16ab56794beca

work around clang static analyzer false positives

runtime/tcpsrv.c
@@ -619,8 +619,8 @@ finalize_it:
 
 /* process a single workset item
  */
-static rsRetVal
-processWorksetItem(tcpsrv_t *pThis, nspoll_t *pPoll, int idx, void *pUsr)
+static rsRetVal ATTR_NONNULL(1)
+processWorksetItem(tcpsrv_t *const pThis, nspoll_t *pPoll, const int idx, void *pUsr)
 {
 	tcps_sess_t *pNewSess = NULL;
 	DEFiRet;
@@ -652,17 +652,22 @@ finalize_it:
 
 /* worker to process incoming requests
  */
-static void *
-wrkr(void *myself)
+static void * ATTR_NONNULL(1)
+wrkr(void *const myself)
 {
-	struct wrkrInfo_s *me = (struct wrkrInfo_s*) myself;
+	struct wrkrInfo_s *const me = (struct wrkrInfo_s*) myself;
 	
 	pthread_mutex_lock(&wrkrMut);
 	while(1) {
+		// wait for work, in which case pSrv will be populated
 		while(me->pSrv == NULL && glbl.GetGlobalInputTermState() == 0) {
 			pthread_cond_wait(&me->run, &wrkrMut);
 		}
-		if(glbl.GetGlobalInputTermState() == 1) {
+		if(me->pSrv == NULL) {
+			// only possible if glbl.GetGlobalInputTermState() == 1
+			// we need to query me->opSrv to avoid clang static
+			// analyzer false positive! -- rgerhards, 2017-10-23
+			assert(glbl.GetGlobalInputTermState() == 1);
 			--wrkrRunning;
 			break;
 		}

runtime/net.c
@@ -381,11 +381,12 @@ finalize_it:
  * *pbIsMatching is set to 0 if there is no match and something else otherwise.
  * rgerhards, 2008-05-27 */
 static rsRetVal
-PermittedPeerWildcardMatch(permittedPeers_t *pPeer, uchar *pszNameToMatch, int *pbIsMatching)
+PermittedPeerWildcardMatch(permittedPeers_t *const pPeer,
+	const uchar *pszNameToMatch,
+	int *const pbIsMatching)
 {
-	permittedPeerWildcard_t *pWildcard;
-	uchar *pC;
-	uchar *pStart; /* start of current domain component */
+	const permittedPeerWildcard_t *pWildcard;
+	const uchar *pC;
 	size_t iWildcard, iName; /* work indexes for backward comparisons */
 	DEFiRet;
 
@@ -413,7 +414,7 @@ PermittedPeerWildcardMatch(permittedPeers_t *pPeer, uchar *pszNameToMatch, int *
 			*pbIsMatching = 0;
 			FINALIZE;
 		}
-		pStart = pC;
+		const uchar *const pStart = pC; /* start of current domain component */
 		while(*pC != '\0' && *pC != '.') {
 			++pC;
 		}
@@ -436,10 +437,13 @@ PermittedPeerWildcardMatch(permittedPeers_t *pPeer, uchar *pszNameToMatch, int *
 				iName = (size_t) (pC - pStart) - pWildcard->lenDomainPart;
 				iWildcard = 0;
 				while(iWildcard < pWildcard->lenDomainPart) {
+					// I give up, I see now way to remove false positive -- rgerhards, 2017-10-23
+					#ifndef __clang_analyzer__
 					if(pWildcard->pszDomainPart[iWildcard] != pStart[iName]) {
 						*pbIsMatching = 0;
 						FINALIZE;
 					}
+					#endif
 					++iName;
 					++iWildcard;
 				}

runtime/net.h
@@ -157,7 +157,7 @@ BEGINinterface(net) /* name must also be changed in ENDinterface macro! */
 	/* permitted peer handling should be replaced by something better (see comments above) */
 	rsRetVal (*AddPermittedPeer)(permittedPeers_t **ppRootPeer, uchar *pszID);
 	rsRetVal (*DestructPermittedPeers)(permittedPeers_t **ppRootPeer);
-	rsRetVal (*PermittedPeerWildcardMatch)(permittedPeers_t *pPeer, uchar *pszNameToMatch, int *pbIsMatching);
+	rsRetVal (*PermittedPeerWildcardMatch)(permittedPeers_t *pPeer, const uchar *pszNameToMatch, int *pbIsMatching);
 	/* v5 interface additions */
 	int (*CmpHost)(struct sockaddr_storage *, struct sockaddr_storage*, size_t);
 	/* v6 interface additions - 2009-11-16 */

runtime/prop.h
@@ -61,7 +61,7 @@ ENDinterface(prop)
  * cannot work around this as we would otherwise need to evaluate
  * pThis more than once.
  */
-static inline uchar * __attribute__((unused))
+static inline uchar * __attribute__((unused)) ATTR_NONNULL(1)
 propGetSzStr(prop_t *pThis)
 {
 	return(pThis->len < CONF_PROP_BUFSIZE) ? pThis->szVal.sz : pThis->szVal.psz;

runtime/rsyslog.h
@@ -51,6 +51,7 @@ extern int src_exists;
 #endif
 
 #define ATTR_NORETURN __attribute__ ((noreturn))
+#define ATTR_NONNULL(...) __attribute__((nonnull(__VA_ARGS__)))
 
 /* ############################################################# *
  * #                 Some constant values                      # *

commit 0745225238ee3e9040e2ee3625f397b2ac6862fa

tcpsrv: Don't decrement wrkrRunning if terminating without a work item
wrkrRunning only gets incremented when a work item is issued, so the worker
shouldn't decrement it when terminating without a work item.

Fixes: 47982120408e ("work around clang static analyzer false positives")
Signed-off-by: John Brooks <jbrooks@ciena.com>

runtime/tcpsrv.c
@@ -693,7 +693,6 @@ wrkr(void *const myself)
 			// we need to query me->opSrv to avoid clang static
 			// analyzer false positive! -- rgerhards, 2017-10-23
 			assert(glbl.GetGlobalInputTermState() == 1);
-			--wrkrRunning;
 			break;
 		}
 		pthread_mutex_unlock(&wrkrMut);