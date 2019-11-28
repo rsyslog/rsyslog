2869f471d6de865859ee542cf8a7541428a4efce
Rainer Gerhards <rgerhards@adiscon.com>
2019-11-28 13:49:09 +0100

    testbench: improve python3 support

--- tests/urlencode.py
+++ tests/urlencode.py
@@ -3,9 +3,12 @@
 # written 2018-11-05 by Rainer Gerhards
 # part of the rsyslog testbench, released under ASL 2.0
 import sys
-import urllib
+try:
+	from urllib.parse import quote_plus
+except ImportError:
+	from urllib import quote_plus
 
 if len(sys.argv) != 2:
 	print("ERROR: urlencode needs exactly one string as argument")
 	sys.exit(1)
-print(urllib.quote_plus(sys.argv[1]))
+print(quote_plus(sys.argv[1]))
