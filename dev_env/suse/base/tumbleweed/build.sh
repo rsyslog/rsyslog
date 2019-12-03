fc8de02265eb03c2497fb5ecf6dc1e4d48147d6f
Rainer Gerhards <rgerhards@adiscon.com>
2019-12-03 09:29:41 +0100

    dev env CentOS 7 /Suse: add snmp testing support
    also update to current standard

--- dev_env/suse/base/tumbleweed/build.sh
+++ dev_env/suse/base/tumbleweed/build.sh
@@ -1,12 +1,11 @@
 set -e
-docker build $1 -t rsyslog/rsyslog_dev_base_suse:tumbleweed .
+docker build $1 --build-arg BUILD_TIME="$(date)" -t rsyslog/rsyslog_dev_base_suse:tumbleweed .
 printf "\n\n================== BUILD DONE, NOW TESTING CONTAINER:"
-docker run -ti rsyslog/rsyslog_dev_base_suse:tumbleweed bash -c  "
+docker run -ti --rm rsyslog/rsyslog_dev_base_suse:tumbleweed bash -c  "
 set -e && \
 git clone https://github.com/rsyslog/rsyslog.git && \
 cd rsyslog && \
-autoreconf -fi && \
-./configure \$RSYSLOG_CONFIGURE_OPTIONS --enable-compile-warnings=yes  && \
+devtools/run-configure.sh
 make -j4
 "
 if [ $? -eq 0 ]; then
