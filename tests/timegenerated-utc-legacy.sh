079ce47f927e867ac7e754c2f8c752137b70181d
Rainer Gerhards <rgerhards@adiscon.com>
2019-12-19 12:59:53 +0100

    testbench: modernize test and make more robust in regard to timing

--- tests/timegenerated-utc-legacy.sh
+++ tests/timegenerated-utc-legacy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# addd 2016-03-22 by RGerhards, released under ASL 2.0
-
+# added 2016-03-22 by RGerhards, released under ASL 2.0
+#
 # NOTE: faketime does NOT properly support subseconds,
 # so we must ensure we do not use them. Actually, what we
 # see is uninitialized data value in tv_usec, which goes
@@ -8,33 +8,29 @@
 # FOR THE SAME REASON, there is NO VALGRIND EQUIVALENT
 # of this test, as valgrind would abort with reports
 # of faketime.
+#
+# IMPORTANT: we use legacy style for the template to ensure
+# that subseconds works properly for this as well. This is
+# a frequent use case.
+#
 . ${srcdir:=.}/diag.sh init
 . $srcdir/faketime_common.sh
-
 export TZ=TEST+02:00
-
 generate_conf
 add_conf '
-$ModLoad ../plugins/imtcp/.libs/imtcp
-$InputTCPServerRun '$TCPFLOOD_PORT'
+module(load="../plugins/imtcp/.libs/imtcp")
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port")
 
-template(name="outfmt" type="string"
-	 string="%timegenerated:::date-utc%\n")
+$template outfmt,"%timegenerated:::date-utc%\n"
 :msg, contains, "msgnum:" action(type="omfile" template="outfmt"
-			         file=`echo $RSYSLOG_OUT_LOG`)
+			         file="'$RSYSLOG_OUT_LOG'")
 '
 
 echo "***SUBTEST: check 2016-03-01"
-rm -f $RSYSLOG_OUT_LOG	# do cleanup of previous subtest
 FAKETIME='2016-03-01 12:00:00' startup
 tcpflood -m1
 shutdown_when_empty
 wait_shutdown
-echo "Mar  1 14:00:00" | cmp - $RSYSLOG_OUT_LOG
-if [ ! $? -eq 0 ]; then
-  echo "invalid timestamps generated, $RSYSLOG_OUT_LOG is:"
-  cat $RSYSLOG_OUT_LOG
-  exit 1
-fi;
-
+export EXPECTED="Mar  1 14:00:00"
+cmp_exact
 exit_test
