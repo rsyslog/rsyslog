e868bc3162126e43e217714b4682bca8752b0b28
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 02:16:44 -0500

    spelling: occurs

--- runtime/msg.c
+++ runtime/msg.c
@@ -4656,7 +4656,7 @@ msgSetPropViaJSON(smsg_t *__restrict__ const pMsg, const char *name, struct json
 /* set message properties based on JSON string. This function does it all,
  * including parsing the JSON string. If an error is detected, the operation
  * is aborted at the time of error. Any modifications made before the
- * error ocurs are still PERSISTED.
+ * error occurs are still PERSISTED.
  * This function is meant to support the external message modification module
  * interface. As such, replacing properties is expressively permited. Note that
  * properties which were derived from the message during parsing are NOT
