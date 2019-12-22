08acec43c6cc657a0d8d59021cd152110df95ea1
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 04:59:44 -0500

    spelling: school

--- runtime/modules.c
+++ runtime/modules.c
@@ -284,7 +284,7 @@ finalize_it:
 /* The following function is the queryEntryPoint for host-based entry points.
  * Modules may call it to get access to core interface functions. Please note
  * that utility functions can be accessed via shared libraries - at least this
- * is my current shool of thinking.
+ * is my current school of thinking.
  * Please note that the implementation as a query interface allows to take
  * care of plug-in interface version differences. -- rgerhards, 2007-07-31
  * ... but often it better not to use a new interface. So we now add core
