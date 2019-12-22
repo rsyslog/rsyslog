8ce670846df7dc55d3ab27d16ed67f831c5bed06
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 05:37:00 -0500

    spelling: terminate

--- runtime/queue.c
+++ runtime/queue.c
@@ -2777,7 +2777,7 @@ DoSaveOnShutdown(qqueue_t *pThis)
 	 * it is reached.
 	 */
 	DBGOPRINT((obj_t*) pThis, "bSaveOnShutdown set, restarting DA worker...\n");
-	pThis->bShutdownImmediate = 0; /* would termiante the DA worker! */
+	pThis->bShutdownImmediate = 0; /* would terminate the DA worker! */
 	pThis->iLowWtrMrk = 0;
 	wtpSetState(pThis->pWtpDA, wtpState_SHUTDOWN);	/* shutdown worker (only) when done (was _IMMEDIATE!) */
 	wtpAdviseMaxWorkers(pThis->pWtpDA, 1, PERMIT_WORKER_START_DURING_SHUTDOWN);	/* restart DA worker */
