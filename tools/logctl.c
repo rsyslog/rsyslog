9e826b559bb980d882e70eb8d88b0bbfc647e76b
Josh Soref <jsoref@users.noreply.github.com>
2019-12-21 21:04:11 -0500

    spelling: formatter

--- tools/logctl.c
+++ tools/logctl.c
@@ -141,7 +141,7 @@ struct results
 };
 
 
-static void formater(struct ofields* fields)
+static void formatter(struct ofields* fields)
 {
 	char str[N];
 	time_t rtime;
@@ -467,7 +467,7 @@ int main(int argc, char* argv[])
 	while (cursor_next (db_c, res)) /* Move cursor & get pointed data */
 	{
 		fields = get_data (res);
-		formater (fields); /* format output */
+		formatter (fields); /* format output */
 		free (fields);
 	}
 
