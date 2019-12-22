873ff9d3eedbd564ebc29285b269be547210edfe
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 00:57:44 -0500

    spelling: limit

--- outchannel.c
+++ outchannel.c
@@ -290,7 +290,7 @@ void ochPrintList(void)
 		dbgprintf("\tFile Template: '%s'\n", pOch->pszFileTemplate == NULL ? "NULL" :
 			(char*) pOch->pszFileTemplate);
 		dbgprintf("\tMax Size.....: %lu\n", (long unsigned) pOch->uSizeLimit);
-		dbgprintf("\tOnSizeLimtCmd: '%s'\n", pOch->cmdOnSizeLimit == NULL ? "NULL" :
+		dbgprintf("\tOnSizeLimitCmd: '%s'\n", pOch->cmdOnSizeLimit == NULL ? "NULL" :
 			(char*) pOch->cmdOnSizeLimit);
 		pOch = pOch->pNext; /* done, go next */
 	}
