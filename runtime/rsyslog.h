bfb0ec1c7aa08be888db3104b4729e65c88d9c80
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 00:17:42 -0500

    spelling: incrementally

--- runtime/rsyslog.h
+++ runtime/rsyslog.h
@@ -772,7 +772,7 @@ void dfltErrLogger(const int, const int, const uchar *errMsg);
 
 /* TODO: remove this -- this is only for transition of the config system */
 extern rsconf_t *ourConf; /* defined by syslogd.c, a hack for functions that do not
-			     yet receive a copy, so that we can incrementially
+			     yet receive a copy, so that we can incrementally
 			     compile and change... -- rgerhars, 2011-04-19 */
 
 
