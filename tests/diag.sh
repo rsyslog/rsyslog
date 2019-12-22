22bb38757860438c56824eab74d44616be5bfb9a
Josh Soref <jsoref@users.noreply.github.com>
2019-12-22 04:20:12 -0500

    spelling: temporary

--- tests/diag.sh
+++ tests/diag.sh
@@ -1214,7 +1214,7 @@ error_exit() {
 		RSYSLOG_DEBUG=$RSYSLOG_DEBUG_SAVE
 		rm IN_AUTO_DEBUG
 	fi
-	# output listening ports as a temporay debug measure (2018-09-08 rgerhards), now disables, but not yet removed (2018-10-22)
+	# output listening ports as a temporary debug measure (2018-09-08 rgerhards), now disables, but not yet removed (2018-10-22)
 	#if [ $(uname) == "Linux" ]; then
 	#	netstat -tlp
 	#else
