4d69817808b55abce46c83be7b228ffd2809a950
Rainer Gerhards <rgerhards@adiscon.com>
2019-12-30 12:44:11 +0100

    config system: emit proper error message on $ in double-quoted string
    closes https://github.com/rsyslog/rsyslog/issues/2869

--- tests/rscript_re_match.sh
+++ tests/rscript_re_match.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # added 2015-09-29 by singh.janmejay
+# test re_match rscript-fn, using single quotes for the match
 # This file is part of the rsyslog project, released under ASL 2.0
-echo ===============================================================================
-echo \[rscript_re_match.sh\]: test re_match rscript-fn
 . ${srcdir:=.}/diag.sh init
 generate_conf
 add_conf '
@@ -13,14 +12,12 @@ input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port
 add_conf "
 if (re_match(\$msg, '.* ([0-9]+)$')) then {"
 add_conf '
-	 action(type="omfile" file=`echo $RSYSLOG_OUT_LOG` template="outfmt")
+	 action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
 }
 '
 startup
 tcpflood -m 1 -I $srcdir/testsuites/date_time_msg
-echo doing shutdown
 shutdown_when_empty
-echo wait on shutdown
 wait_shutdown
 content_check "*Matched*"
 exit_test
