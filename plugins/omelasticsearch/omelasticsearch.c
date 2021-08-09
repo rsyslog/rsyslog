rsyslog/rsyslog

commit 73277e68ea2eb41672f97eb7e2aab27aabdad8b0

[omelasticsearch] Improve errorFile mutex handling
Lock the file during SIGHUPs to avoid issues with concurrent accesses by writeDataError().
Check the return value of pthread_mutex_init() in CODESTARTcreateInstance to make sure the mutex is actually usable.

plugins/omelasticsearch/omelasticsearch.c
@@ -232,7 +232,7 @@ static rsRetVal curlSetup(wrkrInstanceData_t *pWrkrData);
 BEGINcreateInstance
 CODESTARTcreateInstance
 	pData->fdErrFile = -1;
-	pthread_mutex_init(&pData->mutErrFile, NULL);
+	CHKiRet(pthread_mutex_init(&pData->mutErrFile, NULL));
 	pData->caCertFile = NULL;
 	pData->myCertFile = NULL;
 	pData->myPrivKeyFile = NULL;
@@ -240,6 +240,7 @@ CODESTARTcreateInstance
 	pData->retryRulesetName = NULL;
 	pData->retryRuleset = NULL;
 	pData->rebindInterval = DEFAULT_REBIND_INTERVAL;
+finalize_it:
 ENDcreateInstance
 
 BEGINcreateWrkrInstance
@@ -2165,10 +2166,12 @@ ENDfreeCnf
 
 BEGINdoHUP
 CODESTARTdoHUP
+	pthread_mutex_lock(&pData->mutErrFile);
 	if(pData->fdErrFile != -1) {
 		close(pData->fdErrFile);
 		pData->fdErrFile = -1;
 	}
+	pthread_mutex_unlock(&pData->mutErrFile);
 ENDdoHUP
 
 

commit cf977690ca5c4cb26f4853aabde3c2991cf5f83f

omelasticsearch: fix incorrect mutex error handling regression
Commit 73277e68ea2e introduced a slightly wrong error check. This patch
fixes that and also emits an user error message in the very unlikely
case an error actually happens.

Note: this patch was applied as part of the merge process, so no version
ever existed with the defect.

see also https://github.com/rsyslog/rsyslog/pull/4639

plugins/omelasticsearch/omelasticsearch.c
@@ -4,7 +4,7 @@
  * NOTE: read comments in module-template.h for more specifics!
  *
  * Copyright 2011 Nathan Scott.
- * Copyright 2009-2019 Rainer Gerhards and Adiscon GmbH.
+ * Copyright 2009-2021 Rainer Gerhards and Adiscon GmbH.
  *
  * This file is part of rsyslog.
  *
@@ -232,7 +232,11 @@ static rsRetVal curlSetup(wrkrInstanceData_t *pWrkrData);
 BEGINcreateInstance
 CODESTARTcreateInstance
 	pData->fdErrFile = -1;
-	CHKiRet(pthread_mutex_init(&pData->mutErrFile, NULL));
+	if(pthread_mutex_init(&pData->mutErrFile, NULL) != 0) {
+		LogError(errno, RS_RET_ERR, "omelasticsearch: cannot create "
+			"error file mutex, failing this action");
+		ABORT_FINALIZE(RS_RET_ERR);
+	}
 	pData->caCertFile = NULL;
 	pData->myCertFile = NULL;
 	pData->myPrivKeyFile = NULL;