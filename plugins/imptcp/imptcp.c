48752cf26200e00d9acb78f48e37990991a7fa03
Michael Biebl <biebl@debian.org>
2022-02-16 08:48:20 +0100

    typo fix: stoping -> stopping

--- plugins/imptcp/imptcp.c
+++ plugins/imptcp/imptcp.c
@@ -1845,7 +1845,7 @@ static void
 stopWorkerPool(void)
 {
 	int i;
-	DBGPRINTF("imptcp: stoping worker pool\n");
+	DBGPRINTF("imptcp: stopping worker pool\n");
 	pthread_mutex_lock(&io_q.mut);
 	pthread_cond_broadcast(&io_q.wakeup_worker); /* awake wrkr if not running */
 	pthread_mutex_unlock(&io_q.mut);
