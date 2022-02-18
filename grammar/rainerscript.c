rsyslog/rsyslog

commit 5cec5dd634e0033e71ddc3ca9806f7f03819a8e0

rscript: literal numbers were not compared correctly
This problem occurred when numbers were used in rsyslog.conf in
the set statement, e.g.

set $nbr = 1234;

In this case, during comparisons, the number was actually interpreted
as a string with digits. Thus numerical comparisons lead to unexpected
results. Even more so, as in other places of the code they were
treated as native numbers.

This is now fixed. We cannot outrule that this causes, in border cases,
change of behavior to existing configs. But it is unlikely and the
previous behaviour was a clear bug and very unintuitive. This in our
opinion it is justified to risk a breaking change for an expected
very minor subset of installations, if any such exists at all.

The fix was combined with code refactoring. We did this, because
the fix itself would have been quite hard to read, and the need
for refactoring became obvious.

closes https://github.com/rsyslog/rsyslog/issues/4770

grammar/rainerscript.c
@@ -2,7 +2,7 @@
  *
  * Module begun 2011-07-01 by Rainer Gerhards
  *
- * Copyright 2011-2019 Rainer Gerhards and Others.
+ * Copyright 2011-2022 Rainer Gerhards and Others.
  *
  * This file is part of the rsyslog runtime library.
  *
@@ -3046,6 +3046,39 @@ evalStrArrayCmp(es_str_t *const estr_l,
 		if(bMustFree2) es_deleteStr(estr_l);  \
 		varFreeMembers(&l)
 
+/* helper to evaluate comparison in a strcmp() like manner. Result is
+ * to be used for final truth value evaluation.
+ */
+static int eval_strcmp_like(const struct cnfexpr *__restrict__ const expr,
+	void *__restrict__ const usrptr,
+	wti_t *__restrict__ const pWti)
+{
+	es_str_t *__restrict__ estr_r, *__restrict__ estr_l;
+	int bMustFree, bMustFree2;
+	int64_t n_r, n_l;
+	int convok_r, convok_l;
+	struct svar r, l; /* memory for subexpression results */
+	int ret;
+
+	cnfexprEval(expr->l, &l, usrptr, pWti);
+	cnfexprEval(expr->r, &r, usrptr, pWti);
+	n_l = var2Number(&l, &convok_l);
+	if(convok_l) {
+		n_r = var2Number(&r, &convok_r);
+	}
+	if(convok_l && convok_r) {
+		ret = n_l - n_r;
+	} else {
+		estr_l = var2String(&l, &bMustFree);
+		estr_r = var2String(&r, &bMustFree2);
+		ret = es_strcmp(estr_l, estr_r) < 0; /*CMP*/
+		if(bMustFree) es_deleteStr(estr_l);
+		if(bMustFree2) es_deleteStr(estr_r);
+	}
+	FREE_BOTH_RET;
+	return ret;
+}
+
 /* evaluate an expression.
  * Note that we try to avoid malloc whenever possible (because of
  * the large overhead it has, especially on highly threaded programs).
@@ -3196,196 +3229,20 @@ cnfexprEval(const struct cnfexpr *__restrict__ const expr,
 		FREE_BOTH_RET;
 		break;
 	case CMP_LE:
-		cnfexprEval(expr->l, &l, usrptr, pWti);
-		cnfexprEval(expr->r, &r, usrptr, pWti);
 		ret->datatype = 'N';
-		if(l.datatype == 'S') {
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(l.d.estr, r.d.estr) <= 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l <= r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree);
-					ret->d.n = es_strcmp(l.d.estr, estr_r) <= 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_r);
-				}
-			}
-		} else if(l.datatype == 'J') {
-			estr_l = var2String(&l, &bMustFree);
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(estr_l, r.d.estr) <= 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l <= r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree2);
-					ret->d.n = es_strcmp(estr_l, estr_r) <= 0; /*CMP*/
-					if(bMustFree2) es_deleteStr(estr_r);
-				}
-			}
-			if(bMustFree) es_deleteStr(estr_l);
-		} else {
-			if(r.datatype == 'S') {
-				n_r = var2Number(&r, &convok_r);
-				if(convok_r) {
-					ret->d.n = (l.d.n <= n_r); /*CMP*/
-				} else {
-					estr_l = var2String(&l, &bMustFree);
-					ret->d.n = es_strcmp(r.d.estr, estr_l) <= 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_l);
-				}
-			} else {
-				ret->d.n = (l.d.n <= r.d.n); /*CMP*/
-			}
-		}
-		FREE_BOTH_RET;
+		ret->d.n = eval_strcmp_like(expr, usrptr, pWti) <= 0;
 		break;
 	case CMP_GE:
-		cnfexprEval(expr->l, &l, usrptr, pWti);
-		cnfexprEval(expr->r, &r, usrptr, pWti);
 		ret->datatype = 'N';
-		if(l.datatype == 'S') {
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(l.d.estr, r.d.estr) >= 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l >= r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree);
-					ret->d.n = es_strcmp(l.d.estr, estr_r) >= 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_r);
-				}
-			}
-		} else if(l.datatype == 'J') {
-			estr_l = var2String(&l, &bMustFree);
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(estr_l, r.d.estr) >= 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l >= r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree2);
-					ret->d.n = es_strcmp(estr_l, estr_r) >= 0; /*CMP*/
-					if(bMustFree2) es_deleteStr(estr_r);
-				}
-			}
-			if(bMustFree) es_deleteStr(estr_l);
-		} else {
-			if(r.datatype == 'S') {
-				n_r = var2Number(&r, &convok_r);
-				if(convok_r) {
-					ret->d.n = (l.d.n >= n_r); /*CMP*/
-				} else {
-					estr_l = var2String(&l, &bMustFree);
-					ret->d.n = es_strcmp(r.d.estr, estr_l) >= 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_l);
-				}
-			} else {
-				ret->d.n = (l.d.n >= r.d.n); /*CMP*/
-			}
-		}
-		FREE_BOTH_RET;
+		ret->d.n = eval_strcmp_like(expr, usrptr, pWti) >= 0;
 		break;
 	case CMP_LT:
-		cnfexprEval(expr->l, &l, usrptr, pWti);
-		cnfexprEval(expr->r, &r, usrptr, pWti);
 		ret->datatype = 'N';
-		if(l.datatype == 'S') {
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(l.d.estr, r.d.estr) < 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l < r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree);
-					ret->d.n = es_strcmp(l.d.estr, estr_r) < 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_r);
-				}
-			}
-		} else if(l.datatype == 'J') {
-			estr_l = var2String(&l, &bMustFree);
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(estr_l, r.d.estr) < 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l < r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree2);
-					ret->d.n = es_strcmp(estr_l, estr_r) < 0; /*CMP*/
-					if(bMustFree2) es_deleteStr(estr_r);
-				}
-			}
-			if(bMustFree) es_deleteStr(estr_l);
-		} else {
-			if(r.datatype == 'S') {
-				n_r = var2Number(&r, &convok_r);
-				if(convok_r) {
-					ret->d.n = (l.d.n < n_r); /*CMP*/
-				} else {
-					estr_l = var2String(&l, &bMustFree);
-					ret->d.n = es_strcmp(r.d.estr, estr_l) < 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_l);
-				}
-			} else {
-				ret->d.n = (l.d.n < r.d.n); /*CMP*/
-			}
-		}
-		FREE_BOTH_RET;
+		ret->d.n = eval_strcmp_like(expr, usrptr, pWti) < 0;
 		break;
 	case CMP_GT:
-		cnfexprEval(expr->l, &l, usrptr, pWti);
-		cnfexprEval(expr->r, &r, usrptr, pWti);
 		ret->datatype = 'N';
-		if(l.datatype == 'S') {
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(l.d.estr, r.d.estr) > 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l > r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree);
-					ret->d.n = es_strcmp(l.d.estr, estr_r) > 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_r);
-				}
-			}
-		} else if(l.datatype == 'J') {
-			estr_l = var2String(&l, &bMustFree);
-			if(r.datatype == 'S') {
-				ret->d.n = es_strcmp(estr_l, r.d.estr) > 0; /*CMP*/
-			} else {
-				n_l = var2Number(&l, &convok_l);
-				if(convok_l) {
-					ret->d.n = (n_l > r.d.n); /*CMP*/
-				} else {
-					estr_r = var2String(&r, &bMustFree2);
-					ret->d.n = es_strcmp(estr_l, estr_r) > 0; /*CMP*/
-					if(bMustFree2) es_deleteStr(estr_r);
-				}
-			}
-			if(bMustFree) es_deleteStr(estr_l);
-		} else {
-			if(r.datatype == 'S') {
-				n_r = var2Number(&r, &convok_r);
-				if(convok_r) {
-					ret->d.n = (l.d.n > n_r); /*CMP*/
-				} else {
-					estr_l = var2String(&l, &bMustFree);
-					ret->d.n = es_strcmp(r.d.estr, estr_l) > 0; /*CMP*/
-					if(bMustFree) es_deleteStr(estr_l);
-				}
-			} else {
-				ret->d.n = (l.d.n > r.d.n); /*CMP*/
-			}
-		}
-		FREE_BOTH_RET;
+		ret->d.n = eval_strcmp_like(expr, usrptr, pWti) > 0;
 		break;
 	case CMP_STARTSWITH:
 		PREP_TWO_STRINGS;

commit b07b45bc17522674a1b2e8f294765b22ff91f0c9

regression fix: script string comparison did not work correctly
In rscript, comparison operations on strings did not work correctly
and returned false results. This is cause by a regression in commit
5cec5dd634e0. While it fixed number comparisons, it introduced new
problems in string comparisons, which were not present before. Note
that most items in rsyslog are strings, so this can actually cause
some problems.

grammar/rainerscript.c
@@ -3071,7 +3071,7 @@ static int eval_strcmp_like(const struct cnfexpr *__restrict__ const expr,
 	} else {
 		estr_l = var2String(&l, &bMustFree);
 		estr_r = var2String(&r, &bMustFree2);
-		ret = es_strcmp(estr_l, estr_r) < 0; /*CMP*/
+		ret = es_strcmp(estr_l, estr_r);
 		if(bMustFree) es_deleteStr(estr_l);
 		if(bMustFree2) es_deleteStr(estr_r);
 	}