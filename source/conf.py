e2084e3370d3d0c37f9184f7532c03378ead4887
Rainer Gerhards <rgerhards@adiscon.com>
2022-04-20 08:32:12 +0200

    bump version number for next scheduled stable release

--- source/conf.py
+++ source/conf.py
@@ -92,9 +92,8 @@ rst_epilog = """
 # real values will be generated dynamically from info in the repo. If the
 # user builds the docs from "bare" sources not yet processed
 ###############################################################################
-version = '8.2202'
-#release = version + ' daily stable'
-release = '8.2202.0'
+version = '8.2206'
+release = version + ''.0'
 
 
 # For this to be true, it means that we are not attempting to build from
