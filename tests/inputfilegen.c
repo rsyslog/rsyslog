cd8af471448efb0873037d6450afd6a9217b186e
Rainer Gerhards <rgerhards@adiscon.com>
2022-05-27 13:08:04 +0200

    testbench: add testcase for frequent imfile input file change
    PoC test, yet incomplete
    
    This patch not only contains the new test but also supporting
    changes to testbench tooling.
    
    see also: https://github.com/rsyslog/rsyslog/issues/4797

--- tests/inputfilegen.c
+++ tests/inputfilegen.c
@@ -1,5 +1,5 @@
 /* generate an input file suitable for use by the testbench
- * Copyright (C) 2018 by Rainer Gerhards and Adiscon GmbH.
+ * Copyright (C) 2018-2022 by Rainer Gerhards and Adiscon GmbH.
  * Copyright (C) 2016-2018 by Pascal Withopf and Adiscon GmbH.
  *
  * usage: ./inputfilegen num-lines > file
@@ -14,9 +14,20 @@
  *  This is especially useful with -s, as the testbench otherwise does
  *  not know how to do a seq_check. To keep things flexible, can also be
  *  used with -m (this may aid testbench framework generalization).
+ * -f outputfile
+ *  Permits to write data to file "outputfile" instead of stdout. Also
+ *  enables support for SIGHUP.
+ * -S sleep time
+ *  ms to sleep between sending message bulks (bulks size given by -B)
+ * -B number of messages in bulk
+ *  number of messages to send without sleeping as specified in -S.
+ *  IGNORED IF -S is not also given!
  * Part of rsyslog, licensed under ASL 2.0
  */
+#include "config.h"
 #include <stdio.h>
+#include <signal.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -25,9 +36,59 @@
 #else
 	#include <getopt.h>
 #endif
+#if defined(__FreeBSD__)
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+#if defined(HAVE_SYS_SELECT_H)
+#include <sys/select.h>
+#endif
 
 #define DEFMSGS 5
 #define NOEXTRADATA -1
+
+static volatile int bHadHUP = 0;
+static void
+hdlr_sighup(int sig)
+{
+	fprintf(stderr, "had hup, sig %d\n", sig);
+	bHadHUP = 1;
+}
+static void
+sighup_enable()
+{
+	struct sigaction sigAct;
+	memset(&sigAct, 0, sizeof (sigAct));
+	sigemptyset(&sigAct.sa_mask);
+	sigAct.sa_handler = hdlr_sighup;
+	sigaction(SIGHUP, &sigAct, NULL);
+}
+
+void msleep(const int sleepTime)
+{
+	struct timeval tvSelectTimeout;
+
+	tvSelectTimeout.tv_sec = sleepTime / 1000;
+	tvSelectTimeout.tv_usec = (sleepTime % 1000) * 1000; /* micro seconds */
+	if(select(0, NULL, NULL, NULL, &tvSelectTimeout) == -1) {
+		if(errno != EINTR) {
+			perror("select");
+			exit(1);
+		}
+	}
+}
+
+static FILE *
+open_output(const char *fn)
+{
+	FILE *fh_output = fopen(fn, "w");
+	if(fh_output == NULL) {
+		perror(fn);
+		exit(1);
+	}
+	return fh_output;
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,8 +100,13 @@ int main(int argc, char* argv[])
 	long long filesize = -1;
 	char *extradata = NULL;
 	const char *msgcntfile = NULL;
+	const char *outputfile = "-";
+	FILE *fh_output;
+	int sleep_ms = 0;
+	int sleep_msgs = 0; /* messages to xmit between sleeps (if configured) */
+	int ctr = 0;
 
-	while((c=getopt(argc, argv, "m:M:i:d:s:")) != -1) {
+	while((c=getopt(argc, argv, "m:M:i:d:s:f:S:B:")) != -1) {
 		switch(c) {
 		case 'm':
 			nmsgs = atoi(optarg);
@@ -56,6 +122,16 @@ int main(int argc, char* argv[])
 			break;
 		case 's':
 			filesize = atoll(optarg);
+			break;
+		case 'S':
+			sleep_ms = atoi(optarg);
+			break;
+		case 'B':
+			sleep_msgs = atoi(optarg);
+			break;
+		case 'f':
+			outputfile = optarg;
+			sighup_enable();
 			break;
 		case ':':
 			fprintf(stderr, "Option -%c requires an operand\n", optopt);
@@ -97,17 +173,33 @@ int main(int argc, char* argv[])
 		fclose(fh);
 	}
 
+	if(strcmp(outputfile, "-")) {
+		fh_output = open_output(outputfile);
+	} else {
+		fh_output = stdout;
+	}
+
 	if(nchars != NOEXTRADATA) {
 		extradata = (char *)malloc(nchars + 1);
 		memset(extradata, 'X', nchars);
 		extradata[nchars] = '\0';
 	}
 	for(i = nmsgstart; i < (nmsgs+nmsgstart); ++i) {
-		printf("msgnum:%8.8d:", i);
-		if(nchars != NOEXTRADATA) {
-			printf("%s", extradata);
+		if(sleep_ms > 0 && ctr++ >= sleep_msgs) {
+			msleep(sleep_ms);
+			ctr = 0;
 		}
-		printf("\n");
+		if(bHadHUP) {
+			fclose(fh_output);
+			fh_output = open_output(outputfile);
+			fprintf(stderr, "%s reopened\n", outputfile);
+			bHadHUP = 0;
+		}
+		fprintf(fh_output, "msgnum:%8.8d:", i);
+		if(nchars != NOEXTRADATA) {
+			fprintf(fh_output, "%s", extradata);
+		}
+		fprintf(fh_output, "\n");
 	}
 	free(extradata);
 	return 0;
