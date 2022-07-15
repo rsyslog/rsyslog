12b78002ecef55f2a709cf19365fdbe5b5456bb6
Andre lorbach <alorbach@adiscon.com>
2022-07-15 10:12:28 +0200

    Updated fedora pkg build container to version 36
    - Added missing files from fedora:30 build container
    - Added missing packages for sudo/logrotate to
      debian 8 / 10 build containers. Removed not existing
      dependencies.

---
+++ dev_env/fedora/pkg_base/36/extra/do_upload_delete.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Definitions common to these scripts
+source $(dirname "$0")/config.sh
+
+echo "-------------------------------------"
+echo "--- Upload RPM Packages               ---"
+echo "-------------------------------------"
+
+if [ -z $RPM_REPO ]; then
+        echo "Which REPO do you want to upload?--"
+        select szSubRepo in $REPOOPTIONS
+        do
+                break;
+        done
+else
+        echo "REPO is set to '$RPM_REPO'"
+        szSubRepo=$RPM_REPO
+fi
+
+echo "Uploading Branch '$szYumRepoDir/$szSubRepo/' to $REPOUSERNAME@$REPOURL/$szSubRepo/
+"
+
+FILESTOREMOVE=`rsync -au --delete --dry-run -e "ssh -i /private-files/.ssh/id_rsa" --progress $szYumRepoDir/$szSubRepo/* $REPOUSERNAME@$REPOURL/$szSubRepo/ | grep deleting | wc -l`
+
+read -p "ARE YOU SURE TO REMOVE $FILESTOREMOVE files from $REPOUSERNAME@$REPOURL/$szSubRepo/ " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+        exit 1;
+fi
+
+# DO IT
+rsync -au --delete -e "ssh -i /private-files/.ssh/id_rsa" --progress $szYumRepoDir/$szSubRepo/* $REPOUSERNAME@$REPOURL/$szSubRepo/
+
