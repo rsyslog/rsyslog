rsyslog/rsyslog

commit a08591be5d9f3e7e076c5a0347dd5df1c790e965

imtcp: add stream driver parameter to input() configuration
This permits to have different inputs use different stream drivers
and stream driver parameters.

closes https://github.com/rsyslog/rsyslog/issues/3727

plugins/imtcp/imtcp.c
@@ -129,6 +129,14 @@ struct instanceConf_s {
 	sbool bSPFramingFix;
 	unsigned int ratelimitInterval;
 	unsigned int ratelimitBurst;
+	uchar *pszStrmDrvrName; /* stream driver to use */
+	int iStrmDrvrMode;
+	uchar *pszStrmDrvrAuthMode;
+	uchar *pszStrmDrvrPermitExpiredCerts;
+	int iStrmDrvrExtendedCertCheck;
+	int iStrmDrvrSANPreference;
+	int iStrmTlsVerifyDepth;
+	int iAddtlFrameDelim; /* addtl frame delimiter, e.g. for netscreen, default none */
 	struct instanceConf_s *next;
 };
 
@@ -206,6 +214,13 @@ static struct cnfparamdescr inppdescr[] = {
 	{ "name", eCmdHdlrString, 0 },
 	{ "defaulttz", eCmdHdlrString, 0 },
 	{ "ruleset", eCmdHdlrString, 0 },
+	{ "streamdriver.mode", eCmdHdlrNonNegInt, 0 },
+	{ "streamdriver.authmode", eCmdHdlrString, 0 },
+	{ "streamdriver.permitexpiredcerts", eCmdHdlrString, 0 },
+	{ "streamdriver.name", eCmdHdlrString, 0 },
+	{ "streamdriver.CheckExtendedKeyPurpose", eCmdHdlrBinary, 0 },
+	{ "streamdriver.PrioritizeSAN", eCmdHdlrBinary, 0 },
+	{ "streamdriver.TlsVerifyDepth", eCmdHdlrPositiveInt, 0 },
 	{ "supportoctetcountedframing", eCmdHdlrBinary, 0 },
 	{ "ratelimit.interval", eCmdHdlrInt, 0 },
 	{ "framingfix.cisco.asa", eCmdHdlrBinary, 0 },
@@ -307,6 +322,15 @@ createInstance(instanceConf_t **pinst)
 	inst->bSPFramingFix = 0;
 	inst->ratelimitInterval = 0;
 	inst->ratelimitBurst = 10000;
+
+	inst->pszStrmDrvrName = NULL;
+	inst->pszStrmDrvrAuthMode = NULL;
+	inst->pszStrmDrvrPermitExpiredCerts = NULL;
+	inst->iStrmDrvrMode = loadModConf->iStrmDrvrMode;
+	inst->iStrmDrvrExtendedCertCheck = loadModConf->iStrmDrvrExtendedCertCheck;
+	inst->iStrmDrvrSANPreference = loadModConf->iStrmDrvrSANPreference;
+	inst->iStrmTlsVerifyDepth = loadModConf->iStrmTlsVerifyDepth;
+
 	inst->cnf_params->pszLstnPortFileName = NULL;
 
 	/* node created, let's add to config */
@@ -373,6 +397,7 @@ static rsRetVal
 addListner(modConfData_t *modConf, instanceConf_t *inst)
 {
 	DEFiRet;
+	uchar *psz;	/* work variable */
 
 	tcpsrv_t *pOurTcpsrv;
 	CHKiRet(tcpsrv.Construct(&pOurTcpsrv));
@@ -390,10 +415,10 @@ addListner(modConfData_t *modConf, instanceConf_t *inst)
 	CHKiRet(tcpsrv.SetGnutlsPriorityString(pOurTcpsrv, modConf->gnutlsPriorityString));
 	CHKiRet(tcpsrv.SetSessMax(pOurTcpsrv, modConf->iTCPSessMax));
 	CHKiRet(tcpsrv.SetLstnMax(pOurTcpsrv, modConf->iTCPLstnMax));
-	CHKiRet(tcpsrv.SetDrvrMode(pOurTcpsrv, modConf->iStrmDrvrMode));
-	CHKiRet(tcpsrv.SetDrvrCheckExtendedKeyUsage(pOurTcpsrv, modConf->iStrmDrvrExtendedCertCheck));
-	CHKiRet(tcpsrv.SetDrvrPrioritizeSAN(pOurTcpsrv, modConf->iStrmDrvrSANPreference));
-	CHKiRet(tcpsrv.SetDrvrTlsVerifyDepth(pOurTcpsrv, modConf->iStrmTlsVerifyDepth));
+	CHKiRet(tcpsrv.SetDrvrMode(pOurTcpsrv, inst->iStrmDrvrMode));
+	CHKiRet(tcpsrv.SetDrvrCheckExtendedKeyUsage(pOurTcpsrv, inst->iStrmDrvrExtendedCertCheck));
+	CHKiRet(tcpsrv.SetDrvrPrioritizeSAN(pOurTcpsrv, inst->iStrmDrvrSANPreference));
+	CHKiRet(tcpsrv.SetDrvrTlsVerifyDepth(pOurTcpsrv, inst->iStrmTlsVerifyDepth));
 	CHKiRet(tcpsrv.SetUseFlowControl(pOurTcpsrv, modConf->bUseFlowControl));
 	CHKiRet(tcpsrv.SetAddtlFrameDelim(pOurTcpsrv, modConf->iAddtlFrameDelim));
 	CHKiRet(tcpsrv.SetMaxFrameSize(pOurTcpsrv, modConf->maxFrameSize));
@@ -401,15 +426,19 @@ addListner(modConfData_t *modConf, instanceConf_t *inst)
 	CHKiRet(tcpsrv.SetDiscardTruncatedMsg(pOurTcpsrv, modConf->discardTruncatedMsg));
 	CHKiRet(tcpsrv.SetNotificationOnRemoteClose(pOurTcpsrv, modConf->bEmitMsgOnClose));
 	/* now set optional params, but only if they were actually configured */
-	if(modConf->pszStrmDrvrName != NULL) {
-		CHKiRet(tcpsrv.SetDrvrName(pOurTcpsrv, modConf->pszStrmDrvrName));
+	psz = (inst->pszStrmDrvrName == NULL) ? modConf->pszStrmDrvrName : inst->pszStrmDrvrName;
+	if(psz != NULL) {
+		CHKiRet(tcpsrv.SetDrvrName(pOurTcpsrv, psz));
 	}
-	if(modConf->pszStrmDrvrAuthMode != NULL) {
-		CHKiRet(tcpsrv.SetDrvrAuthMode(pOurTcpsrv, modConf->pszStrmDrvrAuthMode));
+	psz = (inst->pszStrmDrvrAuthMode == NULL) ? modConf->pszStrmDrvrAuthMode : inst->pszStrmDrvrAuthMode;
+	if(psz != NULL) {
+		CHKiRet(tcpsrv.SetDrvrAuthMode(pOurTcpsrv, psz));
 	}
 	/* Call SetDrvrPermitExpiredCerts required
 	 * when param is NULL default handling for ExpiredCerts is set! */
-	CHKiRet(tcpsrv.SetDrvrPermitExpiredCerts(pOurTcpsrv, modConf->pszStrmDrvrPermitExpiredCerts));
+	psz = (inst->pszStrmDrvrPermitExpiredCerts == NULL)
+			? modConf->pszStrmDrvrPermitExpiredCerts : inst->pszStrmDrvrPermitExpiredCerts;
+	CHKiRet(tcpsrv.SetDrvrPermitExpiredCerts(pOurTcpsrv, psz));
 	if(pPermPeersRoot != NULL) {
 		CHKiRet(tcpsrv.SetDrvrPermPeers(pOurTcpsrv, pPermPeersRoot));
 	}
@@ -483,6 +512,25 @@ CODESTARTnewInpInst
 			inst->bSPFramingFix = (int) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "ruleset")) {
 			inst->pszBindRuleset = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.mode")) {
+			inst->iStrmDrvrMode = (int) pvals[i].val.d.n;
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.CheckExtendedKeyPurpose")) {
+			inst->iStrmDrvrExtendedCertCheck = (int) pvals[i].val.d.n;
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.PrioritizeSAN")) {
+			inst->iStrmDrvrSANPreference = (int) pvals[i].val.d.n;
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.TlsVerifyDepth")) {
+			if (pvals[i].val.d.n >= 2) {
+				inst->iStrmTlsVerifyDepth = (int) pvals[i].val.d.n;
+			} else {
+				parser_errmsg("streamdriver.TlsVerifyDepth must be 2 or higher but is %d",
+									(int) pvals[i].val.d.n);
+			}
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.authmode")) {
+			inst->pszStrmDrvrAuthMode = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.permitexpiredcerts")) {
+			inst->pszStrmDrvrPermitExpiredCerts = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.name")) {
+			inst->pszStrmDrvrName = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "supportoctetcountedframing")) {
 			inst->cnf_params->bSuppOctetFram = (int) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "ratelimit.burst")) {
@@ -734,6 +782,9 @@ CODESTARTfreeCnf
 	}
 	for(inst = pModConf->root ; inst != NULL ; ) {
 		free((void*)inst->pszBindRuleset);
+		free((void*)inst->pszStrmDrvrAuthMode);
+		free((void*)inst->pszStrmDrvrName);
+		free((void*)inst->pszStrmDrvrPermitExpiredCerts);
 		free((void*)inst->pszInputName);
 		free((void*)inst->dfltTZ);
 		del = inst;

runtime/tcpsrv.c
@@ -1303,7 +1303,6 @@ static rsRetVal
 SetInputName(tcpsrv_t *const pThis,tcpLstnParams_t *const cnf_params, const uchar *const name)
 {
 	DEFiRet;
-	fprintf(stderr, "name %s\n", name);
 	ISOBJ_TYPE_assert(pThis, tcpsrv);
 	if(name == NULL)
 		cnf_params->pszInputName = NULL;
@@ -1349,7 +1348,7 @@ SetNotificationOnRemoteClose(tcpsrv_t *pThis, int bNewVal)
 
 /* set the driver mode -- rgerhards, 2008-04-30 */
 static rsRetVal
-SetDrvrMode(tcpsrv_t *pThis, int iMode)
+SetDrvrMode(tcpsrv_t *pThis, const int iMode)
 {
 	DEFiRet;
 	ISOBJ_TYPE_assert(pThis, tcpsrv);
@@ -1358,7 +1357,7 @@ SetDrvrMode(tcpsrv_t *pThis, int iMode)
 }
 
 static rsRetVal
-SetDrvrName(tcpsrv_t *pThis, uchar *name)
+SetDrvrName(tcpsrv_t *pThis, uchar *const name)
 {
 	DEFiRet;
 	ISOBJ_TYPE_assert(pThis, tcpsrv);
@@ -1370,7 +1369,7 @@ finalize_it:
 
 /* set the driver authentication mode -- rgerhards, 2008-05-19 */
 static rsRetVal
-SetDrvrAuthMode(tcpsrv_t *pThis, uchar *mode)
+SetDrvrAuthMode(tcpsrv_t *pThis, uchar *const mode)
 {
 	DEFiRet;
 	ISOBJ_TYPE_assert(pThis, tcpsrv);

tests/Makefile.am
@@ -1232,12 +1232,19 @@ TESTS +=  \
 	imtcp-tls-gtls-x509fingerprint-invld.sh \
 	imtcp-tls-gtls-x509fingerprint.sh \
 	imtcp-tls-gtls-x509name-invld.sh \
-	imtcp-tls-gtls-x509name.sh
+	imtcp-tls-gtls-x509name.sh \
+	imtcp-drvr-in-input-basic.sh \
+	imtcp-multi-drvr-basic.sh \
+	imtcp-multi-drvr-basic-parallel.sh
 if HAVE_VALGRIND
 TESTS += \
 	imtcp-tls-basic-vg.sh \
 	imtcp_conndrop_tls-vg.sh \
 	manytcp-too-few-tls-vg.sh
+endif
+if ENABLE_OPENSSL
+TESTS +=  \
+	imtcp-multi-drvr-basic-ptcp_gtls_ossl.sh
 endif
 endif
 
@@ -2067,6 +2074,10 @@ EXTRA_DIST= \
 	imtcp-tls-gtls-x509fingerprint.sh \
 	imtcp-tls-gtls-x509name-invld.sh \
 	imtcp-tls-gtls-x509name.sh \
+	imtcp-drvr-in-input-basic.sh \
+	imtcp-multi-drvr-basic.sh \
+	imtcp-multi-drvr-basic-parallel.sh \
+	imtcp-multi-drvr-basic-ptcp_gtls_ossl.sh \
 	imtcp-tls-basic.sh \
 	imtcp-tls-basic-verifydepth.sh \
 	imtcp-tls-basic-vg.sh \

tests/imtcp-drvr-in-input-basic.sh
@@ -1,0 +1,28 @@
+#!/bin/bash
+# added 2021-04-27 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+export NUMMESSAGES=5000
+export QUEUE_EMPTY_CHECK_FUNC=wait_seq_check
+generate_conf
+add_conf '
+global( defaultNetstreamDriverCAFile="'$srcdir'/tls-certs/ca.pem"
+	defaultNetstreamDriverCertFile="'$srcdir'/tls-certs/cert.pem"
+	defaultNetstreamDriverKeyFile="'$srcdir'/tls-certs/key.pem")
+
+module(load="../plugins/imtcp/.libs/imtcp")
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port"
+	StreamDriver.Name="gtls"
+	StreamDriver.Mode="1"
+	StreamDriver.AuthMode="anon" )
+
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+:msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+'
+startup
+tcpflood -p$TCPFLOOD_PORT -m$NUMMESSAGES -Ttls -x$srcdir/tls-certs/ca.pem -Z$srcdir/tls-certs/cert.pem -z$srcdir/tls-certs/key.pem
+shutdown_when_empty
+wait_shutdown
+seq_check
+exit_test

tests/imtcp-multi-drvr-basic-parallel.sh
@@ -1,0 +1,38 @@
+#!/bin/bash
+# This test checks imtcp functionality with multiple drivers running together. It is
+# a minimal test.
+# added 2021-04-27 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+export NUMMESSAGES=100000 # must be even number!
+export QUEUE_EMPTY_CHECK_FUNC=wait_seq_check
+generate_conf
+add_conf '
+global( defaultNetstreamDriverCAFile="'$srcdir'/tls-certs/ca.pem"
+	defaultNetstreamDriverCertFile="'$srcdir'/tls-certs/cert.pem"
+	defaultNetstreamDriverKeyFile="'$srcdir'/tls-certs/key.pem")
+
+module(load="../plugins/imtcp/.libs/imtcp")
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port"
+	StreamDriver.Name="gtls"
+	StreamDriver.Mode="1"
+	StreamDriver.AuthMode="anon" )
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port2")
+
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+:msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+'
+startup
+assign_tcpflood_port2 "$RSYSLOG_DYNNAME.tcpflood_port2"
+# Note: the two tcpflood instances are run in parallel with intent!
+# It stresses the rsyslog engine a bit more. If this fails, see if 
+# the non-parallel test also fails. If so, debug that one, because it
+# is easier to do!
+tcpflood -p$TCPFLOOD_PORT -m$((NUMMESSAGES / 2)) -Ttls -x$srcdir/tls-certs/ca.pem -Z$srcdir/tls-certs/cert.pem -z$srcdir/tls-certs/key.pem &
+tcpflood -p$TCPFLOOD_PORT2 -m$((NUMMESSAGES / 2)) -i$((NUMMESSAGES / 2))
+shutdown_when_empty
+wait_shutdown
+seq_check
+exit_test

tests/imtcp-multi-drvr-basic-ptcp_gtls_ossl.sh
@@ -1,0 +1,45 @@
+#!/bin/bash
+# This test checks imtcp functionality with multiple drivers running together. It is
+# a minimal test.
+# added 2021-04-27 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+export NUMMESSAGES=60000 # must be evenly dividable by 3
+export QUEUE_EMPTY_CHECK_FUNC=wait_seq_check
+generate_conf
+add_conf '
+global( defaultNetstreamDriverCAFile="'$srcdir'/tls-certs/ca.pem"
+	defaultNetstreamDriverCertFile="'$srcdir'/tls-certs/cert.pem"
+	defaultNetstreamDriverKeyFile="'$srcdir'/tls-certs/key.pem")
+
+module(load="../plugins/imtcp/.libs/imtcp")
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port"
+	name="i1"
+	StreamDriver.Name="gtls"
+	StreamDriver.Mode="1"
+	StreamDriver.AuthMode="anon" )
+
+input(type="imtcp" name="i2" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port2")
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port3"
+	name="i3"
+	StreamDriver.Name="ossl"
+	StreamDriver.Mode="1"
+	StreamDriver.AuthMode="anon" )
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port3" name="i3")
+
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+:msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+'
+startup
+assign_tcpflood_port2 "$RSYSLOG_DYNNAME.tcpflood_port2"
+assign_rs_port "$RSYSLOG_DYNNAME.tcpflood_port3"
+tcpflood -p$TCPFLOOD_PORT -m$((NUMMESSAGES / 3)) -Ttls -x$srcdir/tls-certs/ca.pem -Z$srcdir/tls-certs/cert.pem -z$srcdir/tls-certs/key.pem
+tcpflood -p$TCPFLOOD_PORT2 -m$((NUMMESSAGES / 3)) -i$((NUMMESSAGES / 3))
+tcpflood -p$RS_PORT -m$((NUMMESSAGES / 3)) -i$((NUMMESSAGES / 3 * 2)) -Ttls -x$srcdir/tls-certs/ca.pem -Z$srcdir/tls-certs/cert.pem -z$srcdir/tls-certs/key.pem
+shutdown_when_empty
+wait_shutdown
+seq_check
+exit_test

tests/imtcp-multi-drvr-basic.sh
@@ -1,0 +1,34 @@
+#!/bin/bash
+# This test checks imtcp functionality with multiple drivers running together. It is
+# a minimal test.
+# added 2021-04-27 by Rgerhards
+# This file is part of the rsyslog project, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+export NUMMESSAGES=50000 # must be even number!
+export QUEUE_EMPTY_CHECK_FUNC=wait_seq_check
+generate_conf
+add_conf '
+global( defaultNetstreamDriverCAFile="'$srcdir'/tls-certs/ca.pem"
+	defaultNetstreamDriverCertFile="'$srcdir'/tls-certs/cert.pem"
+	defaultNetstreamDriverKeyFile="'$srcdir'/tls-certs/key.pem")
+
+module(load="../plugins/imtcp/.libs/imtcp")
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port"
+	StreamDriver.Name="gtls"
+	StreamDriver.Mode="1"
+	StreamDriver.AuthMode="anon" )
+
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port2")
+
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+:msg, contains, "msgnum:" action(type="omfile" file="'$RSYSLOG_OUT_LOG'" template="outfmt")
+'
+startup
+assign_tcpflood_port2 "$RSYSLOG_DYNNAME.tcpflood_port2"
+tcpflood -p$TCPFLOOD_PORT -m$((NUMMESSAGES / 2)) -Ttls -x$srcdir/tls-certs/ca.pem -Z$srcdir/tls-certs/cert.pem -z$srcdir/tls-certs/key.pem
+tcpflood -p$TCPFLOOD_PORT2 -m$((NUMMESSAGES / 2)) -i$((NUMMESSAGES / 2))
+shutdown_when_empty
+wait_shutdown
+seq_check
+exit_test

commit 2623a89a0c66cced8fc37ac1daa0da936005bad6

imtcp bugfix: legacy config directives did no longer work
Many "$InputTCPServer..." config directives did no longer work
and were completely ignored (e.g. "$InputTCPServerStreamDriverMode").

This was a regression from a08591be5d9 (May, 5th 2021).

closes https://github.com/rsyslog/rsyslog/issues/5021

plugins/imtcp/imtcp.c
@@ -449,6 +449,20 @@ static rsRetVal addInstance(void __attribute__((unused)) *pVal, uchar *pNewVal)
 		CHKmalloc(inst->pszInputName = ustrdup(cs.pszInputName));
 	}
 	inst->cnf_params->bSuppOctetFram = cs.bSuppOctetFram;
+	inst->iStrmDrvrMode = cs.iStrmDrvrMode;
+	inst->bKeepAlive = cs.bKeepAlive ;
+	inst->bUseFlowControl = cs.bUseFlowControl;
+	inst->bDisableLFDelim = cs.bDisableLFDelim;
+	inst->bEmitMsgOnClose = cs.bEmitMsgOnClose;
+	inst->bPreserveCase = cs.bPreserveCase;
+	inst->iKeepAliveProbes = cs.iKeepAliveProbes;
+	inst->iKeepAliveIntvl = cs.iKeepAliveIntvl;
+	inst->iKeepAliveTime = cs.iKeepAliveTime;
+	inst->iKeepAliveTime = cs.iKeepAliveTime;
+	inst->iAddtlFrameDelim = cs.iAddtlFrameDelim;
+	inst->iTCPLstnMax = cs.iTCPLstnMax;
+	inst->iTCPSessMax = cs.iTCPSessMax;
+	inst->iStrmDrvrMode = cs.iStrmDrvrMode;
 
 finalize_it:
 	free(pNewVal);
@@ -954,7 +968,6 @@ RunServerThread(void *myself)
 {
 	tcpsrv_etry_t *const etry = (tcpsrv_etry_t*) myself;
 	rsRetVal iRet;
-	dbgprintf("RGER: running ety %p\n", etry);
 	iRet = tcpsrv.Run(etry->tcpsrv);
 	if(iRet != RS_RET_OK) {
 		LogError(0, iRet, "imtcp: error while terminating server; rsyslog may hang on shutdown");

runtime/nsd_gtls.c
@@ -1416,12 +1416,13 @@ ENDobjDestruct(nsd_gtls)
  * rgerhards, 2008-04-28
  */
 static rsRetVal
-SetMode(nsd_t *pNsd, int mode)
+SetMode(nsd_t *const pNsd, const int mode)
 {
 	DEFiRet;
 	nsd_gtls_t *pThis = (nsd_gtls_t*) pNsd;
 
 	ISOBJ_TYPE_assert((pThis), nsd_gtls);
+	dbgprintf("(tls) mode: %d\n", mode);
 	if(mode != 0 && mode != 1) {
 		LogError(0, RS_RET_INVALID_DRVR_MODE, "error: driver mode %d not supported by "
 				"gtls netstream driver", mode);

tests/imtcp-tls-gtls-x509name-legacy.sh
@@ -12,12 +12,14 @@ global(	defaultNetstreamDriverCAFile="'$srcdir/tls-certs/ca.pem'"
 
 # NOTE: we intentionally use legacy statements here! This *IS* what we want to test!
 $ModLoad ../plugins/imtcp/.libs/imtcp
+$DefaultNetstreamDriver gtls
 $inputTcpserverStreamdriverPermittedPeer rsyslog-client
 
-input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port"
-	StreamDriver.Name="gtls"
-	StreamDriver.Mode="1"
-	StreamDriver.AuthMode="x509/name")
+$InputTCPServerStreamDriverAuthMode x509/name
+$InputTCPServerStreamDriverPermittedPeer Log_Streaming_Client
+$InputTCPServerStreamDriverMode 1
+$InputTCPServerListenPortFile '$RSYSLOG_DYNNAME'.tcpflood_port
+$InputTCPServerRun 0
 
 template(name="outfmt" type="string" string="%msg:F,58:2%\n")
 :msg, contains, "msgnum:" action(	type="omfile" 