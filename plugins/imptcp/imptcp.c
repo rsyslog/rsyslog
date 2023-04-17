rsyslog/rsyslog

commit f052717178dac12f6891932667bdca07af2a0d46

imptcp: slight tuning
- reduce indirect addressing to obtain more speed
- also a fix for an annoying typo
- minor other optimizations
- modernization of one test

plugins/imptcp/imptcp.c
@@ -320,6 +320,8 @@ struct ptcpsess_s {
 	uchar *pMsg_save;	/* message (fragment) save area in regex framing mode */
 	prop_t *peerName;	/* host name we received messages from */
 	prop_t *peerIP;
+	const uchar *startRegex;/* cache for performance reasons */
+	int iAddtlFrameDelim;	/* cache for performance reasons */
 };
 
 
@@ -1076,7 +1078,7 @@ processDataRcvd_regexFraming(ptcpsess_t *const __restrict__ pThis,
  * rgerhards, 2008-03-14
  * EXTRACT from tcps_sess.c
  */
-static rsRetVal
+static rsRetVal ATTR_NONNULL(1, 2)
 processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 	char **buff,
 	const int buffLen,
@@ -1086,14 +1088,10 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 	unsigned *const __restrict__ pnMsgs)
 {
 	DEFiRet;
-	char c = **buff;
-	int octatesToCopy, octatesToDiscard;
-	uchar *propPeerName = NULL;
-	int lenPeerName = 0;
-	uchar *propPeerIP = NULL;
-	int lenPeerIP = 0;
+	const char c = **buff;
+	int octetsToCopy, octetsToDiscard;
 
-	if(pThis->pLstn->pSrv->inst->startRegex != NULL) {
+	if(pThis->startRegex != NULL) {
 		processDataRcvd_regexFraming(pThis, buff, stTime, ttGenTime, pMultiSub, pnMsgs);
 		FINALIZE;
 	}
@@ -1117,6 +1115,10 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 	}
 
 	if(pThis->inputState == eInOctetCnt) {
+		uchar *propPeerName = NULL;
+		int lenPeerName = 0;
+		uchar *propPeerIP = NULL;
+		int lenPeerIP = 0;
 		if(isdigit(c)) {
 			if(pThis->iOctetsRemain <= 200000000) {
 				pThis->iOctetsRemain = pThis->iOctetsRemain * 10 + c - '0';
@@ -1163,21 +1165,21 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 		}
 	} else if(pThis->inputState == eInMsgTruncation) {
 		if ((c == '\n')
-		|| ((pThis->pLstn->pSrv->iAddtlFrameDelim != TCPSRV_NO_ADDTL_DELIMITER)
-		&& (c == pThis->pLstn->pSrv->iAddtlFrameDelim))) {
+		|| ((pThis->iAddtlFrameDelim != TCPSRV_NO_ADDTL_DELIMITER)
+		&& (c == pThis->iAddtlFrameDelim))) {
 			pThis->inputState = eAtStrtFram;
 		}
 	} else {
 		assert(pThis->inputState == eInMsg);
-
 		if (pThis->eFraming == TCP_FRAMING_OCTET_STUFFING) {
-			if(pThis->iMsg >= iMaxLine) {
+			int iMsg = pThis->iMsg; /* cache value for faster access */
+			if(iMsg >= iMaxLine) {
 				/* emergency, we now need to flush, no matter if we are at end of message or not... */
 				int i = 1;
 				char currBuffChar;
 				while(i < buffLen && ((currBuffChar = (*buff)[i]) != '\n'
-					&& (pThis->pLstn->pSrv->iAddtlFrameDelim == TCPSRV_NO_ADDTL_DELIMITER
-						|| currBuffChar != pThis->pLstn->pSrv->iAddtlFrameDelim))) {
+					&& (pThis->iAddtlFrameDelim == TCPSRV_NO_ADDTL_DELIMITER
+						|| currBuffChar != pThis->iAddtlFrameDelim))) {
 					i++;
 				}
 				LogError(0, NO_ERRCODE, "imptcp %s: message received is at least %d byte larger than "
@@ -1195,21 +1197,23 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 				 */
 			}
 			if ((c == '\n')
-				   || ((pThis->pLstn->pSrv->iAddtlFrameDelim != TCPSRV_NO_ADDTL_DELIMITER)
-					   && (c == pThis->pLstn->pSrv->iAddtlFrameDelim))
+				   || ((pThis->iAddtlFrameDelim != TCPSRV_NO_ADDTL_DELIMITER)
+					   && (c == pThis->iAddtlFrameDelim))
 				   ) { /* record delimiter? */
 				if(pThis->pLstn->pSrv->multiLine) {
 					if((buffLen == 1) || ((*buff)[1] == '<')) {
 						doSubmitMsg(pThis, stTime, ttGenTime, pMultiSub);
+						iMsg = 0; /* Reset cached value! */
 						++(*pnMsgs);
 						pThis->inputState = eAtStrtFram;
 					} else {
-						if(pThis->iMsg < iMaxLine) {
-							*(pThis->pMsg + pThis->iMsg++) = c;
+						if(iMsg < iMaxLine) {
+							pThis->pMsg[iMsg++] = c;
 						}
 					}
 				} else {
 					doSubmitMsg(pThis, stTime, ttGenTime, pMultiSub);
+					iMsg = 0; /* Reset cached value! */
 					++(*pnMsgs);
 					pThis->inputState = eAtStrtFram;
 				}
@@ -1219,26 +1223,27 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 				 * we truncate it. This is the best we can do in light of what the engine supports.
 				 * -- rgerhards, 2008-03-14
 				 */
-				if(pThis->iMsg < iMaxLine) {
-					*(pThis->pMsg + pThis->iMsg++) = c;
+				if(likely(iMsg < iMaxLine)) {
+					pThis->pMsg[iMsg++] = c;
 				}
 			}
+			pThis->iMsg = iMsg; /* update "real value" with cached one */
 		} else {
 			assert(pThis->eFraming == TCP_FRAMING_OCTET_COUNTING);
-			octatesToCopy = pThis->iOctetsRemain;
-			octatesToDiscard = 0;
-			if (buffLen < octatesToCopy) {
-				octatesToCopy = buffLen;
+			octetsToCopy = pThis->iOctetsRemain;
+			octetsToDiscard = 0;
+			if (buffLen < octetsToCopy) {
+				octetsToCopy = buffLen;
 			}
-			if (octatesToCopy + pThis->iMsg > iMaxLine) {
-				octatesToDiscard = octatesToCopy - (iMaxLine - pThis->iMsg);
-				octatesToCopy = iMaxLine - pThis->iMsg;
+			if (octetsToCopy + pThis->iMsg > iMaxLine) {
+				octetsToDiscard = octetsToCopy - (iMaxLine - pThis->iMsg);
+				octetsToCopy = iMaxLine - pThis->iMsg;
 			}
 
-			memcpy(pThis->pMsg + pThis->iMsg, *buff, octatesToCopy);
-			pThis->iMsg += octatesToCopy;
-			pThis->iOctetsRemain -= (octatesToCopy + octatesToDiscard);
-			*buff += (octatesToCopy + octatesToDiscard - 1);
+			memcpy(pThis->pMsg + pThis->iMsg, *buff, octetsToCopy);
+			pThis->iMsg += octetsToCopy;
+			pThis->iOctetsRemain -= (octetsToCopy + octetsToDiscard);
+			*buff += (octetsToCopy + octetsToDiscard - 1);
 			if (pThis->iOctetsRemain == 0) {
 				/* we have end of frame! */
 				doSubmitMsg(pThis, stTime, ttGenTime, pMultiSub);
@@ -1270,8 +1275,8 @@ finalize_it:
  * we have just received a bunch of data! -- rgerhards, 2009-06-16
  * EXTRACT from tcps_sess.c
  */
-static rsRetVal
-DataRcvdUncompressed(ptcpsess_t *pThis, char *pData, size_t iLen, struct syslogTime *stTime, time_t ttGenTime)
+static rsRetVal ATTR_NONNULL(1, 2)
+DataRcvdUncompressed(ptcpsess_t *pThis, char *pData, const size_t iLen, struct syslogTime *stTime, time_t ttGenTime)
 {
 	multi_submit_t multiSub;
 	smsg_t *pMsgs[CONF_NUM_MULTISUB];
@@ -1279,7 +1284,6 @@ DataRcvdUncompressed(ptcpsess_t *pThis, char *pData, size_t iLen, struct syslogT
 	unsigned nMsgs = 0;
 	DEFiRet;
 
-	assert(pData != NULL);
 	assert(iLen > 0);
 
 	if(ttGenTime == 0)
@@ -1533,6 +1537,8 @@ addSess(ptcplstn_t *pLstn, int sock, prop_t *peerName, prop_t *peerIP)
 	pSess->peerName = peerName;
 	pSess->peerIP = peerIP;
 	pSess->compressionMode = pLstn->pSrv->compressionMode;
+	pSess->startRegex = pLstn->pSrv->inst->startRegex;
+	pSess->iAddtlFrameDelim = pLstn->pSrv->iAddtlFrameDelim;
 
 	/* add to start of server's listener list */
 	pSess->prev = NULL;

tests/imptcp_multi_line.sh
@@ -15,17 +15,13 @@ startup
 tcpflood -B -I ${srcdir}/testsuites/imptcp_multi_line.testdata
 shutdown_when_empty # shut down rsyslogd when done processing messages
 wait_shutdown       # and wait for it to terminate
-echo 'NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test1
+export EXPECTED='NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test1
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test2
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag multi#012line1
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag multi#012l#012i#012n#012#012e2
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test3
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag multi#012line3
 NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test4
-NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test end' | cmp - $RSYSLOG_OUT_LOG
-if [ ! $? -eq 0 ]; then
-  echo "invalid response generated, $RSYSLOG_OUT_LOG is:"
-  cat $RSYSLOG_OUT_LOG
-  error_exit  1
-fi;
+NEWMSG: <133>Mar  1 01:00:00 172.20.245.8 tag test end'
+cmp_exact
 exit_test

commit 2e8b48258f41bd33b1439ae6e3d2abcd05c5c1be

imptcp bugfix: spam log on oversize message
If an oversize message was received by imptcp, imptcp reported
one error message for EACH oversize character. This could
result in a potentially very large number of similar (and
useless) messages.

This is a regression from commit f052717178.

closes https://github.com/rsyslog/rsyslog/issues/5078

plugins/imptcp/imptcp.c
@@ -1186,6 +1186,7 @@ processDataRcvd(ptcpsess_t *const __restrict__ pThis,
 					"max msg size; message will be split starting at: \"%.*s\"\n",
 					pThis->pLstn->pSrv->pszInputName, i, (i < 32) ? i : 32, *buff);
 				doSubmitMsg(pThis, stTime, ttGenTime, pMultiSub);
+				iMsg = 0;
 				++(*pnMsgs);
 				if(pThis->pLstn->pSrv->discardTruncatedMsg == 1) {
 					pThis->inputState = eInMsgTruncation;