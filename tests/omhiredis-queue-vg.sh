4fa296727cfaa2768fa1da6091b3c19aabe52f78
frikilax <theo.bertin@advens.fr>
2023-05-17 12:13:52 +0200

    OMHIREDIS::ADDED:: new tests for existing functionalities

---
+++ tests/omhiredis-queue-vg.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# added 2023-04-20 by Théo Bertin, released under ASL 2.0
+## Uncomment for debugging
+#export RS_REDIR=-d
+
+export USE_VALGRIND="YES"
+source ${srcdir:=.}/omhiredis-queue.sh
