rsyslog/rsyslog

commit 844d1795a2b1d08547ff132645fbe1ba1f36b7c0

TLS CRL Support Issue 5081

plugins/imtcp/imtcp.c
@@ -144,6 +144,7 @@ struct instanceConf_s {
 	uchar *pszStrmDrvrAuthMode;
 	uchar *pszStrmDrvrPermitExpiredCerts;
 	uchar *pszStrmDrvrCAFile;
+	uchar *pszStrmDrvrCRLFile;
 	uchar *pszStrmDrvrKeyFile;
 	uchar *pszStrmDrvrCertFile;
 	permittedPeers_t *pPermPeersRoot;
@@ -185,6 +186,7 @@ struct modConfData_s {
 	uchar *pszStrmDrvrAuthMode; /* authentication mode to use */
 	uchar *pszStrmDrvrPermitExpiredCerts; /* control how to handly expired certificates */
 	uchar *pszStrmDrvrCAFile;
+	uchar *pszStrmDrvrCRLFile;
 	uchar *pszStrmDrvrKeyFile;
 	uchar *pszStrmDrvrCertFile;
 	permittedPeers_t *pPermPeersRoot;
@@ -370,6 +372,7 @@ createInstance(instanceConf_t **pinst)
 	inst->pszStrmDrvrAuthMode = NULL;
 	inst->pszStrmDrvrPermitExpiredCerts = NULL;
 	inst->pszStrmDrvrCAFile = NULL;
+	inst->pszStrmDrvrCRLFile = NULL;
 	inst->pszStrmDrvrKeyFile = NULL;
 	inst->pszStrmDrvrCertFile = NULL;
 	inst->pPermPeersRoot = NULL;
@@ -526,6 +529,10 @@ addListner(modConfData_t *modConf, instanceConf_t *inst)
 			? modConf->pszStrmDrvrCAFile : inst->pszStrmDrvrCAFile;
 	CHKiRet(tcpsrv.SetDrvrCAFile(pOurTcpsrv, psz));
 
+	psz = (inst->pszStrmDrvrCRLFile == NULL)
+			? modConf->pszStrmDrvrCRLFile : inst->pszStrmDrvrCRLFile;
+	CHKiRet(tcpsrv.SetDrvrCRLFile(pOurTcpsrv, psz));
+
 	psz = (inst->pszStrmDrvrKeyFile == NULL)
 			? modConf->pszStrmDrvrKeyFile : inst->pszStrmDrvrKeyFile;
 	CHKiRet(tcpsrv.SetDrvrKeyFile(pOurTcpsrv, psz));
@@ -628,6 +635,8 @@ CODESTARTnewInpInst
 			inst->pszStrmDrvrPermitExpiredCerts = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.cafile")) {
 			inst->pszStrmDrvrCAFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.crlfile")) {
+			inst->pszStrmDrvrCRLFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.keyfile")) {
 			inst->pszStrmDrvrKeyFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.certfile")) {
@@ -724,6 +733,7 @@ CODESTARTbeginCnfLoad
 	loadModConf->pszStrmDrvrAuthMode = NULL;
 	loadModConf->pszStrmDrvrPermitExpiredCerts = NULL;
 	loadModConf->pszStrmDrvrCAFile = NULL;
+	loadModConf->pszStrmDrvrCRLFile = NULL;
 	loadModConf->pszStrmDrvrKeyFile = NULL;
 	loadModConf->pszStrmDrvrCertFile = NULL;
 	loadModConf->pPermPeersRoot = NULL;
@@ -811,6 +821,8 @@ CODESTARTsetModCnf
 			loadModConf->pszStrmDrvrPermitExpiredCerts = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(modpblk.descr[i].name, "streamdriver.cafile")) {
 			loadModConf->pszStrmDrvrCAFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(modpblk.descr[i].name, "streamdriver.crlfile")) {
+			loadModConf->pszStrmDrvrCRLFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(modpblk.descr[i].name, "streamdriver.keyfile")) {
 			loadModConf->pszStrmDrvrKeyFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(modpblk.descr[i].name, "streamdriver.certfile")) {
@@ -937,6 +949,7 @@ CODESTARTfreeCnf
 	free(pModConf->pszStrmDrvrAuthMode);
 	free(pModConf->pszStrmDrvrPermitExpiredCerts);
 	free(pModConf->pszStrmDrvrCAFile);
+	free(pModConf->pszStrmDrvrCRLFile);
 	free(pModConf->pszStrmDrvrKeyFile);
 	free(pModConf->pszStrmDrvrCertFile);
 	if(pModConf->pPermPeersRoot != NULL) {
@@ -949,6 +962,7 @@ CODESTARTfreeCnf
 		free((void*)inst->pszStrmDrvrName);
 		free((void*)inst->pszStrmDrvrPermitExpiredCerts);
 		free((void*)inst->pszStrmDrvrCAFile);
+		free((void*)inst->pszStrmDrvrCRLFile);
 		free((void*)inst->pszStrmDrvrKeyFile);
 		free((void*)inst->pszStrmDrvrCertFile);
 		free((void*)inst->gnutlsPriorityString);

tools/omfwd.c
@@ -90,6 +90,7 @@ typedef struct _instanceData {
 	int iStrmDrvrSANPreference; /* ignore CN when any SAN set */
 	int iStrmTlsVerifyDepth; /**< Verify Depth for certificate chains */
 	const uchar *pszStrmDrvrCAFile;
+	const uchar *pszStrmDrvrCRLFile;
 	const uchar *pszStrmDrvrKeyFile;
 	const uchar *pszStrmDrvrCertFile;
 	char	*target;
@@ -434,6 +435,7 @@ CODESTARTfreeInstance
 	free(pData->address);
 	free(pData->device);
 	free((void*)pData->pszStrmDrvrCAFile);
+	free((void*)pData->pszStrmDrvrCRLFile);
 	free((void*)pData->pszStrmDrvrKeyFile);
 	free((void*)pData->pszStrmDrvrCertFile);
 	net.DestructPermittedPeers(&pData->pPermPeers);
@@ -827,6 +829,7 @@ static rsRetVal TCPSendInit(void *pvData)
 		CHKiRet(netstrm.SetDrvrPermitExpiredCerts(pWrkrData->pNetstrm,
 			pData->pszStrmDrvrPermitExpiredCerts));
 		CHKiRet(netstrm.SetDrvrTlsCAFile(pWrkrData->pNetstrm, pData->pszStrmDrvrCAFile));
+		CHKiRet(netstrm.SetDrvrTlsCRLFile(pWrkrData->pNetstrm, pData->pszStrmDrvrCRLFile));
 		CHKiRet(netstrm.SetDrvrTlsKeyFile(pWrkrData->pNetstrm, pData->pszStrmDrvrKeyFile));
 		CHKiRet(netstrm.SetDrvrTlsCertFile(pWrkrData->pNetstrm, pData->pszStrmDrvrCertFile));
 
@@ -1214,6 +1217,7 @@ setInstParamDefaults(instanceData *pData)
 	pData->iStrmDrvrSANPreference = 0;
 	pData->iStrmTlsVerifyDepth = 0;
 	pData->pszStrmDrvrCAFile = NULL;
+	pData->pszStrmDrvrCRLFile = NULL;
 	pData->pszStrmDrvrKeyFile = NULL;
 	pData->pszStrmDrvrCertFile = NULL;
 	pData->iRebindInterval = 0;
@@ -1376,6 +1380,8 @@ CODESTARTnewActInst
 			}
 		} else if(!strcmp(actpblk.descr[i].name, "streamdriver.cafile")) {
 			pData->pszStrmDrvrCAFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(actpblk.descr[i].name, "streamdriver.crlfile")) {
+			pData->pszStrmDrvrCRLFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(actpblk.descr[i].name, "streamdriver.keyfile")) {
 			pData->pszStrmDrvrKeyFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(actpblk.descr[i].name, "streamdriver.certfile")) {

runtime/glbl.c
@@ -115,6 +115,7 @@ static struct cnfparamdescr cnfparamdescr[] = {
 	{ "debug.gnutls", eCmdHdlrNonNegInt, 0 },
 	{ "debug.unloadmodules", eCmdHdlrBinary, 0 },
 	{ "defaultnetstreamdrivercafile", eCmdHdlrString, 0 },
+	{ "defaultnetstreamdrivercrlfile", eCmdHdlrString, 0 },
 	{ "defaultnetstreamdriverkeyfile", eCmdHdlrString, 0 },
 	{ "defaultnetstreamdrivercertfile", eCmdHdlrString, 0 },
 	{ "defaultnetstreamdriver", eCmdHdlrString, 0 },
@@ -262,6 +263,7 @@ SIMP_PROP(ParseHOSTNAMEandTAG, parser.bParseHOSTNAMEandTAG, int)
 SIMP_PROP(OptionDisallowWarning, optionDisallowWarning, int)
 /* We omit setter on purpose, because we want to customize it */
 SIMP_PROP_GET(DfltNetstrmDrvrCAF, pszDfltNetstrmDrvrCAF, uchar*)
+SIMP_PROP_GET(DfltNetstrmDrvrCRLF, pszDfltNetstrmDrvrCRLF, uchar*)
 SIMP_PROP_GET(DfltNetstrmDrvrCertFile, pszDfltNetstrmDrvrCertFile, uchar*)
 SIMP_PROP_GET(DfltNetstrmDrvrKeyFile, pszDfltNetstrmDrvrKeyFile, uchar*)
 SIMP_PROP_GET(NetstrmDrvrCAExtraFiles, pszNetstrmDrvrCAExtraFiles, uchar*)
@@ -449,6 +451,25 @@ setNetstrmDrvrCAExtraFiles(void __attribute__((unused)) *pVal, uchar *pNewVal) {
 	}
 	RETiRet;
 }
+
+static rsRetVal
+setDfltNetstrmDrvrCRLF(void __attribute__((unused)) *pVal, uchar *pNewVal) {
+	DEFiRet;
+	FILE *fp;
+	free(loadConf->globals.pszDfltNetstrmDrvrCRLF);
+	fp = fopen((const char*)pNewVal, "r");
+	if(fp == NULL) {
+		LogError(errno, RS_RET_NO_FILE_ACCESS,
+			"error: defaultnetstreamdrivercrlfile file '%s' "
+			"could not be accessed", pNewVal);
+	} else {
+		fclose(fp);
+		loadConf->globals.pszDfltNetstrmDrvrCRLF = pNewVal;
+	}
+
+	RETiRet;
+}
+
 
 static rsRetVal
 setDfltNetstrmDrvrCertFile(void __attribute__((unused)) *pVal, uchar *pNewVal) {
@@ -926,6 +947,7 @@ CODESTARTobjQueryInterface(glbl)
 	pIf->GetMaxLine = glblGetMaxLine;
 	pIf->GetOptionDisallowWarning = GetOptionDisallowWarning;
 	pIf->GetDfltNetstrmDrvrCAF = GetDfltNetstrmDrvrCAF;
+	pIf->GetDfltNetstrmDrvrCRLF = GetDfltNetstrmDrvrCRLF;
 	pIf->GetDfltNetstrmDrvrCertFile = GetDfltNetstrmDrvrCertFile;
 	pIf->GetDfltNetstrmDrvrKeyFile = GetDfltNetstrmDrvrKeyFile;
 	pIf->GetDfltNetstrmDrvr = GetDfltNetstrmDrvr;
@@ -964,6 +986,8 @@ static rsRetVal resetConfigVariables(uchar __attribute__((unused)) *pp, void __a
 	loadConf->globals.pszDfltNetstrmDrvr = NULL;
 	free(loadConf->globals.pszDfltNetstrmDrvrCAF);
 	loadConf->globals.pszDfltNetstrmDrvrCAF = NULL;
+	free(loadConf->globals.pszDfltNetstrmDrvrCRLF);
+	loadConf->globals.pszDfltNetstrmDrvrCRLF = NULL;
 	free(loadConf->globals.pszDfltNetstrmDrvrKeyFile);
 	loadConf->globals.pszDfltNetstrmDrvrKeyFile = NULL;
 	free(loadConf->globals.pszDfltNetstrmDrvrCertFile);
@@ -1209,6 +1233,9 @@ glblDoneLoadCnf(void)
 		} else if(!strcmp(paramblk.descr[i].name, "defaultnetstreamdrivercafile")) {
 			cstr = (uchar*) es_str2cstr(cnfparamvals[i].val.d.estr, NULL);
 			setDfltNetstrmDrvrCAF(NULL, cstr);
+		} else if(!strcmp(paramblk.descr[i].name, "defaultnetstreamdrivercrlfile")) {
+			cstr = (uchar*) es_str2cstr(cnfparamvals[i].val.d.estr, NULL);
+			setDfltNetstrmDrvrCRLF(NULL, cstr);
 		} else if(!strcmp(paramblk.descr[i].name, "defaultnetstreamdriver")) {
 			cstr = (uchar*) es_str2cstr(cnfparamvals[i].val.d.estr, NULL);
 			setDfltNetstrmDrvr(NULL, cstr);
@@ -1438,6 +1465,8 @@ BEGINAbstractObjClassInit(glbl, 1, OBJ_IS_CORE_MODULE) /* class, version */
 	NULL));
 	CHKiRet(regCfSysLineHdlr((uchar *)"defaultnetstreamdrivercafile", 0, eCmdHdlrGetWord,
 	setDfltNetstrmDrvrCAF, NULL, NULL));
+	CHKiRet(regCfSysLineHdlr((uchar *)"defaultnetstreamdrivercrlfile", 0, eCmdHdlrGetWord,
+	setDfltNetstrmDrvrCRLF, NULL, NULL));
 	CHKiRet(regCfSysLineHdlr((uchar *)"defaultnetstreamdriverkeyfile", 0, eCmdHdlrGetWord,
 	setDfltNetstrmDrvrKeyFile, NULL, NULL));
 	CHKiRet(regCfSysLineHdlr((uchar *)"defaultnetstreamdrivercertfile", 0, eCmdHdlrGetWord,

runtime/glbl.h
@@ -91,6 +91,7 @@ BEGINinterface(glbl) /* name must also be changed in ENDinterface macro! */
 
 	SIMP_PROP(DropMalPTRMsgs, int)
 	SIMP_PROP(DfltNetstrmDrvrCAF, uchar*)
+	SIMP_PROP(DfltNetstrmDrvrCRLF, uchar*)
 	SIMP_PROP(DfltNetstrmDrvrCertFile, uchar*)
 	SIMP_PROP(DfltNetstrmDrvrKeyFile, uchar*)
 	SIMP_PROP(DfltNetstrmDrvr, uchar*)

runtime/netstrm.c
@@ -266,6 +266,15 @@ SetDrvrTlsCAFile(netstrm_t *const pThis, const uchar *const file)
 }
 
 static rsRetVal
+SetDrvrTlsCRLFile(netstrm_t *const pThis, const uchar *const file)
+{
+	DEFiRet;
+	ISOBJ_TYPE_assert(pThis, netstrm);
+	iRet = pThis->Drvr.SetTlsCRLFile(pThis->pDrvrData, file);
+	RETiRet;
+}
+
+static rsRetVal
 SetDrvrTlsKeyFile(netstrm_t *const pThis, const uchar *const file)
 {
 	DEFiRet;
@@ -470,6 +479,7 @@ CODESTARTobjQueryInterface(netstrm)
 	pIf->SetDrvrPrioritizeSAN = SetDrvrPrioritizeSAN;
 	pIf->SetDrvrTlsVerifyDepth = SetDrvrTlsVerifyDepth;
 	pIf->SetDrvrTlsCAFile = SetDrvrTlsCAFile;
+	pIf->SetDrvrTlsCRLFile = SetDrvrTlsCRLFile;
 	pIf->SetDrvrTlsKeyFile = SetDrvrTlsKeyFile;
 	pIf->SetDrvrTlsCertFile = SetDrvrTlsCertFile;
 finalize_it:

runtime/netstrm.h
@@ -89,10 +89,11 @@ BEGINinterface(netstrm) /* name must also be changed in ENDinterface macro! */
 
 	/* v15 -- Tls cert functions */
 	rsRetVal (*SetDrvrTlsCAFile)(netstrm_t *pThis, const uchar* file);
+	rsRetVal (*SetDrvrTlsCRLFile)(netstrm_t *pThis, const uchar* file);
 	rsRetVal (*SetDrvrTlsKeyFile)(netstrm_t *pThis, const uchar* file);
 	rsRetVal (*SetDrvrTlsCertFile)(netstrm_t *pThis, const uchar* file);
 ENDinterface(netstrm)
-#define netstrmCURR_IF_VERSION 15 /* increment whenever you change the interface structure! */
+#define netstrmCURR_IF_VERSION 16 /* increment whenever you change the interface structure! */
 /* interface version 3 added GetRemAddr()
  * interface version 4 added EnableKeepAlive() -- rgerhards, 2009-06-02
  * interface version 5 changed return of CheckConnection from void to rsRetVal -- alorbach, 2012-09-06
@@ -101,6 +102,7 @@ ENDinterface(netstrm)
  * interface version 8 changed signature of Connect() -- dsa, 2016-11-14
  * interface version 9 added SetGnutlsPriorityString -- PascalWithopf, 2017-08-08
  * interface version 10 added oserr parameter to Rcv() -- rgerhards, 2017-09-04
+ * interface version 16 CRL file -- Oracle, 2022-01-16
  * */
 
 /* prototypes */

runtime/netstrms.c
@@ -116,6 +116,8 @@ CODESTARTobjDestruct(netstrms)
 	}
 	free((void*)pThis->pszDrvrCAFile);
 	pThis->pszDrvrCAFile = NULL;
+	free((void*)pThis->pszDrvrCRLFile);
+	pThis->pszDrvrCRLFile = NULL;
 	free((void*)pThis->pszDrvrKeyFile);
 	pThis->pszDrvrKeyFile = NULL;
 	free((void*)pThis->pszDrvrCertFile);
@@ -245,6 +247,18 @@ SetDrvrTlsCAFile(netstrms_t *pThis, const uchar *mode)
 	ISOBJ_TYPE_assert(pThis, netstrms);
 	if (mode != NULL) {
 		CHKmalloc(pThis->pszDrvrCAFile = (uchar*) strdup((char*)mode));
+	}
+finalize_it:
+	RETiRet;
+}
+
+static rsRetVal
+SetDrvrTlsCRLFile(netstrms_t *pThis, const uchar *mode)
+{
+	DEFiRet;
+	ISOBJ_TYPE_assert(pThis, netstrms);
+	if (mode != NULL) {
+		CHKmalloc(pThis->pszDrvrCRLFile = (uchar*) strdup((char*)mode));
 	}
 finalize_it:
 	RETiRet;
@@ -394,6 +408,12 @@ GetDrvrTlsCAFile(netstrms_t *pThis)
 	return pThis->pszDrvrCAFile;
 }
 static const uchar *
+GetDrvrTlsCRLFile(netstrms_t *pThis)
+{
+	ISOBJ_TYPE_assert(pThis, netstrms);
+	return pThis->pszDrvrCRLFile;
+}
+static const uchar *
 GetDrvrTlsKeyFile(netstrms_t *pThis)
 {
 	ISOBJ_TYPE_assert(pThis, netstrms);
@@ -471,9 +491,11 @@ CODESTARTobjQueryInterface(netstrms)
 	pIf->SetDrvrTlsVerifyDepth = SetDrvrTlsVerifyDepth;
 	pIf->GetDrvrTlsVerifyDepth = GetDrvrTlsVerifyDepth;
 	pIf->GetDrvrTlsCAFile = GetDrvrTlsCAFile;
+	pIf->GetDrvrTlsCRLFile = GetDrvrTlsCRLFile;
 	pIf->GetDrvrTlsKeyFile = GetDrvrTlsKeyFile;
 	pIf->GetDrvrTlsCertFile = GetDrvrTlsCertFile;
 	pIf->SetDrvrTlsCAFile = SetDrvrTlsCAFile;
+	pIf->SetDrvrTlsCRLFile = SetDrvrTlsCRLFile;
 	pIf->SetDrvrTlsKeyFile = SetDrvrTlsKeyFile;
 	pIf->SetDrvrTlsCertFile = SetDrvrTlsCertFile;
 finalize_it:

runtime/netstrms.h
@@ -38,6 +38,7 @@ struct netstrms_s {
 	int DrvrVerifyDepth;		/**< Verify Depth for certificate chains */
 	uchar *pszDrvrPermitExpiredCerts;
 	const uchar *pszDrvrCAFile;
+	const uchar *pszDrvrCRLFile;
 	const uchar *pszDrvrKeyFile;
 	const uchar *pszDrvrCertFile;
 	uchar *gnutlsPriorityString; /**< priorityString for connection */
@@ -78,9 +79,11 @@ BEGINinterface(netstrms) /* name must also be changed in ENDinterface macro! */
 	const uchar* (*GetDrvrTlsKeyFile)(netstrms_t *pThis);
 	rsRetVal (*SetDrvrTlsCertFile)(netstrms_t *pThis, const uchar *);
 	const uchar* (*GetDrvrTlsCertFile)(netstrms_t *pThis);
-
+	/* v3 */
+	rsRetVal (*SetDrvrTlsCRLFile)(netstrms_t *pThis, const uchar *);
+	const uchar* (*GetDrvrTlsCRLFile)(netstrms_t *pThis);
 ENDinterface(netstrms)
-#define netstrmsCURR_IF_VERSION 2 /* increment whenever you change the interface structure! */
+#define netstrmsCURR_IF_VERSION 3 /* increment whenever you change the interface structure! */
 
 /* prototypes */
 PROTOTYPEObj(netstrms);

commit 281b681df0db524ff635c5158081736c4b8645b0

tls bugfix: parameter 'StreamDriver.CRLFile' not known
imtcp: fix parameter 'StreamDriver.CRLFile' cannot be recognized in input()
omfwd: fix parameter 'StreamDriver.CRLFile' cannot be recognized in action()

Fixes: 844d1795a2b1 ("TLS CRL Support Issue 5081")

Signed-off-by: Qiumiao Zhang <zhangqiumiao1@huawei.com>

plugins/imtcp/imtcp.c
@@ -257,6 +257,7 @@ static struct cnfparamdescr inppdescr[] = {
 	{ "streamdriver.PrioritizeSAN", eCmdHdlrBinary, 0 },
 	{ "streamdriver.TlsVerifyDepth", eCmdHdlrPositiveInt, 0 },
 	{ "streamdriver.cafile", eCmdHdlrString, 0 },
+	{ "streamdriver.crlfile", eCmdHdlrString, 0 },
 	{ "streamdriver.keyfile", eCmdHdlrString, 0 },
 	{ "streamdriver.certfile", eCmdHdlrString, 0 },
 	{ "permittedpeer", eCmdHdlrArray, 0 },

tools/omfwd.c
@@ -213,6 +213,7 @@ static struct cnfparamdescr actpdescr[] = {
 	{ "streamdriver.PrioritizeSAN", eCmdHdlrBinary, 0 },
 	{ "streamdriver.TlsVerifyDepth", eCmdHdlrPositiveInt, 0 },
 	{ "streamdriver.cafile", eCmdHdlrString, 0 },
+	{ "streamdriver.crlfile", eCmdHdlrString, 0 },
 	{ "streamdriver.keyfile", eCmdHdlrString, 0 },
 	{ "streamdriver.certfile", eCmdHdlrString, 0 },
 	{ "resendlastmsgonreconnect", eCmdHdlrBinary, 0 },