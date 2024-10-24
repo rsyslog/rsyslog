ece410f017d0870d2197494891cba7a6613e907b
Andre lorbach <alorbach@adiscon.com>
2024-10-24 15:05:06 +0200

    Updated sync scripts for pkg base container
    - support setting ssh Port for rsync command using env var
    - export REPOSSHPORT parameter to ENV (run*.sh scripts).

--- dev_env/fedora/pkg_base/36/extra/sync_remote.sh
+++ dev_env/fedora/pkg_base/36/extra/sync_remote.sh
@@ -6,33 +6,36 @@ source $szBaseDir/config.sh
 cd $szBaseDir/
 echo "-------------------------------------"
 echo "--- Basedir: $szBaseDir"
-echo "--- Sync remote repository	---"
+echo "--- Sync remote repository        ---"
 echo "-------------------------------------"
 
-if [ -z $RPM_REPO ]; then 
+if [ -z $RPM_REPO ]; then
         echo "sync_remote: RPM_REPO not set"
-	addrepo=
-	addplatform=
-	addarch=
+        addrepo=
+        addplatform=
+        addarch=
 else
-        echo "sync_remote: RPM_REPO set to		$RPM_REPO"
-	addrepo=$RPM_REPO/
-	if [ -z $RPM_PLATFORM ]; then
-		echo "sync_remote: RPM_PLATFORM not set"
-		addplatform=
-		addarch=
-	else
-		echo "sync_remote: RPM_PLATFORM set to	$RPM_PLATFORM"
-		addplatform=$RPM_PLATFORM/
-		if [ -z $RPM_ARCH ]; then
-			echo "sync_remote: RPM_ARCH not set"
-			addarch=
-		else
-			echo "sync_remote: RPM_ARCH set to		$RPM_ARCH"
-			addarch=$RPM_ARCH/
-		fi
-	fi
+        echo "sync_remote: RPM_REPO set to              $RPM_REPO"
+        addrepo=$RPM_REPO/
+        if [ -z $RPM_PLATFORM ]; then
+                echo "sync_remote: RPM_PLATFORM not set"
+                addplatform=
+                addarch=
+        else
+                echo "sync_remote: RPM_PLATFORM set to  $RPM_PLATFORM"
+                addplatform=$RPM_PLATFORM/
+                if [ -z $RPM_ARCH ]; then
+                        echo "sync_remote: RPM_ARCH not set"
+                        addarch=
+                else
+                        echo "sync_remote: RPM_ARCH set to              $RPM_ARCH"
+                        addarch=$RPM_ARCH/
+                fi
+        fi
 fi
 
+# Set default SSH port to 22 if REPOSSHPORT is not set
+REPOSSHPORT=${REPOSSHPORT:-22}
+
 echo "rsync $REPOUSERNAME@$REPOURL/$addrepo$addplatform$addarch to $szBaseDir/yumrepo/$addrepo$addplatform$addarch"
-rsync -avh --update -e "ssh -i /private-files/.ssh/id_rsa" --progress $REPOUSERNAME@$REPOURL/$addrepo$addplatform$addarch $szBaseDir/yumrepo/$addrepo$addplatform$addarch
+rsync -avh --update -e "ssh -i /private-files/.ssh/id_rsa -p $REPOSSHPORT" --progress $REPOUSERNAME@$REPOURL/$addrepo$addplatform$addarch $szBaseDir/yumrepo/$addrepo$addplatform$addarch
