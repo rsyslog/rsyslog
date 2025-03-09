87632911c459e38f9f7f396ffafa37a58b86c805
Rainer Gerhards <rgerhards@adiscon.com>
2025-03-09 18:59:35 +0100

    omsendertrack: first shot at maintaining the sender hash table

---
+++ tests/omsendertrack-basic.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# addd 2016-05-13 by RGerhards, released under ASL 2.0
+. ${srcdir:=.}/diag.sh init
+export NUMMESSAGES=10
+export QUEUE_EMPTY_CHECK_FUNC=wait_file_lines
+generate_conf
+add_conf '
+module(load="../plugins/omsendertrack/.libs/omsendertrack")
+module(load="../plugins/imtcp/.libs/imtcp")
+input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port")
+
+template(name="hostname" type="string" string="%hostname%")
+action(type="omsendertrack" template="hostname")
+
+# The following is just to find a terminating condition for message injection
+template(name="outfmt" type="string" string="%msg:F,58:2%\n")
+:msg, contains, "msgnum:" action(type="omfile" template="outfmt"
+			         file="'$RSYSLOG_OUT_LOG'")
+'
+startup
+tcpflood -m $NUMMESSAGES
+shutdown_when_empty
+wait_shutdown
+seq_check
+exit_test
