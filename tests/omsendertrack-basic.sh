2f6ae7ac21e22def8016bc207099d0f2a38b67d2
Rainer Gerhards <rgerhards@adiscon.com>
2025-03-13 09:22:04 +0100

    sender stats output, nearing real implementation

--- tests/omsendertrack-basic.sh
+++ tests/omsendertrack-basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # addd 2016-05-13 by RGerhards, released under ASL 2.0
 . ${srcdir:=.}/diag.sh init
-export NUMMESSAGES=10
+export NUMMESSAGES=10 # MUST be an even number!
 export QUEUE_EMPTY_CHECK_FUNC=wait_file_lines
 generate_conf
 add_conf '
@@ -10,7 +10,7 @@ module(load="../plugins/imtcp/.libs/imtcp")
 input(type="imtcp" port="0" listenPortFileName="'$RSYSLOG_DYNNAME'.tcpflood_port")
 
 template(name="hostname" type="string" string="%hostname%")
-action(type="omsendertrack" template="hostname")
+action(type="omsendertrack" template="hostname" statefile="'$RSYSLOG_DYNNAME'.sendertrack")
 
 # The following is just to find a terminating condition for message injection
 template(name="outfmt" type="string" string="%msg:F,58:2%\n")
@@ -18,9 +18,15 @@ template(name="outfmt" type="string" string="%msg:F,58:2%\n")
 			         file="'$RSYSLOG_OUT_LOG'")
 '
 startup
-tcpflood -m $NUMMESSAGES
-sleep 4
+tcpflood -h sender1.example.net -m $(( NUMMESSAGES / 2 - 1 ))
+tcpflood -h sender2.example.net -m 1 -i $(( NUMMESSAGES / 2 - 1 )) # this sender must have firstseen <> lastseen
+sleep 2
+tcpflood -h sender2.example.net -m $(( NUMMESSAGES / 2 )) -i $(( NUMMESSAGES / 2 ))
+./msleep 20
 shutdown_when_empty
+echo
+cat ${RSYSLOG_DYNNAME}.sendertrack
+echo
 wait_shutdown
 seq_check
 exit_test
