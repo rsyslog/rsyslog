88dccee42306f6cfd33eb0e2ece33df2322919ec
Rainer Gerhards <rgerhards@adiscon.com>
2025-06-18 13:13:58 +0200

    official containers: bump rsyslog release nbr for next cycle

--- dev_env/ubuntu/base/24.04/tag-previous.sh
+++ dev_env/ubuntu/base/24.04/tag-previous.sh
@@ -1,2 +1,2 @@
-docker tag rsyslog/rsyslog_dev_base_ubuntu:24.04 rsyslog/rsyslog_dev_base_ubuntu:22.04_previous
+docker tag rsyslog/rsyslog_dev_base_ubuntu:24.04 rsyslog/rsyslog_dev_base_ubuntu:24.04_previous
 docker push rsyslog/rsyslog_dev_base_ubuntu:24.04_previous
