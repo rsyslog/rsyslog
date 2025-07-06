rsyslog/rsyslog

commit ad1fd213a79bf802a2af2258c380378d4d69fdb2

imtcp: major multithreading and performance improvements
This commit significantly enhances imtcp by introducing a fully
functional worker thread pool, enabling true multi-threaded
processing for better scalability under high loads. This is
particularly beneficial when using TLS connections.

Notable changes:
- Implemented a complete worker pool for imtcp.
- Introduced the `workerthreads` config parameter for tuning
  concurrency.
- Improved epoll efficiency by enabling edge-triggered mode.
- Added starvation handling via `starvationProtection.maxReads`.
- Refactored session accept logic and optimized network object
  handling.
- Removed an obsolete network driver layer for event notification.
- Fixed multiple issues related to message timing, EPOLLERR
  handling, and tests.
- Improved performance in poll() mode by reducing redundant
  allocations.
- Introduced new CI tests for imtcp without epoll.
- Allowed disabling imtcp tests via a new configure switch.
- Added new impstats counters for worker thread pool statistics.

Details:
- The worker pool replaces an outdated experimental
  implementation.
- If `workerthreads=1`, no worker pool is created to minimize
  context switches.
- Moves worker pool variables inside `tcpsrv` instance to
  prevent conflicts.
- Extracts session `accept()` logic into a dedicated function
  for clarity.
- Fixes message ordering inconsistencies in multi-threaded
  scenarios.
- Properly handles `EPOLLERR` notifications to improve error
  resilience.
- Optimizes poll() mode by avoiding unnecessary reallocation
  of file descriptors.
- Replaces the old network driver layer for event notification
  with a streamlined solution.
  - Now uses **conditional compilation** to select the best
    method (epoll or poll) at build time.
  - This significantly reduces code complexity, improves
    maintainability, and boosts performance.
- The previous "thread pool" was a rough experiment that did

runtime/tcpsrv.h
@@ -21,6 +21,10 @@
 #ifndef INCLUDED_TCPSRV_H
 #define INCLUDED_TCPSRV_H
 
+#if defined(ENABLE_IMTCP_EPOLL) && defined(HAVE_SYS_EPOLL_H)
+#	include <sys/epoll.h>
+#endif
+
 #include "obj.h"
 #include "prop.h"
 #include "net.h"
@@ -60,6 +64,47 @@ struct tcpLstnPortList_s {
 	tcpLstnPortList_t *pNext;	/**< next port or NULL */
 };
 
+
+typedef struct tcpsrvWrkrData_s {
+	statsobj_t *stats;
+	STATSCOUNTER_DEF(ctrRuns, mutCtrRuns);
+	STATSCOUNTER_DEF(ctrRead, mutCtrRead);
+	STATSCOUNTER_DEF(ctrStarvation, mutCtrStarvation);
+	STATSCOUNTER_DEF(ctrAccept, mutCtrAccept);
+} tcpsrvWrkrData_t;
+
+typedef struct workQueue_s {
+	tcpsrv_io_descr_t *head;
+	tcpsrv_io_descr_t *tail;
+	pthread_mutex_t mut;
+	pthread_cond_t workRdy;
+	unsigned numWrkr;	/* how many workers to spawn */
+	pthread_t *wrkr_tids;	/* array of thread IDs */
+	tcpsrvWrkrData_t *wrkr_data;
+} workQueue_t;
+
+/**
+ * The following structure is a descriptor for tcpsrv i/o. It is
+ * primarily used together with epoll at the moment.
+ */
+struct tcpsrv_io_descr_s {
+	int id;		/* index into listener or session table, depending on ptrType */
+	int sock;	/* socket descriptor we need to "monitor" */
+	enum {NSD_PTR_TYPE_LSTN, NSD_PTR_TYPE_SESS} ptrType;
+	union {
+		tcps_sess_t *pSess;
+		netstrm_t **ppLstn;	/**<  accept listener's netstream */
+	} ptr;
+	int isInError; /* boolean, if set, subsystem indicates we need to close because we had an
+			* unrecoverable error at the network layer. */
+	tcpsrv_t *pSrv;	/* our server object */
+	tcpsrv_io_descr_t *next; /* for use in workQueue_t */
+	#if defined(ENABLE_IMTCP_EPOLL)
+	struct epoll_event event; /* to re-enable EPOLLONESHOT */
+	#endif
+	DEF_ATOMIC_HELPER_MUT(mut_isInError);
+};
+
 #define TCPSRV_NO_ADDTL_DELIMITER -1 /* specifies that no additional delimiter is to be used in TCP framing */
 
 /* the tcpsrv object */
@@ -83,18 +128,29 @@ struct tcpsrv_s {
 	uchar *pszDrvrKeyFile;
 	uchar *pszDrvrCertFile;
 	uchar *pszDrvrName;	/**< name of stream driver to use */
-	uchar *pszInputName;	/**< value to be used as input name */ // TODO: REMOVE ME!!!!
+	uchar *pszInputName;	/**< value to be used as input name */
 	uchar *pszOrigin;		/**< module to be used as "origin" (e.g. for pstats) */
 	ruleset_t *pRuleset;	/**< ruleset to bind to */
 	permittedPeers_t *pPermPeers;/**< driver's permitted peers */
 	sbool bEmitMsgOnClose;	/**< emit an informational message when the remote peer closes connection */
 	sbool bEmitMsgOnOpen;
-	sbool bUsingEPoll;	/**< are we in epoll mode (means we do not need to keep track of sessions!) */
 	sbool bUseFlowControl;	/**< use flow control (make light delayable) */
 	sbool bSPFramingFix;	/**< support work-around for broken Cisco ASA framing? */
 	int iLstnCurr;		/**< max nbr of listeners currently supported */
 	netstrm_t **ppLstn;	/**< our netstream listeners */
+	/* We could use conditional compilation, but that causes more complexity and is (proofen causing errors) */
+	union {
+		struct {
+			int efd;
+		} epoll;
+		struct {
+			uint32_t maxfds;
+			uint32_t currfds;
+			struct pollfd *fds;
+		} poll;
+	} evtdata;
 	tcpLstnPortList_t **ppLstnPort; /**< pointer to relevant listen port description */
+	tcpsrv_io_descr_t **ppioDescrPtr; /**< pointer to i/o descriptor object */
 	int iLstnMax;		/**< max number of listeners supported */
 	int iSessMax;		/**< max number of sessions supported */
 	uchar dfltTZ[8];	/**< default TZ if none in timestamp; '\0' =No Default */
@@ -110,6 +166,7 @@ struct tcpsrv_s {
 	unsigned int ratelimitInterval;
 	unsigned int ratelimitBurst;
 	tcps_sess_t **pSessions;/**< array of all of our sessions */
+	unsigned int starvationMaxReads;
 	void *pUsr;		/**< a user-settable pointer (provides extensibility for "derived classes")*/
 	/* callbacks */
 	int      (*pIsPermittedHost)(struct sockaddr *addr, char *fromHostFQDN, void*pUsrSrv, void*pUsrSess);
@@ -124,14 +181,13 @@ struct tcpsrv_s {
 	rsRetVal (*OnSessConstructFinalize)(void*);
 	rsRetVal (*pOnSessDestruct)(void*);
 	rsRetVal (*OnMsgReceive)(tcps_sess_t *, uchar *pszMsg, int iLenMsg); /* submit message callback */
+	/* work queue */
+	workQueue_t workQueue;
+	int currWrkrs;
 };
 
 
 /**
- * The following structure is a set of descriptors that need to be processed.
- * This set will be the result of the epoll or select call and be used
- * in the actual request processing stage. It serves as a basis
- * to run multiple request by concurrent threads. -- rgerhards, 2011-01-24
  */
 struct tcpsrv_workset_s {
 	int idx;	/**< index into session table (or -1 if listener) */
@@ -216,8 +272,11 @@ BEGINinterface(tcpsrv) /* name must also be changed in ENDinterface macro! */
 	rsRetVal (*SetDrvrCRLFile)(tcpsrv_t *pThis, uchar *pszMode);
 	/* added v27 -- sync backlog for listen() */
 	rsRetVal (*SetSynBacklog)(tcpsrv_t *pThis, int);
+	/* added v28 */
+	rsRetVal (*SetNumWrkr)(tcpsrv_t *pThis, int);
+	rsRetVal (*SetStarvationMaxReads)(tcpsrv_t *pThis, unsigned int);
 ENDinterface(tcpsrv)
-#define tcpsrvCURR_IF_VERSION 27 /* increment whenever you change the interface structure! */
+#define tcpsrvCURR_IF_VERSION 28 /* increment whenever you change the interface structure! */
 /* change for v4:
  * - SetAddtlFrameDelim() added -- rgerhards, 2008-12-10
  * - SetInputName() added -- rgerhards, 2008-12-10

.github/workflows/run_checks.yml
@@ -36,6 +36,7 @@ jobs:
       fail-fast: false
       matrix:
         config: [centos_7, centos_8, debian_10, debian_11,
+                 ubuntu_24_imtcp_no_epoll,
                  fedora_35, fedora_36,
                  ubuntu_18, ubuntu_20, ubuntu_24,
                  ubuntu_22_san, ubuntu_24_tsan, ubuntu_22_codecov, ubuntu_22_distcheck,
@@ -78,6 +79,21 @@ jobs:
               export CI_VALGRIND_SUPPRESSIONS='centos7.supp'
               export RSYSLOG_CONFIGURE_OPTIONS_EXTRA="--disable-elasticsearch-tests --disable-kafka-tests \
                             --without-valgrind-testbench --enable-imdtls --enable-omdtls"
+              ;;
+          'ubuntu_24_imtcp_no_epoll')
+              # This check tests if imtcp runs in poll (select) mode. We have only slow CI runners for
+              # platforms where this is the case, thus we do a quick run on ubuntu where we also have all
+              # thread and memory debuggers available.
+              export RSYSLOG_DEV_CONTAINER='rsyslog/rsyslog_dev_base_ubuntu:24.04'
+              export CI_VALGRIND_SUPPRESSIONS="ubuntu22.04.supp"
+              # Note: we completely override the container configure options here!
+              export RSYSLOG_CONFIGURE_OPTIONS_OVERRIDE="--enable-testbench --enable-omstdout \
+                     --enable-imdiag --disable-imtcp-epoll --enable-imtcp-tests\
+                     --disable-impstats --disable-imfile --disable-imfile-tests \
+                     --disable-fmhttp --enable-valgrind --enable-valgrind-testbench \
+                     --disable-helgrind --disable-default-tests --disable-kafka-tests \
+                     --disable-omkafka --disable-imkafka \
+                     --enable-gnutls --enable-openssl --enable-gnutls-tests"
               ;;
           'fedora_35')
               export RSYSLOG_DEV_CONTAINER='rsyslog/rsyslog_dev_base_fedora:35'
@@ -130,7 +146,7 @@ jobs:
               ;;
           'ubuntu_24_tsan')
               export RSYSLOG_DEV_CONTAINER='rsyslog/rsyslog_dev_base_ubuntu:24.04'
-              export CI_MAKE_CHECK_OPT='-j3' # This is CPU-heavy due to tsan, so we need less concurrency to prevent flakes
+              export CI_MAKE_CHECK_OPT='-j2' # This is CPU-heavy due to tsan, so we need less concurrency to prevent flakes
               export CI_VALGRIND_SUPPRESSIONS="ubuntu22.04.supp"
               export CI_SANITIZE_BLACKLIST='tests/tsan.supp'
               export CC='clang'
@@ -169,7 +185,7 @@ jobs:
               export RSYSLOG_CONFIGURE_OPTIONS_OVERRIDE="--enable-testbench --enable-omstdout \
                      --enable-imdiag --disable-impstats --enable-imfile --disable-imfile-tests \
                      --disable-fmhttp --enable-valgrind --enable-valgrind-testbench \
-                     --disable-helgrind --disable-default-tests --enable-kafka-tests \
+                     --disable-helgrind --disable-default-tests --disable-imtcp-tests --enable-kafka-tests \
                      --enable-omkafka --enable-gnutls --disable-gnutls-tests --enable-imkafka"
               ;;
           'elasticsearch')
@@ -180,7 +196,7 @@ jobs:
               export ABORT_ALL_ON_TEST_FAIL='NO'
               export RSYSLOG_CONFIGURE_OPTIONS_OVERRIDE="--enable-testbench --enable-omstdout \
                      --enable-imdiag --enable-impstats --enable-imfile --disable-imfile-tests \
-                     --disable-fmhttp --enable-valgrind --disable-default-tests \
+                     --disable-fmhttp --enable-valgrind --disable-default-tests --disable-imtcp-tests \
                      --enable-elasticsearch-tests --enable-elasticsearch"
               export CI_MAKE_OPT='-j20'
               export CI_MAKE_CHECK_OPT='-j8'

configure.ac
@@ -233,7 +233,7 @@ AC_TYPE_SIGNAL
 AC_FUNC_STAT
 AC_FUNC_STRERROR_R
 AC_FUNC_VPRINTF
-AC_CHECK_FUNCS([flock recvmmsg basename alarm clock_gettime gethostbyname gethostname gettimeofday localtime_r memset mkdir regcomp select setsid socket strcasecmp strchr strdup strerror strndup strnlen strrchr strstr strtol strtoul uname ttyname_r getline malloc_trim prctl epoll_create epoll_create1 fdatasync syscall lseek64 asprintf close_range])
+AC_CHECK_FUNCS([flock recvmmsg basename alarm clock_gettime gethostbyname gethostname gettimeofday localtime_r memset mkdir regcomp select setsid socket strcasecmp strchr strdup strerror strndup strnlen strrchr strstr strtol strtoul uname ttyname_r getline malloc_trim prctl epoll_create epoll_create1 fdatasync syscall lseek64 asprintf close_range pthread_setname_np])
 AC_CHECK_FUNC([setns], [AC_DEFINE([HAVE_SETNS], [1], [Define if setns exists.])])
 AC_CHECK_TYPES([off64_t])
 
@@ -1792,8 +1792,6 @@ AM_CONDITIONAL(ENABLE_LIBFAKETIME, test "x${enable_libfaketime}" = "xyes")
 # are those tests that do not need a special configure option. There are
 # some situations where we really want to turn them of so that we can
 # run tests only for a specific component (e.g. ElasticSearch).
-# This also enables us to do some parallel testing even while the
-# testbench is not yet able to support make -j check
 AC_ARG_ENABLE(default-tests,
         [AS_HELP_STRING([--enable-default-tests],[default-tests enabled @<:@default=yes@:>@])],
         [case "${enableval}" in
@@ -1810,6 +1808,17 @@ if test "x${IP}" = "xno"; then
 	AC_MSG_NOTICE([Will not check network namespace functionality as 'ip' (part of iproute2) is not available.])
 fi
 AM_CONDITIONAL(ENABLE_IP, test "x${IP}" = "xyes")
+
+AC_ARG_ENABLE(imtcp-tests,
+        [AS_HELP_STRING([--enable-imtcp-tests],[imtcp-tests enabled @<:@default=yes@:>@])],
+        [case "${enableval}" in
+         yes) enable_imtcp_tests="yes" ;;
+          no) enable_imtcp_tests="no" ;;
+           *) AC_MSG_ERROR(bad value ${enableval} for --enable-imtcp-tests) ;;
+         esac],
+        [enable_imtcp_tests=yes]
+)
+AM_CONDITIONAL(ENABLE_IMTCP_TESTS, test "x${enable_imtcp_tests}" = "xyes")
 
 
 # valgrind-testbench
@@ -2016,6 +2025,58 @@ AC_ARG_ENABLE(imsolaris,
         [enable_imsolaris=no]
 )
 AM_CONDITIONAL(ENABLE_IMSOLARIS, test x$enable_imsolaris = xyes)
+
+
+# settings for the imtcp input module
+AC_ARG_ENABLE(imtcp_epoll,
+        [AS_HELP_STRING([--disable-imtcp-epoll],[Disable epoll mode in the TCP input module (for testing purposes)])],
+        [case "${enableval}" in
+         yes) enable_imtcp_epoll="yes" ;;
+          no) enable_imtcp_epoll="no" ;;
+           *) AC_MSG_ERROR([bad value ${enableval} for --disable-imtcp-epoll]) ;;
+         esac],
+        [enable_imtcp_epoll=auto]
+)
+
+# Default: Enable imtcp_epoll if epoll is available, unless explicitly disabled
+if test "x$enable_imtcp_epoll" = "xauto"; then
+    if test "x$ac_cv_header_sys_epoll_h" = "xyes" && test "x$ac_cv_func_epoll_create" = "xyes"; then
+        enable_imtcp_epoll="yes"
+    else
+        enable_imtcp_epoll="no"
+    fi
+fi
+
+# Define ENABLE_IMTCP_EPOLL in config.h if epoll is enabled
+if test "x$enable_imtcp_epoll" = "xyes"; then
+    AC_DEFINE([ENABLE_IMTCP_EPOLL], [1], [Enable epoll mode for the imtcp input module])
+fi
+
+# Automake conditional
+AM_CONDITIONAL(ENABLE_IMTCP_EPOLL, test x$enable_imtcp_epoll = xyes)
+
+ Print result
+echo "    TCP input module epoll mode: $enable_imtcp_epoll"
+
+
+
+
+# settings for the imtcp input module
+AC_ARG_ENABLE(imtcp_epoll,
+        [AS_HELP_STRING([--enable-imtcp-epoll],[tcp input module epoll mode permitted @<:@default=yes@:>@])],
+        [case "${enableval}" in
+         yes) enable_imtcp_epoll="yes" ;;
+          no) enable_imtcp_epoll="no" ;;
+           *) AC_MSG_ERROR(bad value ${enableval} for --enable-imptcp-epoll) ;;
+         esac],
+        [enable_imptcp_epoll=yes]
+)
+AM_CONDITIONAL(ENABLE_IMPTCP_EPOLL, test x$enable_imptcp_epoll = xyes)
+# Define a preprocessor macro if enabled
+if test "x$enable_imtcp_epoll" = "xyes"; then
+    AC_DEFINE([ENABLE_IMTCP_EPOLL], [1], [Enable epoll mode for the imtcp input module])
+fi
+
 
 # settings for the ptcp input module
 AC_ARG_ENABLE(imptcp,
@@ -3010,6 +3071,7 @@ if test "$unamestr" != "AIX"; then
 echo "    Klog functionality enabled:               $enable_klog ($os_type)"
 fi
 echo "    /dev/kmsg functionality enabled:          $enable_kmsg"
+echo "    tcp input module epoll permitted:         $enable_imtcp_epoll"
 echo "    plain tcp input module enabled:           $enable_imptcp"
 echo "    DTLS udp input module enabled:            $enable_imdtls"
 echo "    imdiag enabled:                           $enable_imdiag"
@@ -3124,6 +3186,7 @@ echo "    Kafka Tests enabled:                      $enable_kafka_tests"
 echo "    Redis Tests enabled:                      $enable_redis_tests"
 echo "    Omazureeventhubs Tests enabled:           $enable_omazureeventhubs_tests"
 echo "    Imdocker Tests enabled:                   $enable_imdocker_tests"
+echo "    imtcp tests enabled:                      $enable_imtcp_tests"
 echo "    gnutls tests enabled:                     $enable_gnutls_tests"
 echo "    imfile tests enabled:                     $enable_imfile_tests"
 echo "    systemd journal tests enabled:            $enable_journal_tests"

plugins/imdiag/imdiag.c
@@ -617,6 +617,7 @@ addTCPListener(void __attribute__((unused)) *pVal, uchar *pNewVal)
 	}
 	CHKmalloc(cnf_params = (tcpLstnParams_t*) calloc(1, sizeof(tcpLstnParams_t)));
 	CHKiRet(tcpsrv.Construct(&pOurTcpsrv));
+	CHKiRet(tcpsrv.SetNumWrkr(pOurTcpsrv, 1)); // We always need only one worker for this diag module!
 	CHKiRet(tcpsrv.SetSessMax(pOurTcpsrv, iTCPSessMax));
 	CHKiRet(tcpsrv.SetCBIsPermittedHost(pOurTcpsrv, isPermittedHost));
 	CHKiRet(tcpsrv.SetCBRcvData(pOurTcpsrv, doRcvData));

plugins/imtcp/imtcp.c
@@ -91,6 +91,12 @@ static int n_tcpsrv = 0;
 
 static permittedPeers_t *pPermPeersRoot = NULL;
 
+/* default number of workes to configure. We choose 2, as this is probably good for
+ * many installations. High-Volume ones may need much higher number!
+ */
+#define DEFAULT_NUMWRKR 2
+#define DEFAULT_STARVATIONMAXREADS 500
+
 #define FRAMING_UNSET -1
 
 /* config settings */
@@ -123,6 +129,7 @@ static struct configSettings_s {
 struct instanceConf_s {
 	int iTCPSessMax;
 	int iTCPLstnMax;
+	unsigned numWrkr;
 	tcpLstnParams_t *cnf_params;	/**< listener config parameters */
 	uchar *pszBindRuleset;		/* name of ruleset to bind to */
 	ruleset_t *pBindRuleset;	/* ruleset to bind listener to (use system default if unspecified) */
@@ -157,6 +164,7 @@ struct instanceConf_s {
 	int iKeepAliveIntvl;
 	int iKeepAliveProbes;
 	int iKeepAliveTime;
+	unsigned starvationMaxReads;
 	struct instanceConf_s *next;
 };
 
@@ -166,6 +174,7 @@ struct modConfData_s {
 	instanceConf_t *root, *tail;
 	int iTCPSessMax; /* max number of sessions */
 	int iTCPLstnMax; /* max number of sessions */
+	unsigned numWrkr;
 	int iStrmDrvrMode; /* mode for stream driver, driver-dependent (0 mostly means plain tcp) */
 	int iStrmDrvrExtendedCertCheck; /* verify also purpose OID in certificate extended field */
 	int iStrmDrvrSANPreference; /* ignore CN when any SAN set */
@@ -193,6 +202,7 @@ struct modConfData_s {
 	permittedPeers_t *pPermPeersRoot;
 	sbool configSetViaV2Method;
 	sbool bPreserveCase; /* preserve case of fromhost; true by default */
+	unsigned starvationMaxReads;
 };
 
 static modConfData_t *loadModConf = NULL;/* modConf ptr to use for the current load process */
@@ -211,6 +221,8 @@ static struct cnfparamdescr modpdescr[] = {
 	{ "maxsessions", eCmdHdlrPositiveInt, 0 },
 	{ "maxlistners", eCmdHdlrPositiveInt, 0 },
 	{ "maxlisteners", eCmdHdlrPositiveInt, 0 },
+	{ "workerthreads", eCmdHdlrPositiveInt, 0 },
+	{ "starvationprotection.maxreads", eCmdHdlrNonNegInt, 0 },
 	{ "streamdriver.mode", eCmdHdlrNonNegInt, 0 },
 	{ "streamdriver.authmode", eCmdHdlrString, 0 },
 	{ "streamdriver.permitexpiredcerts", eCmdHdlrString, 0 },
@@ -241,6 +253,7 @@ static struct cnfparamdescr inppdescr[] = {
 	{ "port", eCmdHdlrString, CNFPARAM_REQUIRED }, /* legacy: InputTCPServerRun */
 	{ "maxsessions", eCmdHdlrPositiveInt, 0 },
 	{ "maxlisteners", eCmdHdlrPositiveInt, 0 },
+	{ "workerthreads", eCmdHdlrPositiveInt, 0 },
 	{ "flowcontrol", eCmdHdlrBinary, 0 },
 	{ "disablelfdelimiter", eCmdHdlrBinary, 0 },
 	{ "discardtruncatedmsg", eCmdHdlrBinary, 0 },
@@ -254,6 +267,7 @@ static struct cnfparamdescr inppdescr[] = {
 	{ "name", eCmdHdlrString, 0 },
 	{ "defaulttz", eCmdHdlrString, 0 },
 	{ "ruleset", eCmdHdlrString, 0 },
+	{ "starvationprotection.maxreads", eCmdHdlrNonNegInt, 0 },
 	{ "streamdriver.mode", eCmdHdlrNonNegInt, 0 },
 	{ "streamdriver.authmode", eCmdHdlrString, 0 },
 	{ "streamdriver.permitexpiredcerts", eCmdHdlrString, 0 },
@@ -403,6 +417,8 @@ createInstance(instanceConf_t **pinst)
 	inst->iSynBacklog = 0; /* default: unset */
 	inst->iTCPLstnMax = loadModConf->iTCPLstnMax;
 	inst->iTCPSessMax = loadModConf->iTCPSessMax;
+	inst->numWrkr = loadModConf->numWrkr;
+	inst->starvationMaxReads = loadModConf->starvationMaxReads;
 
 	inst->cnf_params->pszLstnPortFileName = NULL;
 
@@ -473,6 +489,8 @@ static rsRetVal addInstance(void __attribute__((unused)) *pVal, uchar *pNewVal)
 	inst->iAddtlFrameDelim = cs.iAddtlFrameDelim;
 	inst->iTCPLstnMax = cs.iTCPLstnMax;
 	inst->iTCPSessMax = cs.iTCPSessMax;
+	inst->numWrkr = DEFAULT_NUMWRKR;
+	inst->starvationMaxReads = DEFAULT_STARVATIONMAXREADS;
 	inst->iStrmDrvrMode = cs.iStrmDrvrMode;
 
 finalize_it:
@@ -497,6 +515,8 @@ addListner(modConfData_t *modConf, instanceConf_t *inst)
 	CHKiRet(tcpsrv.SetCBOnRegularClose(pOurTcpsrv, onRegularClose));
 	CHKiRet(tcpsrv.SetCBOnErrClose(pOurTcpsrv, onErrClose));
 	/* params */
+	CHKiRet(tcpsrv.SetNumWrkr(pOurTcpsrv, inst->numWrkr));
+	CHKiRet(tcpsrv.SetStarvationMaxReads(pOurTcpsrv, inst->starvationMaxReads));
 	CHKiRet(tcpsrv.SetKeepAlive(pOurTcpsrv, inst->bKeepAlive));
 	CHKiRet(tcpsrv.SetKeepAliveIntvl(pOurTcpsrv, inst->iKeepAliveIntvl));
 	CHKiRet(tcpsrv.SetKeepAliveProbes(pOurTcpsrv, inst->iKeepAliveProbes));
@@ -652,6 +672,8 @@ CODESTARTnewInpInst
 			inst->pszStrmDrvrCertFile = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "streamdriver.name")) {
 			inst->pszStrmDrvrName = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
+		} else if(!strcmp(inppblk.descr[i].name, "starvationprotection.maxreads")) {
+			inst->starvationMaxReads = (unsigned) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "gnutlsprioritystring")) {
 			inst->gnutlsPriorityString = (uchar*)es_str2cstr(pvals[i].val.d.estr, NULL);
 		} else if(!strcmp(inppblk.descr[i].name, "permittedpeer")) {
@@ -685,6 +707,8 @@ CODESTARTnewInpInst
 			inst->iTCPSessMax = (int) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "maxlisteners")) {
 			inst->iTCPLstnMax = (int) pvals[i].val.d.n;
+		} else if(!strcmp(inppblk.descr[i].name, "workerthreads")) {
+			inst->numWrkr = (int) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "supportoctetcountedframing")) {
 			inst->cnf_params->bSuppOctetFram = (int) pvals[i].val.d.n;
 		} else if(!strcmp(inppblk.descr[i].name, "keepalive")) {
@@ -723,6 +747,8 @@ CODESTARTbeginCnfLoad
 	/* init our settings */
 	loadModConf->iTCPSessMax = 200;
 	loadModConf->iTCPLstnMax = 20;
+	loadModConf->numWrkr = DEFAULT_NUMWRKR;
+	loadModConf->starvationMaxReads = DEFAULT_STARVATIONMAXREADS;
 	loadModConf->bSuppOctetFram = 1;
 	loadModConf->iStrmDrvrMode = 0;
 	loadModConf->iStrmDrvrExtendedCertCheck = 0;
@@ -800,9 +826,13 @@ CODESTARTsetModCnf
 			}
 		} else if(!strcmp(modpblk.descr[i].name, "maxsessions")) {
 			loadModConf->iTCPSessMax = (int) pvals[i].val.d.n;
+		} else if(!strcmp(modpblk.descr[i].name, "starvationprotection.maxreads")) {
+			loadModConf->starvationMaxReads = (unsigned) pvals[i].val.d.n;
 		} else if(!strcmp(modpblk.descr[i].name, "maxlisteners") ||
 			  !strcmp(modpblk.descr[i].name, "maxlistners")) { /* keep old name for a while */
 			loadModConf->iTCPLstnMax = (int) pvals[i].val.d.n;
+		} else if(!strcmp(modpblk.descr[i].name, "workerthreads")) {
+			loadModConf->numWrkr = (int) pvals[i].val.d.n;
 		} else if(!strcmp(modpblk.descr[i].name, "keepalive")) {
 			loadModConf->bKeepAlive = (int) pvals[i].val.d.n;
 		} else if(!strcmp(modpblk.descr[i].name, "keepalive.probes")) {
@@ -1078,7 +1108,6 @@ CODESTARTafterRun
 	tcpsrv_etry_t *del;
 	while(etry != NULL) {
 		iRet = tcpsrv.Destruct(&etry->tcpsrv);
-		// TODO: check iRet, reprot error
 		del = etry;
 		etry = etry->next;
 		free(del);

commit c5cd7e57c4f82bd05f71d2f8169c3e8a4399e0db

imtcp: prevent double-enqueue of descriptors via inQueue flag
This patch adds an inQueue flag with its own mutex to each
tcpsrv_io_descr_t structure. The flag prevents multiple worker threads
from processing the same descriptor at the same time.

The change was motivated by segmentation faults reported in production
systems after commit ad1fd21, which introduced a worker thread pool to
imtcp. We could not reproduce the faults ourselves, but code analysis
suggests several race conditions may exist.

In particular:

- epoll_wait may return the same descriptor multiple times. This is not
  expected, as we use EPOLLONESHOT. However, if a thread does not clear
  or re-arm the event quickly enough, or in edge cases involving race
  conditions and rapid I/O activity, duplicate delivery may still occur.

- If a descriptor is enqueued more than once, multiple threads may
  process and free it in parallel, causing use-after-free errors.

- closeSess releases the session mutex before destroying the session and
  descriptor. A second thread might still be waiting to acquire the
  mutex and access the now-freed memory.

- shutdown is unordered: stopWrkrPool waits for threads to join, but the
  work queue may still contain descriptors that will be processed after
  their memory has been freed.

- pending epoll events for a socket may still be processed after
  epoll_ctl(..., DEL) was called, leading to access to invalid memory.

The patch:

- Adds an inQueue flag to each descriptor and a mutex to protect it.
- Prevents enqueueWork from queuing a descriptor already in queue.
- Clears the flag when dequeueing the descriptor.
- Initializes and destroys the new mutex at listener startup/cleanup.

While unverified, we believe this patch is a safe and helpful change.
It may fix the reported crashes and in general improves correctness.

The analysis and draft of this patch were created with help from a
Codex-based AI agent. Final review and edits were done by a human.

runtime/tcpsrv.c
@@ -828,6 +828,7 @@ finalize_it:
 	#if defined(ENABLE_IMTCP_EPOLL)
 		/* in epoll mode, ioDescr is dynamically allocated */
 		DESTROY_ATOMIC_HELPER_MUT(pioDescr->mut_isInError);
+		DESTROY_ATOMIC_HELPER_MUT(pioDescr->mut_inQueue);
 		free(pioDescr);
 	#else
 		pThis->pSessions[pioDescr->id] = NULL;
@@ -1020,6 +1021,9 @@ doSingleAccept(tcpsrv_io_descr_t *const pioDescr)
 			pDescrNew->id = idx;
 			pDescrNew->isInError = 0;
 			INIT_ATOMIC_HELPER_MUT(pDescrNew->mut_isInError);
+			/* track if descriptor is already queued */
+			pDescrNew->inQueue = 0;
+		INIT_ATOMIC_HELPER_MUT(pDescrNew->mut_inQueue);
 			pDescrNew->ptrType = NSD_PTR_TYPE_SESS;
 			pDescrNew->ioDirection = NSDSEL_RD;
 			CHKiRet(netstrm.GetSock(pNewSess->pStrm, &pDescrNew->sock));
@@ -1110,6 +1114,8 @@ startWrkrPool(tcpsrv_t *const pThis)
 	CHKmalloc(pThis->workQueue.wrkr_tids = calloc(queue->numWrkr, sizeof(pthread_t)));
 	CHKmalloc(pThis->workQueue.wrkr_data = calloc(queue->numWrkr, sizeof(tcpsrvWrkrData_t)));
 	pThis->currWrkrs = 0;
+	queue->head = NULL;
+	queue->tail = NULL;
 	pthread_mutex_init(&queue->mut, NULL);
 	pthread_cond_init(&queue->workRdy, NULL);
 	for(unsigned i = 0; i < queue->numWrkr; i++) {
@@ -1165,6 +1171,7 @@ dequeueWork(tcpsrv_t *pSrv)
 	if(queue->head == NULL) {
 		queue->tail = NULL;
 	}
+	ATOMIC_STORE_0_TO_INT(&pioDescr->inQueue, &pioDescr->mut_inQueue);
 
 finalize_it:
 	pthread_mutex_unlock(&queue->mut);
@@ -1177,18 +1184,20 @@ enqueueWork(tcpsrv_io_descr_t *const pioDescr)
 	workQueue_t *const queue = &pioDescr->pSrv->workQueue;
 	DEFiRet;
 
-	pthread_mutex_lock(&queue->mut);
-	pioDescr->next = NULL;
-	if(queue->tail == NULL) {
-		assert(queue->head == NULL);
-		queue->head = pioDescr;
-	} else {
-		queue->tail->next = pioDescr;
-	}
-	queue->tail = pioDescr;
+	if(ATOMIC_CAS(&pioDescr->inQueue, 0, 1, &pioDescr->mut_inQueue)) {
+		pthread_mutex_lock(&queue->mut);
+		pioDescr->next = NULL;
+		if(queue->tail == NULL) {
+			assert(queue->head == NULL);
+			queue->head = pioDescr;
+		} else {
+			queue->tail->next = pioDescr;
+		}
+		queue->tail = pioDescr;
 
-	pthread_cond_signal(&queue->workRdy);
-	pthread_mutex_unlock(&queue->mut);
+		pthread_cond_signal(&queue->workRdy);
+		pthread_mutex_unlock(&queue->mut);
+	}
 
 	RETiRet;
 }
@@ -1228,7 +1237,7 @@ wrkr(void *arg)
 		DBGPRINTF("pthread_setname_np failed, not setting thread name for '%s'\n", thrdName);
 	}
 #	endif
-	
+
 	if((localRet = statsobj.Construct(&wrkrData->stats)) == RS_RET_OK) {
 		statsobj.SetName(wrkrData->stats, thrdName);
 		statsobj.SetOrigin(wrkrData->stats, (uchar*)"imtcp");
@@ -1458,7 +1467,9 @@ RunEpoll(tcpsrv_t *const pThis)
 		pThis->ppioDescrPtr[i]->pSrv = pThis;
 		pThis->ppioDescrPtr[i]->id = i;
 		pThis->ppioDescrPtr[i]->isInError = 0;
+		pThis->ppioDescrPtr[i]->inQueue = 0;
 		INIT_ATOMIC_HELPER_MUT(pThis->ppioDescrPtr[i]->mut_isInError);
+		INIT_ATOMIC_HELPER_MUT(pThis->ppioDescrPtr[i]->mut_inQueue);
 		CHKiRet(netstrm.GetSock(pThis->ppLstn[i], &(pThis->ppioDescrPtr[i]->sock)));
 		pThis->ppioDescrPtr[i]->ptrType = NSD_PTR_TYPE_LSTN;
 		pThis->ppioDescrPtr[i]->ptr.ppLstn = pThis->ppLstn;
@@ -1489,6 +1500,7 @@ RunEpoll(tcpsrv_t *const pThis)
 	for(i = 0 ; i < pThis->iLstnCurr ; ++i) {
 		CHKiRet(epoll_Ctl(pThis, pThis->ppioDescrPtr[i], 1, EPOLL_CTL_DEL));
 		DESTROY_ATOMIC_HELPER_MUT(pThis->ppioDescrPtr[i]->mut_isInError);
+		DESTROY_ATOMIC_HELPER_MUT(pThis->ppioDescrPtr[i]->mut_inQueue);
 		free(pThis->ppioDescrPtr[i]);
 	}
 

runtime/tcpsrv.h
@@ -99,12 +99,14 @@ struct tcpsrv_io_descr_s {
 	} ptr;
 	int isInError; /* boolean, if set, subsystem indicates we need to close because we had an
 			* unrecoverable error at the network layer. */
+	int inQueue;	/**< flag: descriptor queued */
 	tcpsrv_t *pSrv;	/* our server object */
 	tcpsrv_io_descr_t *next; /* for use in workQueue_t */
 	#if defined(ENABLE_IMTCP_EPOLL)
 	struct epoll_event event; /* to re-enable EPOLLONESHOT */
 	#endif
 	DEF_ATOMIC_HELPER_MUT(mut_isInError);
+	DEF_ATOMIC_HELPER_MUT(mut_inQueue);
 };
 
 #define TCPSRV_NO_ADDTL_DELIMITER -1 /* specifies that no additional delimiter is to be used in TCP framing */