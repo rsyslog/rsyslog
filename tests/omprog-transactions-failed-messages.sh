bb1024577cc99bba510c42af5cd7eea48f81419b
Rainer Gerhards <rgerhards@adiscon.com>
2025-07-25 08:39:37 +0200

    add revised test to check code change

--- tests/omprog-transactions-failed-messages.sh
+++ tests/omprog-transactions-failed-messages.sh
@@ -68,9 +68,9 @@ while IFS= read -r line; do
                 # If that's not the expected behavior, the following lines must
                 # be removed when the bug is solved.
                 #
-                # (START OF CODE THAT WILL POSSIBLY NEED TO BE REMOVED)
-                messages_processed+=("${messages_to_commit[@]}")
-                unset "messages_processed[${#messages_processed[@]}-1]"
+                ## (START OF CODE THAT WILL POSSIBLY NEED TO BE REMOVED)
+                #messages_processed+=("${messages_to_commit[@]}")
+                #unset "messages_processed[${#messages_processed[@]}-1]"
                 # (END OF CODE THAT WILL POSSIBLY NEED TO BE REMOVED)
 
                 messages_to_commit=()
