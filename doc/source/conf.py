0c87fb405703e03eb3492d485fbee2fff0fed396
Rainer Gerhards <rgerhards@adiscon.com>
2025-08-26 11:45:08 +0200

    prepare for next scheduled stable release cycle

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -94,9 +94,9 @@ rst_epilog = """
 # real values will be generated dynamically from info in the repo. If the
 # user builds the docs from "bare" sources not yet processed
 ###############################################################################
-version = '8.2508'
-release = '8.2508.0'
-#release = version + ' daily stable'
+version = '8.2510'
+#release = '8.2510.0'
+release = version + ' daily stable'
 
 # For this to be true, it means that we are not attempting to build from
 # a release tarball, as otherwise the values above would have been replaced
