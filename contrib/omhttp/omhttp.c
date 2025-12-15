2508bcd4d9aecf7524224706dfd5a49a51e7e721
Shini <adrien.gandarias@gmail.com>
2025-12-15 14:38:52 +0100

    Add support healthchecktimedelay into omhttp

--- contrib/omhttp/omhttp.c
+++ contrib/omhttp/omhttp.c
@@ -199,6 +199,10 @@ typedef struct instanceConf_s {
     /* Stats Counter */
     targetStats_t *listObjStats;
     sbool statsBySenders;
+
+    /* Health Check */
+    time_t *lastHealthCheck;
+    long healthCheckTimeDelay;  // Delay between two Health Check (in seconds)
 } instanceData;
 
 struct modConfData_s {
@@ -278,6 +282,7 @@ static struct cnfparamdescr actpdescr[] = {
     {"httpignorablecodes", eCmdHdlrArray, 0},
     {"profile", eCmdHdlrGetWord, 0},
     {"statsbysenders", eCmdHdlrBinary, 0},
+    {"healthchecktimedelay", eCmdHdlrInt, 0},
 };
 static struct cnfparamblk actpblk = {CNFPARAMBLK_VERSION, sizeof(actpdescr) / sizeof(struct cnfparamdescr), actpdescr};
 
@@ -412,6 +417,7 @@ BEGINdbgPrintInstInfo
     dbgprintf("\ttemplate='%s'\n", pData->tplName);
     dbgprintf("\tnumServers=%d\n", pData->numServers);
     dbgprintf("\thealthCheckTimeout=%lu\n", pData->healthCheckTimeout);
+    dbgprintf("\thealthchecktimedelay=%lu\n", pData->healthCheckTimeDelay);
     dbgprintf("\trestPathTimeout=%lu\n", pData->restPathTimeout);
     dbgprintf("\tserverBaseUrls=");
     for (i = 0; i < pData->numServers; ++i) dbgprintf("%c'%s'", i == 0 ? '[' : ' ', pData->serverBaseUrls[i]);
@@ -560,6 +566,24 @@ static rsRetVal ATTR_NONNULL() checkConn(wrkrInstanceData_t *const pWrkrData) {
     if (pWrkrData->pData->checkPath == NULL) {
         DBGPRINTF("omhttp: checkConn no health check uri configured skipping it\n");
         FINALIZE;
+    }
+
+    /*  Check last time was health check was done
+     *   1- get lastHealthCheck for all server in the list
+     *   2- Check diff time is in good order
+     *   3- add lastcheck upgrade
+     *   4- time_t is an unsigned long (https://koor.fr/C/ctime/time_t.wp)
+     */
+    time_t now = time(NULL);
+    struct tm now_tm = *localtime(&now);
+    if (pWrkrData->pData->lastHealthCheck != NULL && pWrkrData->pData->healthCheckTimeDelay != -1) {
+        const time_t currentServerHealthCheck = pWrkrData->pData->lastHealthCheck[pWrkrData->serverIndex];
+        struct tm lastcheck_tm = *localtime(&currentServerHealthCheck);
+        lastcheck_tm.tm_sec += (int)pWrkrData->pData->healthCheckTimeDelay;
+
+        if (difftime(mktime(&lastcheck_tm), mktime(&now_tm)) >= 0) {
+            ABORT_FINALIZE(RS_RET_OK);
+        }
     }
 
     pWrkrData->reply = NULL;
@@ -1862,6 +1886,8 @@ static void ATTR_NONNULL() setInstParamDefaults(instanceData *const pData) {
     pData->serverBaseUrls = NULL;
     pData->defaultPort = 443;
     pData->healthCheckTimeout = 3500;
+    pData->healthCheckTimeDelay = -1;  // in seconds | -1 is disable
+    pData->lastHealthCheck = NULL;
     pData->token = NULL;
     pData->uid = NULL;
     pData->restPathTimeout = 0;
@@ -2218,6 +2244,8 @@ BEGINnewActInst
             pData->defaultPort = (int)pvals[i].val.d.n;
         } else if (!strcmp(actpblk.descr[i].name, "healthchecktimeout")) {
             pData->healthCheckTimeout = (long)pvals[i].val.d.n;
+        } else if (!strcmp(actpblk.descr[i].name, "healthchecktimedelay")) {
+            pData->healthCheckTimeDelay = (long)pvals[i].val.d.n;
         } else if (!strcmp(actpblk.descr[i].name, "restpathtimeout")) {
             pData->restPathTimeout = (long)pvals[i].val.d.n;
         } else if (!strcmp(actpblk.descr[i].name, "token")) {
@@ -2617,7 +2645,7 @@ NO_LEGACY_CONF_parseSelectorAct
     BEGINqueryEtryPt CODESTARTqueryEtryPt;
 CODEqueryEtryPt_STD_OMODTX_QUERIES CODEqueryEtryPt_STD_OMOD8_QUERIES CODEqueryEtryPt_STD_CONF2_OMOD_QUERIES
     CODEqueryEtryPt_doHUP CODEqueryEtryPt_doHUPWrkr /* Load the worker HUP handling code */
-    CODEqueryEtryPt_STD_CONF2_QUERIES
+        CODEqueryEtryPt_STD_CONF2_QUERIES
 ENDqueryEtryPt
 
 
